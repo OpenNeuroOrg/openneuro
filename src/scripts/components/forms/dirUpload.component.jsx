47dafafe36a3b163ef3e08dbf0aecd73afab1020
Zack Michael <zack@squishmedia.com>
2015-06-04 15:13:26 -0700

    Refactored upload file tree generation to store full file reference

--- src/scripts/components/forms/dirUpload.component.jsx
+++ src/scripts/components/forms/dirUpload.component.jsx
@@ -26,47 +26,62 @@ let Upload = React.createClass({
 
 	_onFileSelect: function (e) {
 		let files = e.target.files;
+
+	/////////////////////////////////////////////////////////////////////
+		var f = files[5];
+		//console.log(f);
+		var reader = new FileReader();
+		reader.onloadend = function (evt) {
+		//	if (evt.target.readyState == FileReader.DONE) {
+				//console.log(evt);
+				//console.log(evt.target.result);
+		//	}
+		};
+		reader.readAsBinaryString(f);
+	/////////////////////////////////////////////////////////////////////
+
 		this.props.onChange(this._generateFileTree(files));
 	},
 
 	_generateFileTree: function (files) {
-		let pathList = [];
-		let dirTree  = {};
+		let pathList = {};
+		let dirTree = {};
 
-		// generate list of paths
+        // generate list of paths
 		for (let i = 0; i < files.length; i++) {
 			let file = files[i];
-            pathList.push(file.webkitRelativePath);
+            pathList[file.webkitRelativePath] = file;
         }
 
         // build path from list
-        for (let i = 0; i < pathList.length; i++) {
-        	let path = pathList[i];
+        for (let key in pathList) {
+        	let path = key;
         	let pathParts = path.split('/');
         	let subObj = dirTree;
         	for (let j = 0; j < pathParts.length; j++) {
         		let part = pathParts[j];
         		if (!subObj[part]) {
-        			subObj[part] = j < pathParts.length - 1 ? {} : path;
+        			subObj[part] = j < pathParts.length - 1 ? {} : pathList[key];
         		}
         		subObj = subObj[part];
         	}
         }
 
-        // convert dirTree to array structure
+		// convert dirTree to array structure
         function objToArr (obj) {
-			let arr = [];
-			for (let key in obj) {
-				if (typeof obj[key] != 'object') {
-					arr.push({name: key, type: 'file', path: obj[key]})
-				} else {
-					arr.push({name: key, type: 'folder', children: objToArr(obj[key])});
-				}
-			}
-			return arr;
+        	let arr = [];
+        	for (let key in obj) {
+        		if (obj[key].webkitRelativePath && obj[key].webkitRelativePath.length > 0) {
+        			arr.push(obj[key]);
+        		} else {
+        			arr.push({name: key, type: 'folder', children: objToArr(obj[key])});
+        		}
+        	}
+        	return arr;
 		}
 
 		dirTree = objToArr(dirTree);
+		console.log(dirTree);
 
         // return tree
         return dirTree;
