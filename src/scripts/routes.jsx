c1f5b37dec47aea19777811e17f353e0fb3121fc
Zack Michael <zack@squishmedia.com>
2015-06-10 13:03:19 -0700

    renamed components dir to common

--- src/scripts/routes.jsx
+++ src/scripts/routes.jsx
@@ -5,17 +5,17 @@ import {DefaultRoute, Route} from 'react-router';
 import requireAuth from './utils/requireAuth';
 
 // views
-import App    from './components/app.jsx';
+import Index    from './common/index.jsx';
 import Signin from './user/signin.component.jsx';
 import Upload from './upload/upload.component.jsx';
-import Home   from './components/views/home.component.jsx';
+import Home   from './common/views/home.component.jsx';
 
 // routes ----------------------------------------------------------------
 
 Upload = requireAuth(Upload);
 
 let routes = (
-	<Route name="app" path="/" handler={App}>
+	<Route name="app" path="/" handler={Index}>
 		<Route name="signIn" handler={Signin}/>
 		<Route name="upload" handler={Upload}/>
 		<Route name="home" handler={Home}/>
