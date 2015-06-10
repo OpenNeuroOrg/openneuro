1a1e0b138f1e40f4264c2b04ccb293676ae697bf
Zack Michael <zack@squishmedia.com>
2015-06-10 13:08:35 -0700

    created new error component

---
+++ src/scripts/upload/error.component.jsx
@@ -0,0 +1,25 @@
+// dependencies -------------------------------------------------------
+
+import React from 'react';
+
+// component setup ----------------------------------------------------
+
+let Error = React.createClass({
+
+// life cycle events --------------------------------------------------
+
+	render: function () {
+
+		return (
+			<div></div>
+    	);
+	}
+
+// custom methods -----------------------------------------------------
+
+});
+
+export default Error;
+
+
+
