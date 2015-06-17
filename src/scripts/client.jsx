d0373b8fb039fa33dbc31c039d44ddaffc593c0a
Zack Michael <zack@squishmedia.com>
2015-06-17 15:27:21 -0700

    created and setup a router container to avoid circular dependencies with the router

--- src/scripts/client.jsx
+++ src/scripts/client.jsx
@@ -1,13 +1,16 @@
 // dependencies ---------------------------------------------------------
 
-import React  from 'react';
-import Router from 'react-router';
-import routes from './routes.jsx';
-
+import React           from 'react';
+import Router          from 'react-router';
+import routes          from './routes.jsx';
+import RouterContainer from './utils/router-container';
 
 // intialize routes -----------------------------------------------------
 
-Router.run(routes, function (Handler) {
+var router = Router.create({routes});
+RouterContainer.set(router);
+
+router.run(function (Handler) {
 	React.render(<Handler/>, document.getElementById('main'));
 });
 
