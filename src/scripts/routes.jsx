6a87061eafb9e1eef80fc19675b87cb821a141d3
Zack Michael <zack@squishmedia.com>
2015-06-17 17:14:38 -0700

    updated routes so we're getting proper redirects

--- src/scripts/routes.jsx
+++ src/scripts/routes.jsx
@@ -1,7 +1,7 @@
 // dependencies ----------------------------------------------------------
 
 import React from 'react';
-import {DefaultRoute, Route} from 'react-router';
+import {NotFoundRoute, DefaultRoute, Route} from 'react-router';
 import requireAuth from './utils/requireAuth';
 
 // views
@@ -10,16 +10,26 @@ import Signin from './user/signin.component.jsx';
 import Upload from './upload/upload.component.jsx';
 import Home   from './common/views/home.component.jsx';
 
+var Redirect = React.createClass({
+  statics: {
+    willTransitionTo: function (transition) {
+      transition.redirect('signIn');
+    }
+  },
+  render: function () {}
+});
+
 // routes ----------------------------------------------------------------
 
 Upload = requireAuth(Upload);
 
 let routes = (
 	<Route name="app" path="/" handler={Index}>
-		<Route name="signIn" handler={Signin}/>
-		<Route name="upload" handler={Upload}/>
-		<Route name="home" handler={Home}/>
+		<Route name="signIn" path="sign-in" handler={Signin}/>
+		<Route name="upload" path="upload"  handler={Upload}/>
+		<Route name="home"   path="home"    handler={Home}/>
 		<DefaultRoute handler={Signin}/>
+		<NotFoundRoute handler={Redirect}/>
 	</Route>
 );
 
