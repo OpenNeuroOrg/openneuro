ccc9be4c13cc6cd77e62a9814d49fa065cc8c8a1
Zack Michael <zack@squishmedia.com>
2015-07-20 13:25:09 -0700

    wrapped md5worker in check to ensure it's not being messaged by the initial worker load

--- src/scripts/utils/md5worker.js
+++ src/scripts/utils/md5worker.js
@@ -2,17 +2,23 @@ import SparkMD5 from 'spark-md5';
 
 self.addEventListener("message", function(e) {
 
-	let file       = e.data,
-        fileReader = new FileReader(),
-    	spark      = new SparkMD5.ArrayBuffer();
+    if (e.data.request !== 'postUri') {
 
-    fileReader.onload = function (e) {
-    	let buffer = e.target.result;
-    	spark.append(buffer);
-    	let hash = spark.end();
-    	postMessage({hash: hash, buffer: buffer});
+    	let file       = e.data,
+            fileReader = new FileReader(),
+        	spark      = new SparkMD5.ArrayBuffer();
+
+        console.log(file.name);
+
+        fileReader.onload = function (e) {
+        	let buffer = e.target.result;
+        	spark.append(buffer);
+        	let hash = spark.end();
+        	postMessage({hash: hash, buffer: buffer});
+        }
+
+        fileReader.readAsArrayBuffer(file);
+
     }
-
-    fileReader.readAsArrayBuffer(file);
 
 }, false);
