f681d6b81c57d10e28b4e79302eeedc342fae8af
Zack Michael <zack@squishmedia.com>
2015-08-13 09:34:46 -0700

    added filetree view back into rename component

--- src/scripts/upload/upload.rename.jsx
+++ src/scripts/upload/upload.rename.jsx
@@ -5,6 +5,8 @@ import Reflux      from 'reflux';
 import Actions     from './upload.actions.js';
 import UploadStore from './upload.store.js';
 import Input       from '../common/forms/input.component.jsx';
+import FileTree    from './upload.file-tree.jsx';
+import {Accordion, Panel} from 'react-bootstrap';
 
 let Rename = React.createClass({
 
@@ -13,7 +15,8 @@ let Rename = React.createClass({
 // life cycle events --------------------------------------------------
 
 	render () {
-		let dirName = this.state.dirName;
+		let dirName = this.state.dirName,
+			tree    = this.state.tree;
 
 		return (
 			<div>
@@ -22,6 +25,11 @@ let Rename = React.createClass({
 					<label className="add-name"><i className="folderIcon fa fa-folder-open" /></label>
 					<Input type="text" placeholder="dataset name" value={dirName} onChange={this._updateDirName} />
 				</div>
+				<Accordion className="fileStructure fadeIn">
+					<Panel header="View File Structure" eventKey='1'>
+				  		<FileTree tree={tree}/>
+				  	</Panel>
+			  	</Accordion>
 				<button className="btn-blue" onClick={this._validate.bind(null, this.state.list)}>Continue</button>
 			</div>
     	);
