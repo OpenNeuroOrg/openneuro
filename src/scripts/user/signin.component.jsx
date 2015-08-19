d11f26832699b105ad59f67e8b1e00426e093221
Zack Michael <zack@squishmedia.com>
2015-08-19 12:18:39 -0700

    setup redirect so sign-in will redirect to dashboard if user is signed in

--- src/scripts/user/signin.component.jsx
+++ src/scripts/user/signin.component.jsx
@@ -2,11 +2,17 @@
 
 import React        from 'react';
 import Actions      from './user.actions.js';
-import UserStore    from './user.store.js';
+import userStore    from './user.store.js';
 
 export default class Signin extends React.Component {
 
 // life cycle events --------------------------------------------------
+	
+	static willTransitionTo(transition) {
+		if (userStore.data.token) {
+			transition.redirect('dashboard');
+		}
+	}
 
 	render () {
 		return (
