21382b41728e7f09ba7701ab79d4a45425ac3e07
Zack Michael <zack@squishmedia.com>
2015-08-20 15:09:20 -0700

    added chunked hash generation for lower memory browsers

--- src/scripts/utils/md5worker.js
+++ src/scripts/utils/md5worker.js
@@ -3,18 +3,39 @@ import SparkMD5 from 'spark-md5';
 self.addEventListener("message", function(e) {
     if (e.data.request !== 'postUri') {
 
-    	let file       = e.data,
-            fileReader = new FileReader(),
-        	spark      = new SparkMD5.ArrayBuffer();
+        let file = e.data;
 
-        fileReader.onload = function (e) {
-        	let buffer = e.target.result;
-        	spark.append(buffer);
-        	let hash = spark.end();
-        	postMessage({hash: hash, buffer: buffer});
+        let blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,    
+            chunkSize = 2097152, // Read in chunks of 2MB     
+            chunks = Math.ceil(file.size / chunkSize),
+            currentChunk = 0,   
+            spark = new SparkMD5.ArrayBuffer(),
+            fileReader = new FileReader();
+
+        fileReader.onload = function (e) {   
+            spark.append(e.target.result);
+            currentChunk++;
+       
+            if (currentChunk < chunks) {   
+                loadNext();
+            } else {
+                let hash = spark.end();
+                postMessage({hash});
+            }
+       
+        };
+       
+        fileReader.onerror = function (e) {
+           postMessage(e);   
+        };
+
+        function loadNext() {
+            let start = currentChunk * chunkSize,
+            end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
+            fileReader.readAsArrayBuffer(blobSlice.call(file, start, end));
         }
 
-        fileReader.readAsArrayBuffer(file);
+        loadNext();
 
     }
 }, false);
