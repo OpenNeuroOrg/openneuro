c3b54717465252d3121748e00db42d3c16b186b3
Zack Michael <zack@squishmedia.com>
2015-09-23 14:42:53 -0700

    added group creation to user creation

--- libs/scitran.js
+++ libs/scitran.js
@@ -13,7 +13,24 @@ export default {
 	 * Create User
 	 */
 	createUser(user, callback) {
-		request.post(config.scitran.baseURL + 'users', {body: user}, callback);
-	}
+		request.post(config.scitran.baseURL + 'users', {body: user}, (err, res) => {
+			this.createGroup(user._id, user._id, callback);
+		});
+	},
+
+	/**
+     * Create Group
+     *
+     * Takes a groupName and a userId and
+     * creates a group with that user as the
+     * admin.
+     */
+    createGroup (groupName, userId, callback) {
+        let body = {
+            _id: groupName,
+            roles: [{access: 'admin', _id: userId}]
+        };
+        request.post(config.scitran.url + 'groups', {body: body}, callback);
+    }
 
 }
