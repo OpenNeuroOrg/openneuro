af64ef1a5933486f750de0e528e7f2c62e32ffb9
Gregory Noack <greg@squishymedia.com>
2015-09-24 14:54:44 -0700

    final adjustments

--- src/scripts/user/signin.component.jsx
+++ src/scripts/user/signin.component.jsx
@@ -4,6 +4,7 @@ import React     from 'react';
 import Reflux    from 'reflux';
 import Actions   from './user.actions.js';
 import userStore from './user.store.js';
+import {Link} 	 from 'react-router';
 import Spinner   from '../common/partials/spinner.component.jsx';
 
 // component setup ----------------------------------------------------
@@ -24,9 +25,9 @@ let Signin = React.createClass({
 
 	render () {
 		let browse = (
-			<button className="btn-admin" onClick={Actions.signIn} >
+			<Link to="public" className="btn-admin">
 				<span>Browse Publicly</span>
-			</button>
+			</Link>
 		);
 		let google = (
 			<button className="btn-admin" onClick={Actions.signIn} >
@@ -37,8 +38,9 @@ let Signin = React.createClass({
 		let form;
 		if (!this.state.loading) {
 			form = (
-				<div className="form">
+				<div className="form col-xs-12 clearfix">
 					{google}
+					 <span className="conjunction">Or</span>
 					{browse}
 				</div>
 			)
@@ -48,37 +50,22 @@ let Signin = React.createClass({
 		return (
 			<div className="sign-in">
 				<div className="intro">
-					<div className="intro-inner fadeIn clearfix">
-						<div className="welcomeBlock flipInX">
-							<h1>Welcome To CRN.</h1>
-							<h2>This some welcome text to explain what is what. Please do this thing to do some other thing here.</h2>
-						</div>
-						<div className="col-sm-12 signInBlock fadeIn">
-							<h2>Sign In with</h2>
-							{form}
-							<Spinner text="Signing in..." active={this.state.loading} />
-						</div>
-					</div>
-					
-					<div className="intro-teasers clearfix">
-						<div className="container maxed">
-							<div className="col-md-4 fadeInUp">
-								<img src="./assets/colab.png" alt="data" />
-								<h4 className="clearfix">Colaborate, Share, and Study</h4>
-								<p>Enhance your research through collaboration. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an </p>
+					<div className="introBG">
+						<div className="intro-inner fadeIn clearfix">
+							<div className="welcomeBlock flipInX">
+								<h1>Welcome to CRN</h1>
+								<h2>A free and open platform that enables the analysis and sharing of neuroimaging data.</h2>
 							</div>
-							<div className="col-md-4 fadeInUp">
-								<img src="./assets/server.png" alt="data" />
-								<h4 className="clearfix">Utilizing Powerful Servers</h4>
-								<p>Agave provides hosted services that allow researchers to manage data, conduct experiments, and publish and share results from anywhere at any time. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an </p>
-							</div>
-							<div className="col-md-4 fadeInUp">
-								<img src="./assets/data.png" alt="data" />
-								<h4 className="clearfix">Get Results</h4>
-								<p>Powerful algorithms to produce substantial data. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an </p>
+							<div className="col-sm-12 signInBlock fadeIn">
+								<h2>Sign In with</h2>
+								{form}
+								<Spinner text="Signing in..." active={this.state.loading} />
 							</div>
 						</div>
 					</div>	
+					<div className="more-info col-xs-12">
+						for more information about the <a href="http://reproducibility.stanford.edu/" target="_blank">Stanford Center for Reproducible Neuroscience</a> and <a href="#" target="_blank">BIDS Specifications</a>
+					</div>
 				</div>
 			</div>
     	);
