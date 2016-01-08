791c06066d6273172f81a66a38dd3699565e8820
Zack Michael <zack@squishymedia.com>
2016-01-07 16:45:57 -0800

    removed unnecessary items from git

--- scripts/create-agave-client.js
+++
@@ -1,6 +0,0 @@
-import agave from '../libs/agave';
-
-agave.createClient((err, res) => {
-	console.log(res.body);
-	console.log(res.req.path);
-});
--- scripts/list-agave-apps.js
+++
@@ -1,6 +0,0 @@
-import agave from '../libs/agave';
-
-agave.listApps((err, res) => {
-	console.log(JSON.stringify(res.body));
-	console.log(res.req.path);
-});
