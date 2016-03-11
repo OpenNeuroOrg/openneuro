13a05ea99a9ae195c4dc95fa47392ce580643fa8
Zack Michael <zack@squishymedia.com>
2016-03-10 16:50:41 -0800

    Updated formatting of job results

--- src/scripts/dataset/dataset.jobs.jsx
+++ src/scripts/dataset/dataset.jobs.jsx
@@ -7,6 +7,7 @@ import actions      from './dataset.actions';
 import Spinner      from '../common/partials/spinner.jsx';
 import { Accordion, Panel } from 'react-bootstrap';
 import WarnButton   from '../common/forms/warn-button.jsx';
+import moment        from 'moment';
 
 let Jobs = React.createClass({
 
@@ -17,50 +18,13 @@ let Jobs = React.createClass({
 	render () {
 
 		let jobs = this.state.jobs.map((job) => {
-			let results, parameters;
-
-			if (job.results) {
-				results = job.results.map((result, index) => {
-					return (
-						<li key={index}>
-							<WarnButton
-								icon="fa-download"
-								tooltip="Download File"
-								prepDownload={actions.getResultDownloadTicket.bind(this, job.jobId, result.name)} />
-							<span>{result.name}</span>
-						</li>
-					);
-				});
-			}
-
-			if (job.parameters) {
-				parameters = [];
-				for (let key in job.parameters) {
-					parameters.push(
-						<li key={key}>
-							<span>{key}</span>: <span>{job.parameters[key]}</span>
-						</li>
-					);
-				}
-			}
-			let jobAccordionHeader = (<div className={job.agave.status.toLowerCase()}>{job.name}<span className="badge">{job.agave.status}</span></div>);
 			return (
-				<Panel className="job" header={jobAccordionHeader}  key={job._id} eventKey={job._id}>
-						<h5>application: {job.appId}</h5>
-						<p>created by {job.userId} at {job.agave.created}</p>
-						{/*
-						<p>id: {job._id}</p>
-						<p>agave job ID: {job.jobId}</p>
-						*/}
-						<ul>{parameters}</ul>
-					<Accordion accordion className="results">
-						<Panel className="fadeIn" header="Download Results"  key={job._id} eventKey={job._id}>
-							<ul>{results}</ul>
-						</Panel>
-					</Accordion>
+				<Panel className="job" header={job.appId}  key={job.appId} eventKey={job.appId}>
+						{this._runs(job)}
 				</Panel>
 			);
 		});
+
 		let header = <h3 className="metaheader">Analyses</h3>;
 		return (
 			<div className="analyses">
@@ -72,7 +36,71 @@ let Jobs = React.createClass({
     	);
 	},
 
-// custom methods -----------------------------------------------------
+// templates methods --------------------------------------------------
+
+	_runs(job) {
+		let runs = job.runs.map((run) => {
+			let results, parameters;
+
+			let jobAccordionHeader = (<div className={run.agave.status.toLowerCase()}><span className="badge">{run.agave.status}</span></div>);
+
+			return (
+				<Panel className="job" header={jobAccordionHeader}  key={run._id} eventKey={run._id}>
+					<p>Run by {run.userId} on {moment(run.agave.created).format('L')}</p>
+					{this._parameters(run)}
+					{this._results(run)}
+				</Panel>
+			);
+		});
+		return runs;
+	},
+
+	_results(run) {
+		if (run.results) {
+			let resultLinks = run.results.map((result, index) => {
+				return (
+					<li key={index}>
+						<WarnButton
+							icon="fa-download"
+							tooltip="Download File"
+							prepDownload={actions.getResultDownloadTicket.bind(this, run.jobId, result.name)} />
+						<span>{result.name}</span>
+					</li>
+				);
+			});
+
+			return (
+				<Accordion accordion className="results">
+					<Panel className="fadeIn" header="Download Results" key={run._id} eventKey={run._id}>
+						<ul>{resultLinks}</ul>
+					</Panel>
+				</Accordion>
+			);
+		}
+	},
+
+	_parameters(run) {
+		if (run.parameters) {
+			let parameters = [];
+			for (let key in run.parameters) {
+				parameters.push(
+					<li key={key}>
+						<span>{key}</span>: <span>{run.parameters[key]}</span>
+					</li>
+				);
+			}
+
+			return (
+				<Accordion accordion className="results">
+					<Panel className="fadeIn" header="Parameters" key={run._id} eventKey={run._id}>
+						<ul>{parameters}</ul>
+					</Panel>
+				</Accordion>
+			);
+		}
+	},
+
+// actions ------------------------------------------------------------
 
 	_downloadResult(jobId, fileName) {
 		actions.downloadResult(jobId, fileName);
