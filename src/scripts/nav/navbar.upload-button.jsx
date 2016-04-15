27047aa0f915c2da149138b72520c607bb0fa9a8
Gregory Noack <greg@squishymedia.com>
2016-04-15 10:03:01 -0700

    linting jsx files in nav dir

--- src/scripts/nav/navbar.upload-button.jsx
+++ src/scripts/nav/navbar.upload-button.jsx
@@ -1,69 +1,64 @@
 // dependencies ------------------------------------------------------------------
 
-import React       		from 'react';
-import Reflux      		from 'reflux';
-import {Link}      		from 'react-router';
-import Actions     		from '../upload/upload.actions.js';
-import uploadStore 		from '../upload/upload.store.js';
-import Upload       	from '../upload/upload.jsx';
+import React            from 'react';
+import Reflux           from 'reflux';
+import Actions          from '../upload/upload.actions.js';
+import uploadStore      from '../upload/upload.store.js';
+import Upload           from '../upload/upload.jsx';
 import {ProgressBar,
-		Modal} 			from 'react-bootstrap';
+        Modal}          from 'react-bootstrap';
 
 // component setup ---------------------------------------------------------------
 
 let UploadBtn = React.createClass({
 
-	mixins: [Reflux.connect(uploadStore)],
+    mixins: [Reflux.connect(uploadStore)],
 
 // life cycle methods ------------------------------------------------------------
 
-	render: function () {
+    render: function () {
 
-		let completed = this.state.progress.completed;
-		let total     = this.state.progress.total;
-		let progress  = total > 0 ? Math.floor(completed / total * 100) : 0;
+        let completed = this.state.progress.completed;
+        let total     = this.state.progress.total;
+        let progress  = total > 0 ? Math.floor(completed / total * 100) : 0;
 
-		progress = (
-			<div>
-				<button className="btn btn-blue btn-view-progress"  onClick={Actions.toggleModal}>view details</button>
-				<ProgressBar active now={progress} />
-			</div>
-		)
+        progress = (
+            <div>
+                <button className="btn btn-blue btn-view-progress"  onClick={Actions.toggleModal}>view details</button>
+                <ProgressBar active now={progress} />
+            </div>
+        );
 
-		let uploadBtn = (
-			<div>
-				<button className="btn btn-blue"  onClick={Actions.toggleModal}>Upload Dataset</button>
-			</div>
-		);
+        let uploadBtn = (
+            <div>
+                <button className="btn btn-blue"  onClick={Actions.toggleModal}>Upload Dataset</button>
+            </div>
+        );
 
-		let uploadModal = (
+        let uploadModal = (
             <Modal show={this.state.showModal} onHide={Actions.toggleModal} className="upload-modal">
-            	<Modal.Header closeButton>
-            		<Modal.Title>Upload Dataset</Modal.Title>
-            	</Modal.Header>
-            	<hr className="modal-inner" />
-            	<Modal.Body>
-            		<div className="tasks-col fadeIn"><Upload /></div>
-            	</Modal.Body>
+                <Modal.Header closeButton>
+                    <Modal.Title>Upload Dataset</Modal.Title>
+                </Modal.Header>
+                <hr className="modal-inner" />
+                <Modal.Body>
+                    <div className="tasks-col fadeIn"><Upload /></div>
+                </Modal.Body>
             </Modal>
         );
 
-		return (
-			<span>
-				<div className="upload-btn-wrap">
-				{uploadStore.data.uploadStatus == 'uploading' ? progress : uploadBtn}
-				{uploadModal}
-				<img src="/favicon-upload.png" id="favicon_upload" className="hidden"/>
-				</div>
-			</span>
-	    );
+        return (
+            <span>
+                <div className="upload-btn-wrap">
+                {uploadStore.data.uploadStatus == 'uploading' ? progress : uploadBtn}
+                {uploadModal}
+                <img src="/favicon-upload.png" id="favicon_upload" className="hidden"/>
+                </div>
+            </span>
+        );
 
-	},
-
-// custom methods ----------------------------------------------------------------
+    }
 
 });
-
-
 
 export default UploadBtn;
--- src/scripts/nav/navbar.usermenu.jsx
+++ src/scripts/nav/navbar.usermenu.jsx
@@ -1,64 +1,57 @@
 // dependencies ------------------------------------------------------------------
 
-import React       		from 'react';
-import Reflux      		from 'reflux';
-import Actions     		from '../user/user.actions.js';
-import userStore   		from '../user/user.store.js';
-import uploadStore 		from '../upload/upload.store.js';
+import React            from 'react';
+import Reflux           from 'reflux';
+import Actions          from '../user/user.actions.js';
+import userStore        from '../user/user.store.js';
+import uploadStore      from '../upload/upload.store.js';
 import {DropdownButton} from 'react-bootstrap';
 
 // component setup ---------------------------------------------------------------
 
 let Usermenu = React.createClass({
 
-	mixins: [Reflux.connect(userStore)],
+    mixins: [Reflux.connect(userStore)],
 
 // life cycle methods ------------------------------------------------------------
 
-	render: function () {
-		let self = this;
-		let isLoggedIn = !!this.state.token;
+    render: function () {
+        //generate user menu
+        let thumbnail,
+            username = this.state.google.displayName;
 
-		//generate user menu
-		let usermenu, thumbnail;
+        if (this.state.google.picture) {
+            thumbnail = this.state.google.picture.replace('sz=50', 'sz=200');
+        }
 
-		let username = this.state.google.displayName;
-		let email   = this.state.google.email;
+        let gear = (<i className="fa fa-gear" />);
 
-		if (this.state.google.picture) {
-			thumbnail = this.state.google.picture.replace("sz=50", "sz=200");
-		}
-
-		let gear = (<i className="fa fa-gear" />);
-
-		// TEMPORARY INLINE STYLING BELOW
-		// MOVE TO STYLESHEET AND REMOVE THIS COMMENT
-		return (
-			<div className="clearfix user-wrap">
-				<span className="username">
-					<span className="greeting">Hello</span>
-					<br/>
-					{username}
-				</span>
-				<img src={thumbnail} alt={username} className="userImgThumb" />
-				<DropdownButton className="user-menu btn-null" eventKey={1} title={gear}>
-					<img src={thumbnail} alt={username} className="userMenuThumb" />
-					<li role="presentation" className="dropdown-header">{username}</li>
-					<li><a onClick={this._signOut} className="um-sign-out">Sign Out</a></li>
-					<li role="separator" className="divider"></li>
-		        </DropdownButton>
-	        </div>
-	    );
-	},
+        // TEMPORARY INLINE STYLING BELOW
+        // MOVE TO STYLESHEET AND REMOVE THIS COMMENT
+        return (
+            <div className="clearfix user-wrap">
+                <span className="username">
+                    <span className="greeting">Hello</span>
+                    <br/>
+                    {username}
+                </span>
+                <img src={thumbnail} alt={username} className="userImgThumb" />
+                <DropdownButton className="user-menu btn-null" eventKey={1} title={gear}>
+                    <img src={thumbnail} alt={username} className="userMenuThumb" />
+                    <li role="presentation" className="dropdown-header">{username}</li>
+                    <li><a onClick={this._signOut} className="um-sign-out">Sign Out</a></li>
+                    <li role="separator" className="divider"></li>
+                </DropdownButton>
+            </div>
+        );
+    },
 
 // custom methods ----------------------------------------------------------------
 
-	_signOut: function () {
-		Actions.signOut(uploadStore.data.uploadStatus);
-	}
+    _signOut: function () {
+        Actions.signOut(uploadStore.data.uploadStatus);
+    }
 
 });
-
-
 
 export default Usermenu;
