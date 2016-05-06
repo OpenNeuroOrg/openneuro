3accccff4a725e2ab8b107f243f953ad744fd4e5
Zack Michael <zack@squishymedia.com>
2016-05-06 14:50:13 -0700

    setup summary to be string encoded and stored as a tag

--- src/scripts/upload/upload.js
+++ src/scripts/upload/upload.js
@@ -53,7 +53,7 @@ export default {
      * updated at the start and end of every file or
      * folder upload request and an error callback.
      */
-    upload (userId, fileTree, validation, count, progress, error) {
+    upload (userId, fileTree, validation, summary, count, progress, error) {
         this.completed = 0;
         this.count = count;
         this.currentProjectId = null;
@@ -96,7 +96,7 @@ export default {
                     this.handleUploadResponse(err, res, () => {
                         let projectId = res.body._id;
                         scitran.addTag('projects', projectId, 'incomplete', () => {
-                            this.uploadSubjects(fileTree[0].name, fileTree[0].children, projectId, validation);
+                            this.uploadSubjects(fileTree[0].name, fileTree[0].children, projectId, validation, summary);
                         });
                     });
                 });
@@ -108,7 +108,7 @@ export default {
      * Upload Subjects
      *
      */
-    uploadSubjects (datasetName, subjects, projectId, validation) {
+    uploadSubjects (datasetName, subjects, projectId, validation, summary) {
         let self = this;
         self.currentProjectId = projectId;
         for (let subject of subjects) {
@@ -133,8 +133,10 @@ export default {
                             }
                         }
                         scitran.updateProject(projectId, {metadata: {authors, validation}}, () => {
-                            let file = new File([JSON.stringify(description)], 'dataset_description.json', {type: 'application/json'});
-                            self.uploadFile('projects', projectId, file, 'project');
+                            scitran.addTag('projects', projectId, 'summary-' + JSON.stringify(summary), () => {
+                                let file = new File([JSON.stringify(description)], 'dataset_description.json', {type: 'application/json'});
+                                self.uploadFile('projects', projectId, file, 'project');
+                            });
                         });
                     });
 
