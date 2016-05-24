1238218bad4a93719eae25ea391e14fa956e5ba4
Zack Michael <zack@squishymedia.com>
2016-05-24 16:27:36 -0700

    Re-apply react update

--- src/scripts/common/partials/tooltip.jsx
+++ src/scripts/common/partials/tooltip.jsx
@@ -1,7 +1,8 @@
 // dependencies ------------------------------------------------------------------
 
-import React       from 'react';
+import React                     from 'react';
 import {Tooltip, OverlayTrigger} from 'react-bootstrap';
+import newId                     from '../../utils/newId';
 
 // component setup ---------------------------------------------------------------
 
@@ -9,8 +10,12 @@ export default class TooltipTop extends React.Component {
 
 // life cycle methods ------------------------------------------------------------
 
+    componentWillMount() {
+        this.id = newId('tooltip-id-');
+    }
+
     render() {
-        let tooltip = <Tooltip>{this.props.tooltip}</Tooltip>;
+        let tooltip = <Tooltip id={this.id}>{this.props.tooltip}</Tooltip>;
 
         return (
             <OverlayTrigger placement='top' overlay={tooltip} delayShow={300} delayHide={150}>
