e0260c42e9534cb4a2aa1c4179baea8054e3d39f
Zack Michael <zack@squishymedia.com>
2016-07-19 13:44:54 -0700

    Cleaned up template retrieval and node-mailer config

--- libs/email/index.js
+++ libs/email/index.js
@@ -1,33 +1,36 @@
 import nodemailer from 'nodemailer';
 import config     from '../../config';
 import templates  from './templates';
-import _          from 'underscore';
+
+// library configuration ---------------------------------------
+
+// setup email transporter
+var transporter = nodemailer.createTransport({
+	service: 'Gmail',
+	auth: {
+		user: config.email.user,
+		pass: config.email.pass
+	}
+});
+
+// public api --------------------------------------------------
 
 export default {
 
+	/**
+	 * Send
+	 *
+	 * Takes an email address, a subject, a template name
+	 * and a data object and immediately sends an email.
+	 */
 	send(receiver, subject, tpl, data) {
-
-		_.templateSettings = {
-			interpolate: /\{\{(.+?)\}\}/g
-		};
-
-		var template = _.template(templates[tpl]);
-
-		// setup transporter
-		var transporter = nodemailer.createTransport({
-			service: 'Gmail',
-			auth: {
-				user: config.email.user,
-				pass: config.email.pass
-			}
-		});
 
 		// configure mail options
 		var mailOptions = {
 			from: config.email.user,
 			to: receiver,
 			subject: subject,
-			html: template(data)
+			html: templates[tpl](data)
 		};
 
 		// send email
