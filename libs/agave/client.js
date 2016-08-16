880725468a8e3423be91ff8528c97c9ecd53ead4
Zack Michael <zack@squishymedia.com>
2016-08-16 14:36:49 -0700

    Fix inconsistent space

--- libs/agave/client.js
+++ libs/agave/client.js
@@ -59,7 +59,7 @@ let client = {
                 console.log(res.body.message);
             } else {
                 // set config changes and save
-                client.config.name           =  config.agave.clientName;
+                client.config.name           = config.agave.clientName;
                 client.config.description    = config.agave.clientDescription;
                 client.config.consumerKey    = res.body.result.consumerKey;
                 client.config.consumerSecret = res.body.result.consumerSecret;
