7c6b9811573d8157ef57b183722a216346461d32
Zack Michael <zack@squishymedia.com>
2016-08-18 13:08:40 -0700

    Restart counts for each unique prefix

--- src/scripts/utils/newid.js
+++ src/scripts/utils/newid.js
@@ -6,9 +6,9 @@
  * accessibility.
  */
 
-let lastId = 0;
+let ids = {};
 
 export default (prefix='id') => {
-	lastId++;
-	return `${prefix}${lastId}`;
+	ids[prefix] = ids[prefix] ? ids[prefix] + 1 : 1;
+	return `${prefix}${ids[prefix]}`;
 }
