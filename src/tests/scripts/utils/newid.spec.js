def5e0f81d04218ed9c759d56ab22e6b88f4591f
Zack Michael <zack@squishymedia.com>
2016-08-18 13:08:58 -0700

    Added tests for newid lib

---
+++ src/tests/scripts/utils/newid.spec.js
@@ -0,0 +1,18 @@
+import assert from 'assert';
+import newid  from '../../../scripts/utils/newid';
+
+describe('utils/newid.js', () => {
+
+    it('should iterate a new id every time it\'s called', () => {
+        assert.equal(newid(), 'id1');
+        assert.equal(newid(), 'id2');
+        assert.equal(newid(), 'id3');
+    });
+
+    it('should accept an id prefix to replace the generic "id"', () => {
+    	assert.equal(newid('my-prefix-'), 'my-prefix-1');
+    	assert.equal(newid('my-prefix-'), 'my-prefix-2');
+    	assert.equal(newid('my-prefix-'), 'my-prefix-3');
+    });
+
+});
