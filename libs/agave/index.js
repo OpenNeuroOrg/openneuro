cf439fd0e4d2e14eef72c4725a952e399fef26b3
Zack Michael <zack@squishymedia.com>
2016-08-24 09:34:16 -0700

    Handle logs not found

--- libs/agave/index.js
+++ libs/agave/index.js
@@ -53,6 +53,8 @@ export default {
                 }, () => {
                     callback(logs, status);
                 });
+            } else {
+                callback(logs, status);
             }
         });
     },
