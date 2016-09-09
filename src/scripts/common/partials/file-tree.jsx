c73bf9891c80ae02a49986a21b8a7ace805397ca
Zack Michael <zack@squishymedia.com>
2016-09-09 12:03:10 -0700

    Restricted file viewing to specific file types

--- src/scripts/common/partials/file-tree.jsx
+++ src/scripts/common/partials/file-tree.jsx
@@ -3,6 +3,7 @@
 import React      from 'react';
 import WarnButton from '../forms/warn-button.jsx';
 import Spinner    from './spinner.jsx';
+import files      from '../../utils/files';
 
 class FileTree extends React.Component {
 
@@ -128,7 +129,10 @@ class FileTree extends React.Component {
         }
 
         let displayBtn;
-        if (!item.children && this.props.displayFile) {
+        if (
+            !item.children && this.props.displayFile &&
+            files.hasExtension(item.name, ['.txt', '.json', '.csv', '.tsv', '.html', '.pdf'])
+        ) {
             displayBtn = (
                 <WarnButton
                     icon="fa-eye"
