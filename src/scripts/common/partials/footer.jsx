b1f7c7428ec305ee8bf200b51544db2cf4ff1d13
zack <zack@squishymedia.com>
2016-10-27 16:44:05 -0700

    Removed sign in and browse links from footer

--- src/scripts/common/partials/footer.jsx
+++ src/scripts/common/partials/footer.jsx
@@ -1,23 +1,14 @@
 // dependencies -------------------------------------------------------
 
 import React       from 'react';
-import Reflux      from 'reflux';
-import {Link}      from 'react-router';
-import uploadStore from '../../upload/upload.store.js';
-import Actions     from '../../user/user.actions.js';
-import userStore   from '../../user/user.store.js';
 import packageJson from '../../../../package.json';
 
 
 let Footer = React.createClass({
 
-    mixins: [Reflux.connect(userStore)],
-
 // life cycle events --------------------------------------------------
 
     render () {
-
-        let showSignOut = !!this.state.token && !this.state.loading;
 
         return (
             <span>
@@ -28,8 +19,6 @@ let Footer = React.createClass({
                     </div>
                     <div className="col-xs-12 col-md-4 footer-menu">
                         <ul>
-                            <li> {showSignOut ? <a onClick={this._signOut}>Sign Out</a> : <a onClick={userStore.signIn.bind(null)} >Sign in</a>}</li>
-                            <li><Link to="public">Browse</Link></li>
                             <li>About</li>
                             <li>Privacy</li>
                             <li>Terms</li>
@@ -42,10 +31,6 @@ let Footer = React.createClass({
             </footer>
             </span>
         );
-    },
-
-    _signOut(){
-        Actions.signOut(uploadStore.data.uploadStatus);
     }
 });
 
