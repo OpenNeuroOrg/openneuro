7ef9366ddda656abac8445d8d6c3eb34a5a9b1cf
Zack Michael <zack@squishymedia.com>
2016-11-17 16:20:46 -0800

    Parse file name from full filepaths

--- src/scripts/dataset/dataset.file-display.jsx
+++ src/scripts/dataset/dataset.file-display.jsx
@@ -17,7 +17,7 @@ export default class FileDisplay extends React.Component {
         return (
             <Modal show={this.props.show} onHide={this.props.onHide} className={'display-file-modal ' + this._extension(file.name)}>
                 <Modal.Header closeButton>
-                    <Modal.Title>{file.name}<div className="modal-download btn-admin-blue">{this._download(file.link)}</div></Modal.Title>
+                    <Modal.Title>{file.name.split('/')[file.name.split('/').length - 1]}<div className="modal-download btn-admin-blue">{this._download(file.link)}</div></Modal.Title>
                 </Modal.Header>
                 <hr className="modal-inner" />
                 <Modal.Body>
