9e73a0b7b4936af3125b0fd1a10f2625bda7750e
Zack Michael <zack@squishymedia.com>
2017-02-14 16:54:30 -0800

    Setup id encoding/decoding strategy

--- src/scripts/utils/crn.js
+++ src/scripts/utils/crn.js
@@ -52,6 +52,27 @@ export default {
 // Datasets --------------------------------------------------------------------------------
 
     /**
+     * Create Project
+     *
+     * Takes a request body and
+     * generates a request to make a project in scitran.
+     */
+    createProject (group, label, callback) {
+        request.post(config.crn.url + 'datasets', {
+            body: {group, label}
+        }, callback);
+    },
+
+    /**
+     * Create Snapshot
+     */
+    createSnapshot (projectId, callback) {
+        request.post(config.crn.url + 'datasets/' + projectId + '/snapshot', {
+            query: {project: projectId}
+        }, callback);
+    },
+
+    /**
      * Add Permission
      */
     addPermission(container, id, permission, callback) {
