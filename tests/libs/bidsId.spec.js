14b9bcc5ccfe6367a80156a5ee3b5e701b2aca16
Zack Michael <zack@squishymedia.com>
2017-02-20 14:51:15 -0800

    Add describe blocks for methods in a file

--- tests/libs/bidsId.spec.js
+++ tests/libs/bidsId.spec.js
@@ -3,22 +3,28 @@ var bidsId = require('../../libs/bidsId');
 
 describe('libs/bidsId.js', () => {
 
-    it('should decode hex strings to ASCII', () => {
-        assert.equal(bidsId.hexToASCII('202020206473303030313437'), '    ds000147');
+    describe('hexToASCII()', () => {
+        it('should decode hex strings to ASCII', () => {
+            assert.equal(bidsId.hexToASCII('202020206473303030313437'), '    ds000147');
+        });
     });
 
-
-    it('should encode ASCII strings to hex', () => {
-        assert.equal(bidsId.hexFromASCII('    ds000147'), '202020206473303030313437');
+    describe('hexFromASCII()', () => {
+        it('should encode ASCII strings to hex', () => {
+            assert.equal(bidsId.hexFromASCII('    ds000147'), '202020206473303030313437');
+        });
     });
 
-    it('should decode hex BIDS IDs to ASCII', () => {
-        assert.equal(bidsId.decodeId('202020206473303030313437'), 'ds000147');
+    describe('decodeId()', () => {
+        it('should decode hex BIDS IDs to ASCII', () => {
+            assert.equal(bidsId.decodeId('202020206473303030313437'), 'ds000147');
+        });
     });
 
-    it('should encode BIDS IDs', () => {
-        assert.equal(bidsId.encodeId('ds000147'), '202020206473303030313437');
+    describe('encodeId()', () => {
+        it('should encode BIDS IDs', () => {
+            assert.equal(bidsId.encodeId('ds000147'), '202020206473303030313437');
+        });
     });
-
 
 });
