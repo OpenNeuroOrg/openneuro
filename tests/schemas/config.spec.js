7b4744b9c469c81adc090cce1f311e3fec740946
Nell Hardcastle <nell@squishymedia.com>
2017-04-09 20:26:23 -0700

    Add tests for schema.validateBody

--- tests/schemas/config.spec.js
+++ tests/schemas/config.spec.js
@@ -66,4 +66,33 @@ describe('schemas/config.js', () => {
             assert.ok(!schema.validateJson(compiledSchema, extraPropConfig));
         });
     });
+    describe('validateBody()', () => {
+        it('should validate the json body of a post request', (done) => {
+            let mockReq = {body: goodConfig};
+            let mockRes = {};
+            let validate = schema.validateBody(configSchema);
+            validate(mockReq, mockRes, () => { done(); });
+        });
+        it('should throw 400 error on invalid json', (done) => {
+            let mockReq = {body: {}};
+            let mockRes = {
+                status: (code) => {
+                    // Check for the 400 code
+                    assert.equal(code, 400);
+                    return {
+                        send: (err) => {
+                            // Verify an error was returned
+                            assert.ok(err);
+                            done();
+                        }
+                    }
+                }
+            }
+            let validate = schema.validateBody(configSchema);
+            validate(mockReq, mockRes, () => {
+                // Fail if next() is called
+                assert.ok(false);
+            });
+        });
+    });
 });
