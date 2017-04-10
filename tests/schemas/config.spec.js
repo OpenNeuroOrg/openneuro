2bc3ab9da1cb106e496179f6ae4377d280931a5a
Nell Hardcastle <nell@squishymedia.com>
2017-04-09 20:33:40 -0700

    Refactor validateBody into a generic test for libs/schema.

--- tests/schemas/config.spec.js
+++ tests/schemas/config.spec.js
@@ -1,39 +1,7 @@
 var assert = require('assert');
 var schema = require('../../libs/schema');
 var configSchema = require('../../schemas/config');
-
-// Dummy configuration that should pass validation
-const goodConfig = {
-    'url': 'http://localhost:9876',
-    'port': 8111,
-    'apiPrefix': '/crn/',
-    'location': '/srv/crn-server',
-    'headers': {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-        'Access-Control-Allow-Headers': 'content-type, Authorization'
-    },
-    'scitran': {
-        'url':       'http://nginx:80/api/',
-        'secret':    'quaemeeco9uXaiquai4ze6moovahroh8',
-        'fileStore': '/srv/bids-core/persistent/data'
-    },
-    'mongo': {
-        'url': 'mongodb://mongo:27017/'
-    },
-    'aws': {
-        'credentials': {
-            'accessKeyId': 'JEIHOIJAQU3EET5ONGOF',
-            'secretAccessKey': 'eeQuahCeekahwiphoyoe3Do8Maekee9Nedohlese',
-            'region': 'us-east-1'
-        },
-        's3': {
-            'bucket': 'test-bucket',
-            'concurrency': 10,
-            'timeout': 10 * 60 * 1000
-        }
-    }
-};
+var goodConfig = require('../data/goodConfig');
 
 describe('schemas/config.js', () => {
     describe('test configuration schema validation', () => {
@@ -64,35 +32,6 @@ describe('schemas/config.js', () => {
             let extraPropConfig = Object.assign({extraConfig: 1234},
                 goodConfig);
             assert.ok(!schema.validateJson(compiledSchema, extraPropConfig));
-        });
-    });
-    describe('validateBody()', () => {
-        it('should validate the json body of a post request', (done) => {
-            let mockReq = {body: goodConfig};
-            let mockRes = {};
-            let validate = schema.validateBody(configSchema);
-            validate(mockReq, mockRes, () => { done(); });
-        });
-        it('should throw 400 error on invalid json', (done) => {
-            let mockReq = {body: {}};
-            let mockRes = {
-                status: (code) => {
-                    // Check for the 400 code
-                    assert.equal(code, 400);
-                    return {
-                        send: (err) => {
-                            // Verify an error was returned
-                            assert.ok(err);
-                            done();
-                        }
-                    }
-                }
-            }
-            let validate = schema.validateBody(configSchema);
-            validate(mockReq, mockRes, () => {
-                // Fail if next() is called
-                assert.ok(false);
-            });
         });
     });
 });
