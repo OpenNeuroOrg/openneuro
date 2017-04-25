4bad7a1f70f99136d075b352233fa45f2a613976
Nell Hardcastle <nell@squishymedia.com>
2017-04-25 16:07:10 -0700

    Return schema validation objects instead of string on error.

--- libs/schema.js
+++ libs/schema.js
@@ -14,17 +14,12 @@ let schema = {
         return compiledSchema(jsonBody);
     },
 
-    getSchemaError(compiledSchema) {
-        let ajv = new Ajv();
-        return ajv.errorsText(compiledSchema.errors);
-    },
-
     validateBody(inputSchema) {
         return function (req, res, next) {
             let compiledSchema = schema.compileJsonSchema(inputSchema);
             let valid = schema.validateJson(compiledSchema, req.body);
             if (!valid) {
-                return res.status(400).send({error: schema.getSchemaError(valid)});
+                return res.status(400).send({error: compiledSchema.errors});
             } else {
                 return next();
             }
