94e739c9a9fc33d31beef9ab79003a9efd636909
Nell Hardcastle <nell@squishymedia.com>
2017-05-10 23:12:19 -0700

    Add some tests for the parameters to CLI arguments function.

---
+++ tests/libs/aws.spec.js
@@ -0,0 +1,31 @@
+var assert   = require('assert');
+var aws = require('../../libs/aws');
+
+const subjectParam = {participant_label: ['01', '02', '03']};
+const nCpusParam = {n_cpus: 4};
+const templateNameParam = {template_name: 'template1'};
+
+describe('libs/aws/batch.js', () => {
+    describe('_prepareArguments()', () => {
+        it('should return a valid list argument', () => {
+            let params = Object.assign({}, subjectParam);
+            let args = aws.batch._prepareArguments(params);
+            assert.equal(args, '--participant_label 01 02 03');
+        });
+        it('should return a valid string argument', () => {
+            let params =  Object.assign({}, templateNameParam);
+            let args = aws.batch._prepareArguments(params);
+            assert.equal(args, '--template_name template1');
+        });
+        it('should return a valid numeric argument', () => {
+            let params = Object.assign({}, nCpusParam);
+            let args = aws.batch._prepareArguments(params);
+            assert.equal(args, '--n_cpus 4');
+        });
+        it('should combine multiple arguments', () => {
+            let params = Object.assign({}, subjectParam, nCpusParam);
+            let args = aws.batch._prepareArguments(params);
+            assert.equal(args, '--participant_label 01 02 03 --n_cpus 4');
+        });
+    });
+});
