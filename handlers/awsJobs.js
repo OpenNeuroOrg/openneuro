a1546e3aba6495167b15b303a941e8999a245799
James Stiehl <james@squishymedia.com>
2017-05-24 16:42:09 -0700

    formatting results to preserve folder structure

--- handlers/awsJobs.js
+++ handlers/awsJobs.js
@@ -273,10 +273,85 @@ let handlers = {
                                     results.push(result);
                                 }
                             });
+///////////////////this could use some cleanup but works fro now//////////////////////
+                            let formattedResults = [];
+                            let resultStore = {};
+
+
+                            let nestResultsByPath = (array, store) => {
+                                let parent = store[array[0]];
+                                let path = parent.dirPath;
+
+                                let checkChildren = (childrenArray, path) => {
+                                    return childrenArray.find((child) => {
+                                        return child.dirPath === path;
+                                    });
+                                }
+
+                                array.forEach((level, k) => {
+                                    //right now setting up top level before passing in store. can probably change this
+                                    if(k === 0) {
+                                        parent._id = k;
+                                        parent.name = level;
+                                        return;
+                                    }
+
+                                    path = path + level + (k === array.length-1 ? '': '/'); //last element in array is the filename
+
+                                    let child = checkChildren(parent.children, path);
+                                    if(child) {
+                                        parent = child;
+                                    } else {
+                                        child = {
+                                            dirPath: path,
+                                            children: [],
+                                            type: 'folder',
+                                            parentId: k-1,
+                                            name: level,
+                                            _id: k
+                                        };
+                                        if(k === array.length -1){
+                                            delete child.children;
+                                            child.type = 'file';
+                                            child.path = params.Bucket + '/' + s3Prefix + path;
+                                        }
+                                        parent.children.push(child);
+                                        parent = child;
+                                    }
+                                });
+
+                                return store;
+                            }
+                            results.forEach((result, index) => {
+                                let pathArray = result.path.split('/').slice(3);
+                                if(pathArray.length === 1) {
+                                    resultStore[pathArray[0]] = {
+                                        type: 'file',
+                                        dirPath: pathArray[0],
+                                        name: pathArray[0],
+                                        path: result.path
+                                    };
+                                } else {
+                                    if(!resultStore[pathArray[0]]) {
+                                        resultStore[pathArray[0]] = {
+                                            type: 'folder',
+                                            children: [],
+                                            name: pathArray[0],
+                                            dirPath: pathArray[0] + '/'
+                                        };
+                                    }
+                                    nestResultsByPath(pathArray, resultStore);
+                                }
+                            });
+
+                            Object.keys(resultStore).forEach((result) => {
+                                formattedResults.push(resultStore[result])
+                            });
+///////////////////////////////////////////////////////////////////////////////////////////
                             c.crn.jobs.updateOne({_id: ObjectID(jobId)}, {
                                 $set:{
                                     'analysis.status': finalStatus,
-                                    results: results,
+                                    results: formattedResults,
                                     'analysis.logstreams': logStreamNames
                                 }
                             });
