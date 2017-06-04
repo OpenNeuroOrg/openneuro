2a9a062808ffdd35d5c4f343d1e71bd1cbafdd0a
Nell Hardcastle <nell@squishymedia.com>
2017-06-04 14:38:41 -0700

    Break the logs handler out into reusable functions.

--- handlers/awsJobs.js
+++ handlers/awsJobs.js
@@ -294,47 +294,12 @@ let handlers = {
     getJobLogs (req, res, next) {
         let jobId = req.params.jobId; //this will be the mongoId for a given analysis
 
-        //Recursive function to snag all logs from a logstream
-        let logsFunc = (params, logs, callback) => {
-            let logStreamName = params.logStreamName;
-            aws.cloudwatch.sdk.getLogEvents(params, (err, data)=> {
-                if(err) {return callback(err);}
-                //Cloudwatch returns a token even if there are no events. That is why checking events length
-                if((data.events && data.events.length > 0) && data.nextForwardToken) {
-                    if(!logs[logStreamName]) {
-                        logs[logStreamName] = [];
-                    }
-                    logs[logStreamName] = logs[logStreamName].concat(data.events);
-                    params.nextToken = data.nextForwardToken;
-                    if(params.startFromHead) {
-                        delete params.startFromHead; //only necessary on first call I think.
-                    }
-                    logsFunc(params, logs, callback);
-                } else {
-                    callback();
-                }
-            });
-        };
-
-        c.crn.jobs.findOne({_id: ObjectID(jobId)}, {}, (err, job) => {
-            if(err) {next(err);}
-            let logStreamNames = job.analysis.logstreams || [];
-            let logs = {};
-            //cloudwatch log events requires knowing jobId and taskArn(s)
-            // taskArns are available on job which we can access with a describeJobs call to batch
-            async.eachSeries(logStreamNames, (logStreamName, cb) => {
-                // this currently works to grab the latest logs for a job. Need to update to get all logs for a job using next tokens
-                // however there is a bug that will require a little more work to make this happen. https://forums.aws.amazon.com/thread.jspa?threadID=251240&tstart=0
-                let params = {
-                    logGroupName: config.aws.cloudwatchlogs.logGroupName,
-                    logStreamName: logStreamName,
-                    startFromHead: true
-                };
-                logsFunc(params, logs, cb);
-            }, (err) =>{
-                if(err) {return next(err);}
+        aws.cloudwatch.getLogsByJobId(jobId, (err, logs) => {
+            if (err) {
+                return next(err);
+            } else {
                 res.send(logs);
-            });
+            }
         });
     },
 
--- libs/aws/cloudwatchlogs.js
+++ libs/aws/cloudwatchlogs.js
@@ -1,10 +1,60 @@
 /*eslint no-console: ["error", { allow: ["log"] }] */
+import mongo from '../../libs/mongo';
+import {ObjectID} from 'mongodb';
+import async from 'async';
+import config from '../../config';
+
+let c = mongo.collections;
 
 export default (aws) => {
 
     const cloudwatchlogs = new aws.CloudWatchLogs();
 
     return {
-        sdk: cloudwatchlogs
+        sdk: cloudwatchlogs,
+
+        getLogsByStreamName(params, logs, callback) {
+            //Recursive function to snag all logs from a logstream
+            let logStreamName = params.logStreamName;
+            this.sdk.getLogEvents(params, (err, data)=> {
+                if(err) {return callback(err);}
+                //Cloudwatch returns a token even if there are no events. That is why checking events length
+                if((data.events && data.events.length > 0) && data.nextForwardToken) {
+                    if(!logs[logStreamName]) {
+                        logs[logStreamName] = [];
+                    }
+                    logs[logStreamName] = logs[logStreamName].concat(data.events);
+                    params.nextToken = data.nextForwardToken;
+                    if(params.startFromHead) {
+                        delete params.startFromHead; //only necessary on first call I think.
+                    }
+                    this.getLogsByStreamName(params, logs, callback);
+                } else {
+                    callback();
+                }
+            });
+        },
+
+        getLogsByJobId(jobId, callback) {
+            c.crn.jobs.findOne({_id: ObjectID(jobId)}, {}, (err, job) => {
+                if(err) {next(err);}
+                let logStreamNames = job.analysis.logstreams || [];
+                let logs = {};
+                //cloudwatch log events requires knowing jobId and taskArn(s)
+                // taskArns are available on job which we can access with a describeJobs call to batch
+                async.eachSeries(logStreamNames, (logStreamName, cb) => {
+                    // this currently works to grab the latest logs for a job. Need to update to get all logs for a job using next tokens
+                    // however there is a bug that will require a little more work to make this happen. https://forums.aws.amazon.com/thread.jspa?threadID=251240&tstart=0
+                    let params = {
+                        logGroupName: config.aws.cloudwatchlogs.logGroupName,
+                        logStreamName: logStreamName,
+                        startFromHead: true
+                    };
+                    this.getLogsByStreamName(params, logs, cb);
+                }, (err) => {
+                    callback(err, logs);
+                });
+            });
+        }
     };
 };
