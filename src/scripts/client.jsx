11b06db5c9078593002d0fe7b045494f62a8416f
James Stiehl <james@squishymedia.com>
2017-06-09 07:53:00 -0700

    google analytics

--- src/scripts/client.jsx
+++ src/scripts/client.jsx
@@ -6,6 +6,15 @@ import Router           from 'react-router';
 import routes           from './routes.jsx';
 import RouterContainer  from './utils/router-container';
 
+//Google analytics
+import ReactGA          from 'react-ga';
+ReactGA.initialize('UA-100754266-1');
+
+// Router.HistoryLocation.listen((location)=> {
+//     ReactGA.ga('send', 'pageview', location.pathname);
+// });
+
+console.log(Router.HistoryLocation);
 // intialize router -----------------------------------------------------
 
 let router = Router.create({routes: routes, location: Router.HistoryLocation});
