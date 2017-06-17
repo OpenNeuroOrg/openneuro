3f4d923ec527e6655997ca0a4e375389ec213658
Nell Hardcastle <nell@squishymedia.com>
2017-06-16 23:30:59 -0700

    Format front page app metadata with markdown (like the dataset pages).

--- src/scripts/front-page/front-page.pipelines.jsx
+++ src/scripts/front-page/front-page.pipelines.jsx
@@ -11,6 +11,7 @@ import {Link}      from 'react-router';
 import {Panel}     from 'react-bootstrap';
 import pluralize   from 'pluralize';
 import Spinner     from '../common/partials/spinner.jsx';
+import markdown    from '../utils/markdown';
 
 // component setup ----------------------------------------------------
 
@@ -99,6 +100,25 @@ let Pipelines = React.createClass({
 
     _pipelineDetail(pipeline) {
         let longDescription = pipeline.descriptions;
+        let description = longDescription.description ? markdown.format(longDescription.description) : '';
+        let acknowledgments = '';
+        let support = '';
+        if (longDescription.acknowledgments) {
+            acknowledgments = (
+                <span>
+                    <h4>Acknowledgments</h4>
+                    <div dangerouslySetInnerHTML={markdown.format(longDescription.acknowledgments)}/>
+                </span>
+            );
+        }
+        if (longDescription.support) {
+            support = (
+                <span>
+                    <h4>Support</h4>
+                    <div dangerouslySetInnerHTML={markdown.format(longDescription.support)}/>
+                </span>
+            );
+        }
         return (
             <div className="selected-pipeline fade-in">
                 <div className="container slide-in-down">
@@ -107,11 +127,9 @@ let Pipelines = React.createClass({
                         <div className="col-sm-6 mate-slide">
                             <a href="#" className="close-selected" onClick={FPActions.selectPipeline.bind(null, '')}>X CLOSE</a>
                             <h2>{pipeline.jobDefinitionName}</h2>
-                            <p>{longDescription.description}</p>
-                            <h4>Acknowledgments</h4>
-                            <p>{longDescription.acknowledgments}</p>
-                            <h4>Support</h4>
-                            <p><a href={'http://' + longDescription.support} target="_blank">{longDescription.support}</a></p>
+                            <div dangerouslySetInnerHTML={description}/>
+                            {acknowledgments}
+                            {support}
                         </div>
                         {this._exampleResults()}
                     </div>
