18c12491b372832b67b0040ea85306d6cc9e2e4b
Nell Hardcastle <nell@squishymedia.com>
2017-07-10 11:50:40 -0700

    Add a queue library for connecting calls to node-resque

---
+++ libs/queue/index.js
@@ -0,0 +1,19 @@
+import {redis} from '../redis';
+import NR from 'node-resque';
+import tasks from './tasks.js';
+
+export default {
+    queue: null,
+    connect(callback) {
+        if (!this.queue) {
+            if (!redis) {
+                throw 'Redis must be connected to connect a queue.';
+            }
+            this.queue = new NR.queue({connection: {redis: redis}}, tasks),
+            this.queue.connect();
+            callback(this.queue);
+        } else {
+            callback(this.queue);
+        }
+    }
+}
---
+++ libs/queue/tasks.js
@@ -0,0 +1,17 @@
+import aws from '../aws';
+
+export default {
+    uploadSnapshot: {
+        plugins: [ 'jobLock', 'retry' ],
+        pluginOptions: {
+            jobLock: {},
+            retry: {
+                retryLimit: 3,
+                retryDelay: (1000 * 5),
+            }
+        },
+        perform: function(snapshotHash, callback){
+            aws.s3.uploadSnapshot(snapshotHash, callback);
+        },
+    }
+}
