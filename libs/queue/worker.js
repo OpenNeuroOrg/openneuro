a30015112a5526f39f14494324e4d1713bf52aa6
Nell Hardcastle <nell@squishymedia.com>
2017-07-10 11:51:50 -0700

    Add a node-resque worker.

---
+++ libs/queue/worker.js
@@ -0,0 +1,22 @@
+import os from 'os';
+import redis from '../redis';
+import config from '../../config';
+import NR from 'node-resque';
+import tasks from './tasks';
+
+redis.connect(config.redis, (redis) => {
+    let workerConfig = {
+        connection: {redis: redis},
+        looping: true,
+        timeout: 5000,
+        queues: ['*'],
+        name: os.hostname() + ":" + process.pid
+    }
+
+    let worker = new NR.worker(workerConfig, tasks);
+
+    worker.connect(() => {
+        worker.workerCleanup();
+        worker.start();
+    });
+});
