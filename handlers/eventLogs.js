e4d77a1b4733f2b2f458c711df535f38fbbf26d1
James Stiehl <james@squishymedia.com>
2017-07-11 17:01:47 -0700

    fixing lint errors

--- handlers/eventLogs.js
+++ handlers/eventLogs.js
@@ -12,11 +12,11 @@ let events = Object.keys(config.events);
  */
 let handlers = {
     getEventLogs (req, res, next) {
-        c.crn.logs.find({type:{$in: events}}, {"sort" : [['date', 'desc']]}).toArray((err, logs)=> {
+        c.crn.logs.find({type:{$in: events}}, {sort : [['date', 'desc']]}).toArray((err, logs)=> {
             if(err) return next(err);
             res.send(logs);
         });
     }
-}
+};
 
 export default handlers;
