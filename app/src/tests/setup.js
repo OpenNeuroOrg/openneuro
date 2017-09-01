2fab3d13d4abbaf5e1e0ae10832a27004233997c
Nell Hardcastle <nell@squishymedia.com>
2017-08-31 20:30:54 -0700

    Fix linting issues by removing unused console warning test override.

--- app/src/tests/setup.js
+++ app/src/tests/setup.js
@@ -6,8 +6,3 @@ global.React = React;
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
-
-// Fail tests on any warning
-console.error = message => {
-   throw new Error(message);
-};
