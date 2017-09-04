2d5ccef1072413a37d1183978b7a0d69fe90cd5a
Nell Hardcastle <nell@squishymedia.com>
2017-09-03 19:09:41 -0700

    Add tests for happybrowser behavior and a UA override to simplify tests.

---
+++ app/src/scripts/common/partials/__tests__/happybrowser.spec.jsx
@@ -0,0 +1,39 @@
+import Happybrowser from '../happybrowser';
+
+const chrome = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36';
+const chromium = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/60.0.3112.113 Chrome/60.0.3112.113 Safari/537.36';
+const ie = 'Mozilla/5.0 (Windows; U; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)';
+const firefox = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0';
+
+describe('common/partials/happybrowser', () => {
+    it('renders successfully', () => {
+        const wrapper = shallow(
+            <Happybrowser ua={chrome} />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+    it('is hidden in Chrome', () => {
+        const wrapper = shallow(
+            <Happybrowser ua={chrome} />
+        );
+        expect(wrapper.hasClass('hidden')).toBe(true);
+    });
+    it('is hidden in Chromium', () => {
+        const wrapper = shallow(
+            <Happybrowser ua={chromium} />
+        );
+        expect(wrapper.hasClass('hidden')).toBe(true);
+    });
+    it('is visible in IE', () => {
+        const wrapper = shallow(
+            <Happybrowser ua={ie} />
+        );
+        expect(wrapper.hasClass('hidden')).toBe(false);
+    });
+    it('is visible in Firefox', () => {
+        const wrapper = shallow(
+            <Happybrowser ua={firefox} />
+        );
+        expect(wrapper.hasClass('hidden')).toBe(false);
+    });
+});
