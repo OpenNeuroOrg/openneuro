0de2c5637ace54759400ef390a8a4ee438385a57
Nell Hardcastle <nell@squishymedia.com>
2017-09-07 16:01:04 -0700

    Additional tests for correct validation output.
    Checks for invalid flag or undefined inputs in more situations for #32.

--- app/src/scripts/dataset/__tests__/dataset.validation.spec.jsx
+++ app/src/scripts/dataset/__tests__/dataset.validation.spec.jsx
@@ -25,16 +25,42 @@ describe('dataset/Validation', () => {
         );
         expect(wrapper.html()).toBe(null);
     });
-    it('renders when the dataset is marked invalid', () => {
+    it('invalid dataset is not super-valid', () => {
         const wrapper = shallow(
             <Validation {...defProps} invalid={true} />
         );
-        expect(wrapper).toMatchSnapshot();
+        const div = wrapper.find('Panel > div.super-valid');
+        expect(div).toHaveLength(0);
+    });
+    it('invalid dataset panel span has class ds-danger and is "Invalid"', () => {
+        const validation = new Validation();
+        const header = shallow(validation._header([], [], true));
+        const span = header.find('span.ds-danger');
+        expect(span).toHaveLength(1);
+        expect(span.text()).toBe('Invalid');
     });
     it('returns the correct message on invalid datasets', () => {
         const wrapper = shallow(
             <Validation {...defProps} invalid={true} />
         );
         expect(wrapper.contains('This does not appear to be a BIDS dataset.')).toBe(true);
+    });
+    it('renders with default props when errors prop is undefined', () => {
+        const wrapper = shallow(
+            <Validation {...defProps} errors={undefined} />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+    it('renders with default props when warnings prop is undefined', () => {
+        const wrapper = shallow(
+            <Validation {...defProps} warnings={undefined} />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+    it('renders as invalid when errors prop is undefined', () => {
+        const wrapper = shallow(
+            <Validation {...defProps} errors={undefined} invalid={true} />
+        );
+        expect(wrapper.find('Panel > div').text()).toBe('This does not appear to be a BIDS dataset. ');
     });
 });
