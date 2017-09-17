6295374c8c5b99ac49220214b376e5486fb93145
Nell Hardcastle <nell@squishymedia.com>
2017-09-17 12:41:40 -0700

    Add some pagination to eventLogs queries.

--- server/handlers/eventLogs.js
+++ server/handlers/eventLogs.js
@@ -12,10 +12,26 @@ let events = Object.keys(config.events);
  */
 let handlers = {
     getEventLogs (req, res, next) {
-        c.crn.logs.find({type:{$in: events}}, {sort : [['date', 'desc']]}).toArray((err, logs)=> {
-            if(err) return next(err);
-            res.send(logs);
-        });
+        // This stuff could be a middleware?
+        let limit = 30;
+        let skip = 0;
+        let reqLimit = parseInt(req.query.limit);
+        let reqSkip = parseInt(req.query.skip);
+        if (!isNaN(reqLimit) && reqLimit < limit) {
+            limit = req.query.limit;
+        }
+        if (!isNaN(reqSkip) && reqSkip) {
+            skip = reqSkip;
+        }
+        c.crn.logs.find({type:{$in: events}},
+            {
+                sort : [['date', 'desc']],
+                limit: limit,
+                skip: skip
+            }).toArray((err, logs) => {
+                if(err) return next(err);
+                res.send(logs);
+            });
     }
 };
 
