c944fb30cd6f4d24b345719183f702fc33f99f20
Nell Hardcastle <nell@squishymedia.com>
2017-09-19 13:26:31 -0700

    Run all tests in UTC +5:45 to prevent timezone errors in snapshots.

--- app/jestsetup.js
+++ app/jestsetup.js
@@ -1,6 +1,10 @@
 // Make Enzyme functions available in all test files without importing
 import React from 'react';
 import { shallow, render, mount } from 'enzyme';
+import moment from 'moment-timezone';
+
+// Run all tests in virtual Katmandu (UTC +05:45)
+moment.tz.setDefault('Asia/Katmandu');
 
 global.React = React;
 global.shallow = shallow;
