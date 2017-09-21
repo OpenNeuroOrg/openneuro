d22c6d749dd0a36709a8eee8703f93f3944443ce
Nell Hardcastle <nell@squishymedia.com>
2017-09-20 18:42:58 -0700

    Update client.jsx to use the new analytics wrapper and v4 components.

--- app/src/scripts/client.jsx
+++ app/src/scripts/client.jsx
@@ -2,25 +2,11 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Router from 'react-router'
-import routes from './routes.jsx'
-import RouterContainer from './utils/router-container'
-import config from '../../config'
+import { Route } from 'react-router'
+import Routes from './routes.jsx'
+import analyticsWrapper from './utils/analytics.js'
 
-//Google analytics
-import ReactGA from 'react-ga'
-ReactGA.initialize(config.analytics.trackingId)
-
-Router.HistoryLocation.addChangeListener(e => {
-  let path = e.path
-  ReactGA.ga('send', 'pageview', path)
-})
-// intialize router -----------------------------------------------------
-
-let router = Router.create({ routes: routes, location: Router.HistoryLocation })
-
-RouterContainer.set(router)
-
-Router.run(routes, Router.HistoryLocation, function(Handler) {
-  ReactDOM.render(<Handler />, document.getElementById('main'))
-})
+ReactDOM.render(
+  <Route component={analyticsWrapper(Routes)} />,
+  document.getElementById('main'),
+)
