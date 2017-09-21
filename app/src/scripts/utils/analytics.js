a42a0416cecf7eb545d194e97fc48023197a82ec
Nell Hardcastle <nell@squishymedia.com>
2017-09-20 18:40:10 -0700

    Add a Google Analytics wrapper that works with react-router v4

---
+++ app/src/scripts/utils/analytics.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import GoogleAnalytics from 'react-ga'
+import config from '../../../config'
+
+GoogleAnalytics.initialize(config.analytics.trackingId)
+
+const analyticsWrapper = (WrappedComponent, options = {}) => {
+  const trackPage = page => {
+    GoogleAnalytics.set({
+      page,
+      ...options,
+    })
+    GoogleAnalytics.pageview(page)
+  }
+
+  const HOC = class extends Component {
+    componentDidMount() {
+      const page = this.props.location.pathname
+      trackPage(page)
+    }
+
+    componentWillReceiveProps(nextProps) {
+      const currentPage = this.props.location.pathname
+      const nextPage = nextProps.location.pathname
+
+      if (currentPage !== nextPage) {
+        trackPage(nextPage)
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />
+    }
+  }
+
+  return HOC
+}
+
+export default analyticsWrapper
