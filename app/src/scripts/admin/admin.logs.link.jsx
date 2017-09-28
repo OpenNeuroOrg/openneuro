77dac7ca2b89d1e9aefcfcb25e2a5f6a846edfbe
Nell Hardcastle <nell@dev-nell.com>
2017-09-27 20:37:03 -0700

    Don't link to missing datasetIds in event logs (#116)
    * Move log links to a component responsible for handling any linkable log entry.
    * Handle special case for logs affected by #102

---
+++ app/src/scripts/admin/admin.logs.link.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const LogLink = ({ log }) => {
+  // datasetId check is for bugged logs from #102
+  if ('job' in log.data && 'datasetId' in log.data.job) {
+    // Jobs are always run against snapshots, so we link to a snapshot + job ref for those
+    const job = log.data.job
+    return (
+      <Link
+        to={'snapshot'}
+        params={{ datasetId: job.datasetId, snapshotId: job.snapshotId }}
+        query={{
+          app: job.appLabel,
+          version: job.appVersion,
+          job: job.jobId,
+        }}>
+        {job.datasetLabel} <br /> {job.appLabel}:{job.appVersion}
+      </Link>
+    )
+  } else if ('dataset' in log.data) {
+    const dataset = log.data.dataset
+    return (
+      <Link to={'dataset'} params={{ datasetId: dataset.datasetId }}>
+        {dataset.datasetLabel}
+      </Link>
+    )
+  } else {
+    // No linkable objects
+    return <span />
+  }
+}
+
+LogLink.propTypes = {
+  log: React.PropTypes.object,
+}
+
+export default LogLink
