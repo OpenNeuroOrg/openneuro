023547c22b1ed464db1777b62c806c82c8245e12
Nell Hardcastle <nell@squishymedia.com>
2017-09-29 13:19:47 -0700

    Remove extra navbar.

--- app/src/scripts/routes.jsx
+++ app/src/scripts/routes.jsx
@@ -5,7 +5,6 @@ import { Route, Switch } from 'react-router-dom'
 import requireAuth from './utils/requireAuth'
 
 // views
-import NavBar from './nav/navbar.jsx'
 import FrontPage from './front-page/front-page.jsx'
 import Admin from './admin/admin.jsx'
 import { Dashboard, PublicDashboard } from './dashboard/dashboard.jsx'
@@ -15,39 +14,36 @@ import Faq from './faq/faq.jsx'
 // routes ----------------------------------------------------------------
 
 const appRoutes = () => (
-  <div>
-    <Route component={NavBar} />
-    <Switch>
-      <Route name="front-page" exact path="/" component={FrontPage} />
-      <Route name="faq" exact path="/faq" component={Faq} />
-      <Route
-        name="dashboard"
-        exact
-        path="/dashboard"
-        component={requireAuth(Dashboard)}
-      />
-      <Route
-        name="admin"
-        exact
-        path="/admin"
-        component={requireAuth(Admin, 'admin')}
-      />
-      <Route name="dashboard" path="/dashboard" component={Dashboard} />
-      <Route name="public" path="/public" component={PublicDashboard} />
-      <Route
-        name="dataset"
-        exact
-        path="/datasets/:datasetId"
-        component={Dataset}
-      />
-      <Route
-        name="snapshot"
-        exact
-        path="/datasets/:datasetId/versions/:snapshotId"
-        component={Dataset}
-      />
-    </Switch>
-  </div>
+  <Switch>
+    <Route name="front-page" exact path="/" component={FrontPage} />
+    <Route name="faq" exact path="/faq" component={Faq} />
+    <Route
+      name="dashboard"
+      exact
+      path="/dashboard"
+      component={requireAuth(Dashboard)}
+    />
+    <Route
+      name="admin"
+      exact
+      path="/admin"
+      component={requireAuth(Admin, 'admin')}
+    />
+    <Route name="dashboard" path="/dashboard" component={Dashboard} />
+    <Route name="public" path="/public" component={PublicDashboard} />
+    <Route
+      name="dataset"
+      exact
+      path="/datasets/:datasetId"
+      component={Dataset}
+    />
+    <Route
+      name="snapshot"
+      exact
+      path="/datasets/:datasetId/versions/:snapshotId"
+      component={Dataset}
+    />
+  </Switch>
 )
 
 export default appRoutes
