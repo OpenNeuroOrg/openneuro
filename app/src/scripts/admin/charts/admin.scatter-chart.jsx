4ecc150639fc55d9d867b5c8a48e6c89fa852333
Xia Amendolara <xia@squishymedia.com>
2017-10-31 15:04:45 -0700

    add scatter plot with styling

---
+++ app/src/scripts/admin/charts/admin.scatter-chart.jsx
@@ -0,0 +1,54 @@
+// dependencies ----------------------------------------------------------------------
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { VictoryChart } from 'victory'
+import { VictoryScatter } from 'victory'
+import { VictoryBar } from 'victory'
+import { VictoryAxis } from 'victory'
+
+// Life Cycle ----------------------------------------------------------------------
+
+const Scatter = ({ logs, month, year }) => {
+  let ms = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+  ]
+
+  let data = []
+  Object.entries(logs).forEach(([key, value]) => {
+    let log = value
+    data.push({ x: log.month, y: log.entries.length })
+  })
+
+  return (
+    <div>
+      <div>
+        <VictoryChart>
+          <VictoryAxis
+            style={{ tickLabels: { fontSize: 15 } }}
+            tickValues={ms}
+            tickFormat={ms}
+          />
+          <VictoryScatter
+            style={{ data: { fill: '#24c85e' } }}
+            data={data}
+            labels={datum => datum.y}
+          />
+        </VictoryChart>
+      </div>
+    </div>
+  )
+}
+
+export default Scatter
