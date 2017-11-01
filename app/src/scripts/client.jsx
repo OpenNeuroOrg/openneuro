7d5f7958868268d962ea8259424156f73b0cb0c7
Nell Hardcastle <nell@squishymedia.com>
2017-11-01 12:27:01 -0700

    Add opbeat initialization and router integration.

--- app/src/scripts/client.jsx
+++ app/src/scripts/client.jsx
@@ -1,14 +1,22 @@
 // dependencies ---------------------------------------------------------
-
+import initOpbeat from 'opbeat-react'
+import { wrapRouter } from 'opbeat-react'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Index from './index.jsx'
 import analyticsWrapper from './utils/analytics.js'
 
+initOpbeat({
+  orgId: 'bafe9710d0924564ae85e865c4861460',
+  appId: 'e7bc74804e'
+})
+
+const OpbeatRouter = wrapRouter(Router)
+
 ReactDOM.render(
-  <Router>
+  <OpbeatRouter>
     <Route component={analyticsWrapper(Index)} />
-  </Router>,
+  </OpbeatRouter>,
   document.getElementById('main'),
 )
