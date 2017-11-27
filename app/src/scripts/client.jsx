5c33e6c981d48a31b0eaee64640c8058d573098d
Nell Hardcastle <nell@squishymedia.com>
2017-11-27 12:06:44 -0800

    Configure Sentry with git and environment information.

--- app/src/scripts/client.jsx
+++ app/src/scripts/client.jsx
@@ -7,8 +7,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Index from './index.jsx'
 import analyticsWrapper from './utils/analytics.js'
 
+const ravenConfig = {
+  release: __GIT_HASH__,
+  tags: { branch: __GIT_BRANCH__ },
+  environment: process.env.AWS_BATCH_QUEUE,
+}
+
 Raven.config(
   'https://ba0c58863b3e40a2a412132bfd2711ea@sentry.io/251076',
+  ravenConfig,
 ).install()
 
 ReactDOM.render(
