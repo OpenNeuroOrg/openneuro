b41d059ecdad99fb6486348b0bb5b32b71ce0327
Nell Hardcastle <nell@squishymedia.com>
2017-11-27 11:29:03 -0800

    Enable Sentry / Raven support for tracking client errors.

--- app/src/scripts/client.jsx
+++ app/src/scripts/client.jsx
@@ -1,10 +1,15 @@
 // dependencies ---------------------------------------------------------
 import 'url-search-params-polyfill'
+import Raven from 'raven-js'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Index from './index.jsx'
 import analyticsWrapper from './utils/analytics.js'
+
+Raven.config(
+  'https://ba0c58863b3e40a2a412132bfd2711ea@sentry.io/251076',
+).install()
 
 ReactDOM.render(
   <Router>
