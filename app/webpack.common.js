2c79a057a3a2d3b9f3a21fb72b93783788e7c526
Teal Hobson-Lowther <teal@squishymedia.com>
2017-12-13 14:19:49 -0800

    add a .prettierignore to stop lint from breaking webpack.* code

--- app/webpack.common.js
+++ app/webpack.common.js
@@ -8,9 +8,7 @@ const git = require('git-rev-sync')
 
 const env = {
   CRN_SERVER_URL: JSON.stringify(process.env.CRN_SERVER_URL),
-  SCITRAN_AUTH_GOOGLE_CLIENT_ID: JSON.stringify(
-    process.env.SCITRAN_AUTH_GOOGLE_CLIENT_ID,
-  ),
+  SCITRAN_AUTH_GOOGLE_CLIENT_ID: JSON.stringify(process.env.SCITRAN_AUTH_GOOGLE_CLIENT_ID),
   GOOGLE_TRACKING_ID: JSON.stringify(process.env.GOOGLE_TRACKING_ID),
   ORCID_AUTH_CLIENT_ID: JSON.stringify(process.env.ORCID_AUTH_CLIENT_ID),
   ORCID_AUTH_REDIRECT_URI: JSON.stringify(process.env.ORCID_AUTH_REDIRECT_URI),
