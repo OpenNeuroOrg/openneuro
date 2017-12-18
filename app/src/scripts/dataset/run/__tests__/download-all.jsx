8c9f126cffae81593fe13713898672b916d4a61a
Nell Hardcastle <nell@squishymedia.com>
2017-12-18 15:16:50 -0800

    Add a test and stub for DownloadS3 component to display S3 instructions.

--- app/src/scripts/dataset/run/__tests__/download-all.jsx
+++ app/src/scripts/dataset/run/__tests__/download-all.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import DownloadAll from '../download-all.jsx'
 import WarnButton from '../../../common/forms/warn-button.jsx'
+import DownloadS3 from '../download-s3.jsx'
 
 const dsId = '59c16458c650bd001111f010'
 const sId = '3030313030332d3030303031'
@@ -16,6 +17,13 @@ describe('dataset/run/DownloadAll', () => {
     expect(
       shallow(<DownloadAll datasetId={dsId} snapshotId={sId} />).find(
         WarnButton,
+      ).length,
+    ).toBe(1)
+  })
+  it('should render a DownloadS3 component for downloading via AWS cli', () => {
+    expect(
+      shallow(<DownloadAll datasetId={dsId} snapshotId={sId} />).find(
+        DownloadS3,
       ).length,
     ).toBe(1)
   })
---
+++ app/src/scripts/dataset/run/download-s3.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DownloadS3 = () => {
+  return <div>Download via S3</div>
+}
+
+export default DownloadS3
