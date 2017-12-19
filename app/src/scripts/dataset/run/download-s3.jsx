25a8f85b08513280754fe6d007bdfdc67bcdf794
Nell Hardcastle <nell@squishymedia.com>
2017-12-18 16:54:01 -0800

    Implement opening the S3 results link in another tab.

--- app/src/scripts/dataset/run/download-s3.jsx
+++ app/src/scripts/dataset/run/download-s3.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import config from '../../../../config'
 
+const tryOpenS3 = url => event => {
+  event.preventDefault()
+  window.open(url, 'S3')
+}
+
 const DownloadS3 = ({ datasetHash, analysisId }) => {
   const bucket = config.aws.s3.analysisBucket
   const url = 's3://' + bucket + '/' + datasetHash + '/' + analysisId
   return (
     <span>
-      <a className="btn-warn-component" href={url}>
+      <a className="btn-warn-component" href={url} onClick={tryOpenS3(url)}>
         <i className="fa fa-link" aria-hidden="true" /> S3 URL
       </a>
     </span>
