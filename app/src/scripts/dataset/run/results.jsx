0f795fed2a34ce42a35675f9a3e7ed4659897089
Nell Hardcastle <nell@squishymedia.com>
2017-12-19 15:22:34 -0800

    Fix no results rendering bug from refactor.

--- app/src/scripts/dataset/run/results.jsx
+++ app/src/scripts/dataset/run/results.jsx
@@ -8,41 +8,53 @@ import markdown from '../../utils/markdown'
 
 const JobResults = ({ run, acknowledgements, displayFile, toggleFolder }) => {
   const type = 'results'
-  return (
-    <Accordion accordion className="results">
-      <Panel className="fade-in" header={type} key={run._id} eventKey={run._id}>
-        <div className="app-acknowledgements">
-          <label>Acknowledgements</label>
-          <div
-            className="markdown"
-            dangerouslySetInnerHTML={markdown.format(acknowledgements)}
-          />
-        </div>
-        <hr />
-        <DownloadAll run={run} />
-        <div className="file-structure fade-in panel-group">
-          <div className="panel panel-default">
-            <div className="panel-collapse" aria-expanded="false">
-              <div className="panel-body">
-                <FileTree
-                  tree={run[type]}
-                  treeId={run._id}
-                  editable={false}
-                  getFileDownloadTicket={actions.getResultDownloadTicket.bind(
-                    this,
-                    run.snapshotId,
-                    run._id,
-                  )}
-                  displayFile={displayFile.bind(this, run.snapshotId, run._id)}
-                  toggleFolder={toggleFolder}
-                />
+  if (run[type] && run[type].length > 0) {
+    return (
+      <Accordion accordion className="results">
+        <Panel
+          className="fade-in"
+          header={type}
+          key={run._id}
+          eventKey={run._id}>
+          <div className="app-acknowledgements">
+            <label>Acknowledgements</label>
+            <div
+              className="markdown"
+              dangerouslySetInnerHTML={markdown.format(acknowledgements)}
+            />
+          </div>
+          <hr />
+          <DownloadAll run={run} />
+          <div className="file-structure fade-in panel-group">
+            <div className="panel panel-default">
+              <div className="panel-collapse" aria-expanded="false">
+                <div className="panel-body">
+                  <FileTree
+                    tree={run[type]}
+                    treeId={run._id}
+                    editable={false}
+                    getFileDownloadTicket={actions.getResultDownloadTicket.bind(
+                      this,
+                      run.snapshotId,
+                      run._id,
+                    )}
+                    displayFile={displayFile.bind(
+                      this,
+                      run.snapshotId,
+                      run._id,
+                    )}
+                    toggleFolder={toggleFolder}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Panel>
-    </Accordion>
-  )
+        </Panel>
+      </Accordion>
+    )
+  } else {
+    return null
+  }
 }
 
 export default JobResults
