3ba70569a577e3367182693d0126b360a4bcd787
Nell Hardcastle <nell@squishymedia.com>
2017-12-18 16:29:18 -0800

    Fix DownloadS3 props and add test for window open call.

--- app/src/scripts/dataset/run/__tests__/download-s3.jsx
+++ app/src/scripts/dataset/run/__tests__/download-s3.jsx
@@ -2,13 +2,36 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import DownloadS3 from '../download-s3.jsx'
 
-const dsId = '59c16458c650bd001111f010'
-const sId = '3030313030332d3030303031'
+const datasetHash = 'dataset-hash'
+const analysisId = 'analysis-id'
+
+// Mock the needed configuration
+jest.mock('../../../../../config', () => {
+  return {
+    aws: {
+      s3: {
+        analysisBucket: 'test-bucket',
+      },
+    },
+  }
+})
 
 describe('dataset/run/DownloadS3', () => {
   it('should render something', () => {
     expect(
-      shallow(<DownloadS3 datasetId={dsId} snapshotId={sId} />),
+      shallow(<DownloadS3 datasetHash={datasetHash} analysisId={analysisId} />),
     ).toMatchSnapshot()
+  })
+  it('opens a window when the download link is clicked', () => {
+    global.open = jest.fn()
+    const wrapper = shallow(
+      <DownloadS3 datasetHash={datasetHash} analysisId={analysisId} />,
+    )
+    const s3Link = wrapper.find('a')
+    s3Link.simulate('click')
+    expect(global.open).toHaveBeenCalledWith(
+      's3://test-bucket/dataset-hash/analysis-id',
+      'S3',
+    )
   })
 })
