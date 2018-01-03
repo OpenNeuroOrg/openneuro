c521f4b488cddb8e3efb131e12952d14c733027c
Teal Hobson-Lowther <teal@squishymedia.com>
2018-01-03 14:43:53 -0800

    update unit tests for job results

--- app/src/scripts/dataset/run/__tests__/results.spec.jsx
+++ app/src/scripts/dataset/run/__tests__/results.spec.jsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme'
 import Results from '../results.jsx'
 
 describe('dataset/run/Results', () => {
-  it('should render something', () => {
+  it('should render something when all job results are in', () => {
     const exampleRun = {
       results: [
         {
@@ -14,6 +14,9 @@ describe('dataset/run/Results', () => {
             'openneuro.outputs/cf02b5bf17df80d056effadccffd65bd/dcedf0c3-4438-466d-9f85-61c75275b355/avg_brain_size.txt',
         },
       ],
+      analysis: {
+        jobs: ['only_one_job'],
+      },
     }
     expect(
       shallow(
@@ -25,6 +28,32 @@ describe('dataset/run/Results', () => {
         />,
       ),
     ).toMatchSnapshot()
+  })
+  it('should render nothing if there are less results than there are jobs', () => {
+    const lessResultsRun = {
+      results: [
+        {
+          type: 'file',
+          dirPath: 'avg_brain_size.txt',
+          name: 'avg_brain_size.txt',
+          path:
+            'openneuro.outputs/cf02b5bf17df80d056effadccffd65bd/dcedf0c3-4438-466d-9f85-61c75275b355/avg_brain_size.txt',
+        },
+      ],
+      analysis: {
+        jobs: ['job_one', 'job_two'],
+      },
+    }
+    expect(
+      shallow(
+        <Results
+          run={lessResultsRun}
+          acknowledgements="acknowledge"
+          displayFile={jest.fn()}
+          toggleFolder={jest.fn()}
+        />,
+      ).getElement(),
+    ).toBe(null)
   })
   it('should render nothing when there are no results', () => {
     const noResultsRun = {
