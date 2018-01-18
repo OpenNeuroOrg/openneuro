ecfb36184b4e3e0bdcbe311adce8d82e2c64e605
Xia Amendolara <xia@squishymedia.com>
2018-01-17 16:14:43 -0800

    add input type file

--- app/src/scripts/common/forms/parameter-types/file.jsx
+++ app/src/scripts/common/forms/parameter-types/file.jsx
@@ -1,3 +1,18 @@
+import React from 'react'
+
 export default () => {
-  return null
+  return (
+    <button
+      className="admin-button"
+      // onClick={this._handleCheck.bind(this, field.id)}
+      key="required">
+      <i className="fa fa-square-o" />
+      // {
+      //   this.props.checked.includes(field.id)
+      //     ? 'fa fa-check-square-o'
+      //     : 'fa fa-square-o'
+      // }
+      required
+    </button>
+  )
 }
