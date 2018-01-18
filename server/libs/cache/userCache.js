07c4f18cb61e0067fdeb7a3201673522a54e23f5
Teal Hobson-Lowther <teal@squishymedia.com>
2018-01-18 10:46:43 -0800

    create keyspace for user cache, strengthen error conditionals

--- server/libs/cache/userCache.js
+++ server/libs/cache/userCache.js
@@ -1,25 +1,40 @@
 import { redis } from '../redis'
 
 export default {
-  get(req, callback) {
-    const userId = req.json.userId
-    redis.get(userId, (err, data) => {
-      if (err) {
-        return callback(err, null)
-      }
-      if (data != null) {
-        const body = JSON.parse(data)
-        const res = { statusCode: 200, body: body }
-        return callback(null, res)
-      } else {
-        return callback()
-      }
-    })
+  keyPrefix: 'scitran:users:',
+
+  getKey(id) {
+    return this.keyPrefix.concat(id)
   },
+
+  get(req, callback) {
+    const userId = req.json ? req.json.userId : null
+    const userKey = userId ? this.getKey(userId) : null
+    if (userKey) {
+      redis.get(userKey, (err, data) => {
+        if (err) {
+          return callback(err, null)
+        }
+        if (data != null) {
+          const body = JSON.parse(data)
+          const res = { statusCode: 200, body: body }
+          return callback(null, res)
+        } else {
+          return callback()
+        }
+      })
+    } else {
+      return callback()
+    }
+  },
+
   store(userData, callback) {
+    const userId = userData && userData._id ? userData._id : null
+    const userKey = userId ? this.getKey(userId) : null
     const data = JSON.stringify(userData)
-    const userId = userData._id
-    redis.set(userId, data)
+    if (userKey) {
+      redis.set(userKey, data)
+    }
     callback()
   },
 }
