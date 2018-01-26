b5e10a9ba42a2028e79dae476bf2b180450e95c6
Nell Hardcastle <nell@squishymedia.com>
2018-01-25 19:27:44 -0800

    Add a basic vendor bundle for react deps used across the application.

--- app/webpack.common.js
+++ app/webpack.common.js
@@ -22,6 +22,7 @@ module.exports = {
   entry: {
     app: './scripts/client.jsx',
     css: './sass/main.scss',
+    vendor: ['react', 'react-dom', 'react-router-dom'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -30,6 +31,10 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+    }),
     new HtmlWebpackPlugin({
       title: 'OpenNeuro',
       template: path.resolve(__dirname, 'src/index.html'),
