98a76a1c010085bf30d2932e815d356a11960615
Xia Amendolara <xia@squishymedia.com>
2018-01-31 15:09:56 -0800

    update logic for scatter, and add conditional rendering for years without successful jobs

--- app/src/scripts/admin/charts/admin.scatter-chart.jsx
+++ app/src/scripts/admin/charts/admin.scatter-chart.jsx
@@ -25,66 +25,68 @@ const Scatter = ({ logs, year, jobs }) => {
     'Dec',
   ]
 
-  // console.log(year)
-
   let data = []
-  // console.log(logs)
-  Object.values(logs.SUCCEEDED[year]).map(job => {
-    let dateArr = job.dateTime.split(' ')
-    let key = dateArr[1]
-    let entries = {}
-    if (!entries[key]) {
-      entries[key] = []
-      entries[key].push(job.dateTime)
-    } else {
-      entries[key].push(job.dateTime)
-    }
-    console.log(entries)
-    data.push({ x: key, y: entries[key].length })
+  let entries = {}
+  let key
+  let dataLength = 0
 
-    // for (let job of log) {
-    // }
-    // console.log(index)
-    // console.log(log[1][year])
-    // let key = log[1][year]
-  })
+  if (logs.SUCCEEDED[year]) {
+    Object.values(logs.SUCCEEDED[year]).map(job => {
+      let dateArr = job.dateTime.split(' ')
+      key = dateArr[1]
+      if (!entries[key]) {
+        entries[key] = []
+        entries[key].push(job.dateTime)
+      } else {
+        entries[key].push(job.dateTime)
+      }
+    })
+    entries[key] ? (dataLength = entries[key].length) : null
+    data.push({ x: key, y: dataLength })
 
-  return (
-    <div>
-      {/* <h3>{year != 'all' ? 'Successful jobs for ' + year : null}</h3> */}
-      <VictoryChart>
-        <VictoryAxis
-          style={{ tickLabels: { fontSize: 15 } }}
-          tickValues={ms}
-          tickFormat={ms}
-        />
-        <VictoryScatter
-          style={{ data: { fill: '#007c92' } }}
-          data={data}
-          labels={datum => datum.y}
-          size={10}
-          onClick={jobs.bind(this)}
-          events={[
-            {
-              target: 'data',
-              eventHandlers: {
-                onClick: () => {
-                  return [
-                    {
-                      target: 'labels',
-                      mutation: props => {
-                        jobs(props.text)
+    // Need to set a default height and width on this div
+    return (
+      <div className="chart-container">
+        <VictoryChart>
+          <VictoryAxis
+            style={{ tickLabels: { fontSize: 15 } }}
+            tickValues={ms}
+            tickFormat={ms}
+          />
+          <VictoryScatter
+            style={{ data: { fill: '#007c92' } }}
+            data={data}
+            labels={datum => datum.y}
+            size={10}
+            onClick={jobs.bind(this)}
+            events={[
+              {
+                target: 'data',
+                eventHandlers: {
+                  onClick: () => {
+                    return [
+                      {
+                        target: 'labels',
+                        mutation: props => {
+                          jobs(props.text)
+                        },
                       },
-                    },
-                  ]
+                    ]
+                  },
                 },
               },
-            },
-          ]}
-        />
-      </VictoryChart>
-    </div>
-  )
+            ]}
+          />
+        </VictoryChart>
+      </div>
+    )
+  } else {
+    return (
+      <div className="chart-container">
+        <p>Sorry, there were not any successful jobs in {year}.</p>
+      </div>
+    )
+  }
 }
 
 export default Scatter
