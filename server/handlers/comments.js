9ffdfbe5cac5e148d1eadafd823c4168f78a9ea8
Teal Hobson-Lowther <teal@squishymedia.com>
2018-02-06 09:51:35 -0800

    add server-side routing for updateComment

--- server/handlers/comments.js
+++ server/handlers/comments.js
@@ -2,7 +2,7 @@ import config from '../config'
 import request from '../libs/request'
 import crypto from 'crypto'
 import mongo from '../libs/mongo'
-import {ObjectID} from 'mongodb'
+import { ObjectID } from 'mongodb'
 
 let c = mongo.collections
 
@@ -23,17 +23,35 @@ export default {
   * ** maybe returns the newly created comment id
   */
   create(req, res, next) {
-    console.log('server/handlers/comments/create with data:', req.body, req.params.datasetId)
     let comment = req.body
-    let datasetId = req.params.datasetId
 
     c.crn.comments.insertOne(comment, (err, response) => {
-      console.log('server err:', err, 'server response:', response)
       if (err) {
         return next(err)
       }
       res.send(response.ops)
     })
+  },
+
+  /**
+  * Update Comment
+  *
+  * Updates an entry in the comments database, 
+  */
+  update(req, res, next) {
+    let comment = req.body
+    let commentId = req.params.commentId
+
+    c.crn.comments.updateOne(
+      { _id: ObjectID(commentId) },
+      { $set: { text: comment.text } },
+      (err) => {
+        if (err) {
+          return next(err)
+        }
+        res.send()
+      },
+    )
   },
 
   /**
@@ -43,37 +61,24 @@ export default {
    * replies to a comment
    */
   delete(req, res, next) {
-    console.log('server/handlers/comments/delete with req: ', req.body, 'and params:', req.params)
     const commentId = req.params.commentId
     const parentId = req.body.parentId
-
-    console.log('deleting an entry in the mongo db with commentId:', commentId, 'and parentId:', parentId)
     // delete the comment in question
-    c.crn.comments.deleteOne(
-      { _id: ObjectID(commentId)},
-      err => {
-        if (err) {
-          return next(err)
-        }
-
-        console.log('successfully deleted one comment...')
-        console.log('deleting children of said comment...')
-        // delete the children of that comment
-        if (parentId) {
-          c.crn.comments.deleteMany(
-            { parentId: commentId},
-            err => {
-              if (err) {
-                return next(err)
-              }
-              console.log('successfully deleted the children of that comment.')
-              return res.send()
-            },
-          )
-        }
-        return res.send()
-      },
-    )
+    c.crn.comments.deleteOne({ _id: ObjectID(commentId) }, err => {
+      if (err) {
+        return next(err)
+      }
+      // delete the children of that comment
+      if (parentId) {
+        c.crn.comments.deleteMany({ parentId: commentId }, err => {
+          if (err) {
+            return next(err)
+          }
+          return res.send()
+        })
+      }
+      return res.send()
+    })
   },
 
   // read ------------------------------------------
@@ -84,7 +89,6 @@ export default {
   * Returns a list of comments that are associated with a dataset
   */
   getComments(req, res, next) {
-      console.log('/server/handlers/comments.js getComments')
     let datasetId = req.params.datasetId
 
     c.crn.comments
