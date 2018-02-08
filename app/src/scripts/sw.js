6aa6fd01e06a64fd8fa061fea977452201ee32b4
Nell Hardcastle <nell@squishymedia.com>
2018-02-08 09:45:50 -0800

    Clean up demo link and restrict special handling to the configured bucket.

--- app/src/scripts/sw.js
+++ app/src/scripts/sw.js
@@ -5,6 +5,7 @@
  */
 import packageJson from '../../package.json'
 import { zipResponse } from './serviceworker/s3'
+import config from '../../config'
 
 const CACHE_NAME = `openneuro-${packageJson.version}`
 const CACHE_PATHS = serviceWorkerOption.assets
@@ -22,8 +23,9 @@ self.addEventListener('fetch', event => {
   // for non-GET requests.
   if (event.request.method === 'GET') {
     const url = new URL(event.request.url)
-    const awsHostname = '.s3.amazonaws.com'
-    if (url.hostname.endsWith(awsHostname)) {
+    const bucket = config.aws.s3.datasetBucket
+    const bucketHostname = `${bucket}.s3.amazonaws.com`
+    if (url.hostname.endsWith(bucketHostname)) {
       const hostname = url.hostname
       const prefix = url.pathname.slice(1)
       event.respondWith(zipResponse(hostname, prefix))
