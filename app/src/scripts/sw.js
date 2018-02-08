a416dfa84021c434c09772565c3c17b19349b374
Nell Hardcastle <nell@squishymedia.com>
2018-02-08 14:34:38 -0800

    Less aggressive service worker caching.

--- app/src/scripts/sw.js
+++ app/src/scripts/sw.js
@@ -33,16 +33,8 @@ self.addEventListener('fetch', event => {
     } else {
       // Respond from cache, then the network
       event.respondWith(
-        caches.open(CACHE_NAME).then(function(cache) {
-          return cache.match(event.request).then(function(response) {
-            return (
-              response ||
-              fetch(event.request).then(function(response) {
-                cache.put(event.request, response.clone())
-                return response
-              })
-            )
-          })
+        caches.match(event.request).then(function(response) {
+          return response || fetch(event.request)
         }),
       )
     }
