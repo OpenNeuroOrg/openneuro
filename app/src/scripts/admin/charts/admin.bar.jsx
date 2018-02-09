b0442ff722ce3c4bef238a43231339f09cfaf9ef
Xia Amendolara <xia@squishymedia.com>
2018-02-09 09:52:28 -0800

    fix broken logic for bar graph

--- app/src/scripts/admin/charts/admin.bar.jsx
+++ app/src/scripts/admin/charts/admin.bar.jsx
@@ -2,64 +2,46 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import {
-  VictoryChart,
-  VictoryScatter,
-  VictoryAxis,
-  VictoryStack,
-  VictoryBar,
-} from 'victory'
+import { VictoryChart, VictoryAxis, VictoryStack, VictoryBar } from 'victory'
 
 // Life Cycle ----------------------------------------------------------------------
 
 const Bar = ({ year, logs, months }) => {
   let ms = months
-  let dataLength = 0
   let key
   // ARRAY AND OBJECT VARS FOR DATA
-  let data = []
-  let total = []
   let failed = []
   let succeeded = []
-  let entries = {}
+  let entries = {
+    failed: [],
+    succeeded: [],
+  }
 
   Object.keys(logs).map(type => {
     if (logs[type][year]) {
       Object.values(logs[type][year]).map(job => {
         let dateArr = job.dateTime.split(' ')
-        let status = job.log.data.job.status
+        let status = job.log.data.job.status.toLowerCase()
         key = dateArr[1]
-        if (!entries[key]) {
-          entries[key] = []
-          entries[key].push({ date: job.dateTime, status: status })
+        if (!entries[status][key]) {
+          entries[status][key] = []
+          entries[status][key].push({ date: job.dateTime, status: status })
         } else {
-          entries[key].push({ date: job.dateTime, status: status })
+          entries[status][key].push({ date: job.dateTime, status: status })
         }
       })
     }
   })
+  Object.keys(entries).map(log => {
+    if (entries[log][key]) {
+      if (log === 'failed') {
+        failed.push({ x: key, y: entries[log][key].length })
+      } else if (log === 'succeeded') {
+        succeeded.push({ x: key, y: entries[log][key].length })
+      }
+    }
+  })
 
-  // if array is not empty
-  if (entries[key]) {
-    dataLength = entries[key].length
-    total.push({ x: key, y: dataLength })
-    // need to get length of job statuses for the month
-    Object.values(entries).map(jobs => {
-      let count = []
-      jobs.map(job => {
-        // Add data to chart
-        if (job.status === 'FAILED') {
-          count.push(job.date)
-          failed.push({ x: key, y: count.length })
-        } else if (job.status === 'SUCCEEDED') {
-          count.push(job.date)
-          succeeded.push({ x: key, y: count.length })
-        }
-      })
-    })
-  }
-
-  console.log(total)
   if (logs.FAILED[year].length || logs.SUCCEEDED[year].length) {
     return (
       <div className="chart-container">
@@ -69,17 +51,11 @@ const Bar = ({ year, logs, months }) => {
             tickValues={ms}
             tickFormat={ms}
           />
-          <VictoryStack colorScale={['#38A171', '#c82424', '#df7600']}>
-            <VictoryBar data={failed} />
+          <VictoryStack colorScale={['#38A171', '#c82424']}>
             <VictoryBar data={succeeded} />
-            <VictoryBar data={total} />
+            <VictoryBar data={failed} />
           </VictoryStack>
         </VictoryChart>
-        <span>
-          <span className="failed">Failed</span>
-          <span className="succeed">Succeeded</span>
-          <span className="total">Total</span>
-        </span>
       </div>
     )
   } else {
@@ -94,6 +70,7 @@ const Bar = ({ year, logs, months }) => {
 Bar.propTypes = {
   logs: PropTypes.object,
   year: PropTypes.node,
+  months: PropTypes.array,
 }
 
 export default Bar
