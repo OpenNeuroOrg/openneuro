a1b41634ce6254922332ada17e6737bfe9c2c0cb
Nell Hardcastle <nell@squishymedia.com>
2018-02-26 10:27:28 -0800

    Add mongodb testing fixtures to server.

---
+++ server/jest-setup.js
@@ -0,0 +1,16 @@
+const MongodbMemoryServer = require('mongodb-memory-server')
+
+const MONGO_DB_NAME = 'jest'
+const mongod = new MongodbMemoryServer.default({
+  instance: {
+    dbName: MONGO_DB_NAME,
+  },
+  binary: {
+    version: '3.2.19',
+  },
+})
+
+module.exports = () => {
+  global.__MONGOD__ = mongod
+  global.__MONGO_DB_NAME__ = MONGO_DB_NAME
+}
---
+++ server/mongo-environment.js
@@ -0,0 +1,28 @@
+const NodeEnvironment = require('jest-environment-node')
+
+class MongoEnvironment extends NodeEnvironment {
+  constructor(config) {
+    super(config)
+  }
+
+  async setup() {
+    console.log('Setup MongoDB Test Environment')
+
+    this.global.__MONGO_URI__ = await global.__MONGOD__.getConnectionString()
+    this.global.__MONGO_DB_NAME__ = global.__MONGO_DB_NAME__
+
+    await super.setup()
+  }
+
+  async teardown() {
+    console.log('Teardown MongoDB Test Environment')
+
+    await super.teardown()
+  }
+
+  runScript(script) {
+    return super.runScript(script)
+  }
+}
+
+module.exports = MongoEnvironment
