986edc46e97005403c37a58bc534748e3e62f6f3
Teal Hobson-Lowther <teal@squishymedia.com>
2018-03-02 14:26:51 -0800

    break dataset and modals into separate routes

--- app/src/scripts/dataset/dataset.routes.jsx
+++ app/src/scripts/dataset/dataset.routes.jsx
@@ -9,111 +9,130 @@ import Subscribe from './tools/subscribe.jsx'
 import UpdateWarn from './dataset.update-warning.jsx'
 import FileEdit from './dataset.file-edit.jsx'
 import FileDisplay from './dataset.file-display.jsx'
+import DatasetLoader from './dataset.dataset-loader.jsx'
+import SnapshotLoader from './dataset.snapshot-loader.jsx'
 
-const DatasetRoutes = () => {
-  return (
-    <Switch>
-      {/* Dataset routes */}
-      <Route
-        name="dataset"
-        exact
-        path="/datasets/:datasetId"
-        component={DatasetContent}
-      />
-      <Route
-        name="snapshot-create"
-        exact
-        path="/datasets/:datasetId/create-snapshot"
-        component={CreateSnapshot}
-      />
-      <Route
-        name="publish"
-        exact
-        path="/datasets/:datasetId/publish"
-        component={Publish}
-      />
-      <Route
-        name="share"
-        exact
-        path="/datasets/:datasetId/share"
-        component={Share}
-      />
-      <Route
-        name="jobs"
-        exact
-        path="/datasets/:datasetId/jobs"
-        component={Jobs}
-      />
-      <Route
-        name="subscribe"
-        exact
-        path="/datasets/:datasetId/subscribe"
-        component={Subscribe}
-      />
-      <Route
-        name="warn"
-        exact
-        path="/datasets/:datasetId/update-warn"
-        component={UpdateWarn}
-      />
-      <Route
-        name="fileEdit"
-        exact
-        path="/datasets/:datasetId/file-edit"
-        component={FileEdit}
-      />
-      <Route
-        name="fileDisplay"
-        exact
-        path="/datasets/:datasetId/file-display"
-        component={FileDisplay}
-      />
+export default class DatasetRoutes extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* Dataset Loader */}
+        <Route
+          name="dataset-loader"
+          path="/datasets/:datasetId"
+          component={DatasetLoader}
+        />
 
-      {/* Snapshot routes */}
-      <Route
-        name="snapshot"
-        exact
-        path="/datasets/:datasetId/versions/:snapshotId"
-        component={DatasetContent}
-      />
-      <Route
-        name="publish"
-        exact
-        path="/datasets/:datasetId/versions/:snapshotId/publish"
-        component={Publish}
-      />
-      <Route
-        name="share"
-        exact
-        path="/datasets/:datasetId/versions/:snapshotId/share"
-        component={Share}
-      />
-      <Route
-        name="jobs"
-        exact
-        path="/datasets/:datasetId/versions/:snapshotId/jobs"
-        component={Jobs}
-      />
-      <Route
-        name="subscribe"
-        exact
-        path="/datasets/:datasetId/versions/:snapshotId/subscribe"
-        component={Subscribe}
-      />
-      <Route
-        name="warn"
-        exact
-        path="/datasets/:datasetId/versions/:snapshotId/update-warn"
-        component={UpdateWarn}
-      />
-      <Route
-        name="fileDisplay"
-        exact
-        path="/datasets/:datasetId/versions/:snapshotId/file-display"
-        component={FileDisplay}
-      />
-    </Switch>
-  )
+        {/* Snapshot Loader */}
+        <Route
+          name="snapshot-loader"
+          path="/datasets/:datasetId/versions/:snapshotId"
+          component={SnapshotLoader}
+        />
+
+        {/* Dataset Route Switch */}
+        <Switch>
+          {/* Dataset routes */}
+          <Route
+            name="dataset"
+            exact
+            path="/datasets/:datasetId"
+            component={DatasetContent}
+          />
+          <Route
+            name="snapshot-create"
+            exact
+            path="/datasets/:datasetId/create-snapshot"
+            component={CreateSnapshot}
+          />
+          <Route
+            name="publish"
+            exact
+            path="/datasets/:datasetId/publish"
+            component={Publish}
+          />
+          <Route
+            name="share"
+            exact
+            path="/datasets/:datasetId/share"
+            component={Share}
+          />
+          <Route
+            name="jobs"
+            exact
+            path="/datasets/:datasetId/jobs"
+            component={Jobs}
+          />
+          <Route
+            name="subscribe"
+            exact
+            path="/datasets/:datasetId/subscribe"
+            component={Subscribe}
+          />
+          <Route
+            name="warn"
+            exact
+            path="/datasets/:datasetId/update-warn"
+            component={UpdateWarn}
+          />
+          <Route
+            name="fileEdit"
+            exact
+            path="/datasets/:datasetId/file-edit"
+            component={FileEdit}
+          />
+          <Route
+            name="fileDisplay"
+            exact
+            path="/datasets/:datasetId/file-display"
+            component={FileDisplay}
+          />
+
+          {/* Snapshot routes */}
+          <Route
+            name="snapshot"
+            exact
+            path="/datasets/:datasetId/versions/:snapshotId"
+            component={DatasetContent}
+          />
+          <Route
+            name="publish"
+            exact
+            path="/datasets/:datasetId/versions/:snapshotId/publish"
+            component={Publish}
+          />
+          <Route
+            name="share"
+            exact
+            path="/datasets/:datasetId/versions/:snapshotId/share"
+            component={Share}
+          />
+          <Route
+            name="jobs"
+            exact
+            path="/datasets/:datasetId/versions/:snapshotId/jobs"
+            component={Jobs}
+          />
+          <Route
+            name="subscribe"
+            exact
+            path="/datasets/:datasetId/versions/:snapshotId/subscribe"
+            component={Subscribe}
+          />
+          <Route
+            name="warn"
+            exact
+            path="/datasets/:datasetId/versions/:snapshotId/update-warn"
+            component={UpdateWarn}
+          />
+          <Route
+            name="fileDisplay"
+            exact
+            path="/datasets/:datasetId/versions/:snapshotId/file-display"
+            component={FileDisplay}
+          />
+        </Switch>
+      </div>
+    )
+  }
 }
-
-export default DatasetRoutes
