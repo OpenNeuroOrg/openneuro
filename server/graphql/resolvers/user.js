abfbbd860b7b18144387a9ee218815ef73c9aa78
Nell Hardcastle <nell@squishymedia.com>
2018-03-05 14:19:32 -0800

    Add resolver implementations for datasets and stub for users.

---
+++ server/graphql/resolvers/user.js
@@ -0,0 +1,13 @@
+/**
+ * User resolvers
+ *
+ * These are passthroughs to SciTran until we have authentication working internally
+ */
+
+export const users = () => {
+  return []
+}
+
+export const whoami = () => {
+  return {}
+}
