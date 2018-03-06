5888d5267539510e6e9fc8444a35aa147647154f
Teal Hobson-Lowther <teal@squishymedia.com>
2018-03-06 11:30:06 -0800

    user avatar is now generated through a function in userStore

--- app/src/scripts/user/user.store.js
+++ app/src/scripts/user/user.store.js
@@ -423,6 +423,29 @@ let UserStore = Reflux.createStore({
         }
       })
   },
+
+  generateThumbnail(profile) {
+    let thumbnail, thumbnailUrl
+    if (profile.imageUrl) {
+      let username =
+        profile && profile.firstName && profile.lastName
+          ? profile.firstName + ' ' + profile.lastName
+          : null
+      thumbnailUrl = profile.imageUrl.replace('sz=50', 'sz=200')
+      thumbnail = (
+        <img src={thumbnailUrl} alt={username} className="user-img-thumb" />
+      )
+    } else {
+      let firstLetter =
+        profile && profile.firstname ? profile.firstname.slice(0, 1) : null
+      thumbnail = (
+        <div className="user-generic-thumb">
+          <div className="user-generic-thumb-letter">{firstLetter}</div>
+        </div>
+      )
+    }
+    return thumbnail
+  },
 })
 
 export default UserStore
