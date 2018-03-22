c36a2121967537d4e4d13f4dd5a4d0de83898e86
Teal Hobson-Lowther <teal@squishymedia.com>
2018-03-22 13:53:58 -0700

    decode incoming comment reply params; incorporate mailgun messageId validation

--- server/handlers/comments.js
+++ server/handlers/comments.js
@@ -47,9 +47,14 @@ export default {
    */
   async reply(req, res, next) {
     let comment
-    const parentId = req.params.commentId
-    const userId = req.params.userId
+    const parentId = req.params.commentId ? decodeURIComponent(req.params.commentId) : null
+    const userId = req.params.userId ? decodeURIComponent(req.params.userId) : null
     const text = textToDraft(req.body['stripped-text'])
+    const inReplyTo = req.body['In-Reply-To']
+    const messageId = inReplyTo ? await c.crn.mailgunIdentifiers.findOne({messageId: inReplyTo}) : null
+    if (!messageId) {
+      return res.sendStatus(404)
+    }
     const user = await c.scitran.users.findOne({ _id: ObjectID(userId) })
     let originalComment = await c.crn.comments.findOne({
       _id: ObjectID(parentId),
