4acc76203bd672b989a1c3e9b30e7ded95ddb2b6
Ross Blair <rblair2@stanford.edu>
2018-03-27 14:02:51 -0700

    fix how promises were being used in for loops and being returned in accession number update script

--- server/libs/accnum.js
+++ server/libs/accnum.js
@@ -1,22 +1,34 @@
+/* Scritp to update accession numbers in both scitran and crn databases.
+ * Expects first argument passed to the script to be a json file with key 
+ * 'accessionNumbers' and value an array of arrays of the form:
+ * [["ds000001", "ds000002"],...] where the first element of the inner array 
+ * is an existing accession number, and the second element is the accession
+ * number it should be updated to.
+ */
+
 import config from '../config'
 import bidsId from './bidsId'
 import { ObjectID } from 'mongodb'
 import mongo from './mongo'
+const fs = require('fs')
 
 let c = mongo.collections
 
 async function updateAccessionNumber(oldId, newId) {
   let item = await c.scitran.projects.findOne({ _id: ObjectID(oldId) }, {})
   updateId(c.scitran.projects, item, { _id: ObjectID(newId) }, ObjectID(oldId))
-  await updateSnapshotsId(oldId, newId)
-  await updateJobs(oldId, newId)
-  let cntr_item = await c.crn.counters.findOne({ _id: ObjectID(oldId) }, {})
-  return await updateId(
-    c.crn.counters,
-    cntr_item,
-    { _id: ObjectID(newId) },
-    ObjectID(oldId),
+  let retPromises = []
+  retPromises.push(
+    updateSnapshotsId(oldId, newId),
+    updateJobs(oldId, newId),
+    updateId(
+      c.crn.counters,
+      await c.crn.counters.findOne({ _id: ObjectID(oldId) }, {}),
+      { _id: ObjectID(newId) },
+      ObjectID(oldId),
+    ),
   )
+  return Promise.all(retPromises)
 }
 
 async function updateSnapshotsId(oldId, newId) {
@@ -24,6 +36,7 @@ async function updateSnapshotsId(oldId, newId) {
     { original: ObjectID(oldId) },
     {},
   )
+  let retPromises = []
   for (
     let item = await cursor.next();
     item != null;
@@ -32,7 +45,7 @@ async function updateSnapshotsId(oldId, newId) {
     let snapId = item['_id']
     let verId = bidsId.decodeId(snapId)
     let newSnapId = bidsId.decodeId(newId).slice(2) + '-' + verId
-    return await updateId(
+    let updatePromise = updateId(
       c.scitran.project_snapshots,
       item,
       {
@@ -41,11 +54,14 @@ async function updateSnapshotsId(oldId, newId) {
       },
       snapId,
     )
+    retPromises.push(updatePromise)
   }
+  return Promise.all(retPromises)
 }
 
 async function updateJobs(oldId, newId) {
   const cursor = c.crn.jobs.find({ datasetId: oldId }, {})
+  let retPromises = []
   for (
     let item = await cursor.next();
     item != null;
@@ -55,7 +71,7 @@ async function updateJobs(oldId, newId) {
     let snapId = item['snapshotId']
     let verId = bidsId.decodeId(snapId)
     let newSnapId = bidsId.decodeId(newId).slice(2) + '-' + verId
-    return await c.crn.jobs.update(
+    let jobPromise = c.crn.jobs.update(
       { _id: ObjectID(jobId) },
       {
         $set: {
@@ -64,7 +80,9 @@ async function updateJobs(oldId, newId) {
         },
       },
     )
+    retPromises.push(jobPromise)
   }
+  return Promise.all(retPromises)
 }
 
 async function updateId(collection, item, updates, delId) {
@@ -87,7 +105,11 @@ async function updateId(collection, item, updates, delId) {
     })
 }
 
-let accessionNumbers = [['ds001001', 'ds001002']]
+const args = process.argv
+
+let toUpdate = JSON.parse(fs.readFileSync(args[2]))
+
+let accessionNumbers = toUpdate.accessionNumbers
 
 mongo.connect(config.mongo.url, () => {
   Promise.all(
