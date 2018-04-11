18ee8b95d70bca1a1bf2afc0233f9f4e627f9ab1
Teal Hobson-Lowther <teal@squishymedia.com>
2018-04-11 15:22:23 -0700

    create page component for api key generator

---
+++ packages/openneuro-app/src/scripts/user/api.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import userStore from './user.store'
+import Spinner from '../common/partials/spinner.jsx'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+export default class APIKeyGen extends React.Component {
+  constructor(props) {
+    super(props)
+    this.requestKey = this._requestKey.bind(this)
+    this.state = {
+      loading: false,
+      key: null,
+    }
+  }
+  _requestKey() {
+    this.setState({ loading: true })
+    userStore.createAPIKey((err, key) => {
+      if (err) {
+        console.log(err)
+        this.setState({ loading: false })
+      } else {
+        console.log(key)
+        this.setState({ key: key, loading: false })
+      }
+    })
+  }
+
+  _requestButton() {
+    return (
+      <button
+        className="btn btn-lg btn-primary"
+        onClick={this.requestKey.bind(this)}>
+        GENERATE NEW API KEY
+      </button>
+    )
+  }
+
+  _loadingSpinner() {
+    if (this.state.loading) {
+      return <Spinner active={true} />
+    } else {
+      return null
+    }
+  }
+
+  _key() {
+    let content = (
+      <div className="api-key-div">
+        <div className="api-key">{this.state.key}</div>
+        <div className="copy-key">
+          <CopyToClipboard
+            text={this.state.key}
+            onCopy={this._onCopy.bind(this)}>
+            <span className="copy-key">
+              <i className="fa fa-link" aria-hidden="true" />{' '}
+              {this._copyNotification()}
+            </span>
+          </CopyToClipboard>
+        </div>
+      </div>
+    )
+    if (this.state.key) {
+      return content
+    } else {
+      return null
+    }
+  }
+  _copyNotification() {
+    let copyClass = this.state.linkCopied
+      ? 'copy-notification-active copy-notification-copied'
+      : 'copy-notification-active'
+    let copyText = this.state.linkCopied ? 'Copied!' : 'Copy key to clipboard'
+    return (
+      <span>
+        <span className={copyClass}>{copyText}</span>
+      </span>
+    )
+  }
+
+  _onCopy() {
+    this.setState({ linkCopied: true })
+    setTimeout(() => {
+      this.setState({ linkCopied: false })
+    }, 3000)
+  }
+
+  render() {
+    let helperText = this.state.key
+      ? 'Your API Key:'
+      : 'Click the button below to generate an API key'
+    return (
+      <div className="container api">
+        <div className="panel">
+            <div className="panel-heading">
+                CLI API Key Generator
+            </div>
+            <div className="panel-body explanation">
+                Users that wish to use the CLI uploader will be required to authenticate their requests with an API key. Please store your API key in a safe place. If you lose your key, you can obtain a new one on this page. Once a new key is obtained, the previous key will become invalid.
+            </div>
+        </div>
+        <div className="panel">
+          <div className="panel-heading">{helperText}</div>
+          <div className="panel-body">
+            {this._loadingSpinner()}
+            {this._key()}
+            {this._requestButton()}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+APIKeyGen.propTypes = {}
