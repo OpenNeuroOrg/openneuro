302dbc9b096262db6db67fdc92c7bd7137abd3b6
Nell Hardcastle <nell@squishymedia.com>
2018-04-15 15:45:07 -0700

    CLI: Add a basic upload implementation.

---
+++ packages/openneuro-cli/src/upload.js
@@ -0,0 +1,61 @@
+import walk from 'walk-promise'
+import { files } from 'openneuro-client'
+import validate from 'bids-validator'
+import { streamFiles } from './files'
+
+/**
+ * BIDS validator promise wrapper
+ *
+ * @param {string} dir directory to validate
+ * @param {Object} options validate.BIDS options
+ */
+const validatePromise = (dir, options = {}) => {
+  return new Promise((resolve, reject) => {
+    validate.BIDS(dir, options, (errors, warnings) => {
+      if (errors.length + warnings.length === 0) {
+        resolve()
+      } else {
+        reject({ errors, warnings })
+      }
+    })
+  })
+}
+
+/**
+ * Validate and upload a dataset
+ *
+ * @param {Object} client - Initialized Apollo client to upload with
+ * @param {string} dir - Directory to upload
+ * @param {string} datasetId - Optionally update an existing dataset
+ */
+export const validateAndUpload = (client, dir, datasetId) => {
+  return validatePromise(dir)
+    .then(datasetUpload(client, dir, datasetId))
+    .catch(err => {
+      // Validator errors handled here
+      console.log(err)
+    })
+}
+
+/**
+ * Make an upload request given an array of streams
+ */
+export const uploadStreams = (client, datasetId) => streams => {
+  return client.mutate({
+    mutation: files.updateFiles,
+    variables: { datasetId, files: streams },
+  })
+}
+
+/**
+ * Read and upload a dataset directory
+ *
+ * @param {Object} client - Initialized Apollo client to upload with
+ * @param {string} dir - Directory to upload
+ * @param {string} datasetId - Optionally update an existing dataset
+ */
+export const datasetUpload = (client, dir, datasetId) => {
+  return walk(dir)
+    .then(streamFiles(dir))
+    .then(uploadStreams(client, datasetId))
+}
