08a6f216125a3366fb5ce44e6a965f9a77b31836
Nell Hardcastle <nell@squishymedia.com>
2018-04-20 15:30:58 -0700

    CLI: Show file progress percentage and bytes remaining.
    Runs at least once per file and at most twice per second.

--- packages/openneuro-cli/src/files.js
+++ packages/openneuro-cli/src/files.js
@@ -1,10 +1,31 @@
 import fs from 'fs'
 import path from 'path'
 import { promisify } from 'util'
+import { debounce } from './utils'
 
 const readdir = promisify(fs.readdir)
 
-export const getFileTree = (basepath, root) => {
+const bytesToSize = bytes => {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  if (bytes === 0) return 'n/a'
+  const i = parseInt(String(Math.floor(Math.log(bytes) / Math.log(1024))), 10)
+  if (i === 0) return `${bytes} ${sizes[i]})`
+  return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`
+}
+
+export const fileProgress = (console, filename, stream, size) => () => {
+  const percentage = Math.round(100 * (stream.bytesRead / size))
+  const remainingBytes = size - stream.bytesRead
+  const remaining = remainingBytes
+    ? `(${bytesToSize(remainingBytes)} remaining)`
+    : ''
+  console.log(
+    `Transferring "${filename}" - ${percentage}% complete ${remaining}`,
+  )
+  return { percentage, remainingBytes }
+}
+
+export const getFileTree = (basepath, root, logging = true) => {
   return readdir(root).then(async contents => {
     // Run stat for each file
     const stats = contents.map(filePath => ({
@@ -18,7 +39,16 @@ export const getFileTree = (basepath, root) => {
       name: path.relative(basepath, root),
       files: stats
         .filter(({ stat }) => stat.isFile())
-        .map(({ filePath }) => fs.createReadStream(filePath)),
+        .map(({ stat, filePath }) => {
+          const stream = fs.createReadStream(filePath)
+          if (logging) {
+            stream.on(
+              'data',
+              debounce(fileProgress(console, filePath, stream, stat.size), 500),
+            )
+          }
+          return stream
+        }),
       directories: await Promise.all(
         // This is an array of each promise related to the next level in the tree
         stats
