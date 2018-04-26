197ebdacb8114950f06ef8a6fc2da20ad07a3199
Nell Hardcastle <nell@squishymedia.com>
2018-04-26 16:08:43 -0700

    App: Connect react-apollo to App component.

--- packages/openneuro-app/src/scripts/app.jsx
+++ packages/openneuro-app/src/scripts/app.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { pageTitle, pageDescription } from './resources/strings.js'
 import Index from './index.jsx'
 import analyticsWrapper from './utils/analytics.js'
+import getClient from 'openneuro-client'
 
 const App = () => {
   return (
-    <div>
-      <Helmet>
-        <title>{pageTitle}</title>
-        <meta name="description" content={pageDescription} />
-      </Helmet>
-      <Router>
-        <Route component={analyticsWrapper(Index)} />
-      </Router>
-    </div>
+    <ApolloProvider client={getClient('/crn/graphql')}>
+      <div>
+        <Helmet>
+          <title>{pageTitle}</title>
+          <meta name="description" content={pageDescription} />
+        </Helmet>
+        <Router>
+          <Route component={analyticsWrapper(Index)} />
+        </Router>
+      </div>
+    </ApolloProvider>
   )
 }
 
