80cb2761842c784bbb03e888df17dd7d289384ef
Nell Hardcastle <nell@squishymedia.com>
2018-04-28 12:55:00 -0700

    client: Add authentication token header support.

--- packages/openneuro-client/src/client.js
+++ packages/openneuro-client/src/client.js
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch'
 import ApolloClient from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { createUploadLink } from 'apollo-upload-client/src'
 import FormData from 'form-data'
@@ -18,8 +20,26 @@ const createClient = uri => {
   return new ApolloClient({ uri, link, cache })
 }
 
-const createLink = uri => {
-  return createUploadLink({ uri, fetch, serverFormData: FormData })
+const authLink = getAuthorization =>
+  setContext((_, { headers }) => {
+    // Passthrough any headers but add in authorization if set
+    const token = getAuthorization ? getAuthorization() : false
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    }
+  })
+
+const createLink = (uri, getAuthorization) => {
+  // We have to setup authLink to inject credentials here
+  const httpUploadLink = createUploadLink({
+    uri,
+    fetch,
+    serverFormData: FormData,
+  })
+  return authLink(getAuthorization).concat(httpUploadLink)
 }
 
 export { files, datasets }
