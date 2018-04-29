8b9a0bf14ec75e441f82e7ccc71c00f3b89df93f
Nell Hardcastle <nell@squishymedia.com>
2018-04-29 16:03:30 -0700

    Server: Implement user resolver via SciTran.

--- packages/openneuro-server/graphql/resolvers/user.js
+++ packages/openneuro-server/graphql/resolvers/user.js
@@ -3,11 +3,24 @@
  *
  * These are passthroughs to SciTran until we have authentication working internally
  */
+import { promisify } from 'util'
+import scitran from '../../libs/scitran.js'
+
+export const user = (obj, { id }) => {
+  return promisify(scitran.getUser)(id).then(({ body }) => {
+    return {
+      id: body._id,
+      firstName: body.firstname,
+      lastName: body.lastname,
+      email: body.email,
+    }
+  })
+}
 
 export const users = () => {
   return []
 }
 
-export const whoami = () => {
+export const whoami = (obj, { id }) => {
   return {}
 }
