2f2c3dd21b689e7839745a31951908a8bdedba2f
Nell Hardcastle <nell@squishymedia.com>
2018-04-29 16:08:12 -0700

    Server: Integrate new resolvers in the resolver tree.

--- packages/openneuro-server/graphql/resolvers/index.js
+++ packages/openneuro-server/graphql/resolvers/index.js
@@ -1,21 +1,33 @@
 import { GraphQLDate, GraphQLTime, GraphQLDateTime } from 'graphql-iso-date'
 import { GraphQLUpload } from 'apollo-upload-server'
-import { dataset, datasets, createDataset, updateFiles } from './dataset'
-import { whoami, users } from './user'
+import Dataset from './dataset.js'
+import { dataset, datasets, createDataset, updateFiles } from './dataset.js'
+import { draft, snapshot } from './datalad.js'
+import { summary } from './summary.js'
+import { whoami, user, users } from './user.js'
 
 export default {
+  Date: GraphQLDate,
+  Time: GraphQLTime,
+  DateTime: GraphQLDateTime,
+  Upload: GraphQLUpload,
   Query: {
     dataset,
     datasets,
     whoami,
+    user,
     users,
   },
   Mutation: {
     createDataset,
     updateFiles,
   },
-  Date: GraphQLDate,
-  Time: GraphQLTime,
-  DateTime: GraphQLDateTime,
-  Upload: GraphQLUpload,
+  User: user,
+  Dataset: {
+    uploader: ds => user(ds, { id: ds.uploader }),
+    draft,
+  },
+  Draft: draft,
+  Snapshot: snapshot,
+  Summary: summary,
 }
