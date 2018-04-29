e318e9e8b82b3c0d738e5c032c4bd751985f21e4
Nell Hardcastle <nell@squishymedia.com>
2018-04-29 16:07:30 -0700

    Server: Add stub resolvers for summaries and draft/snapshot info.

---
+++ packages/openneuro-server/graphql/resolvers/datalad.js
@@ -0,0 +1,6 @@
+/**
+ * Resolvers for state held by the datalad service
+ */
+export const draft = () => null
+
+export const snapshot = () => null
---
+++ packages/openneuro-server/graphql/resolvers/summary.js
@@ -0,0 +1,6 @@
+/**
+ * Summary resolver
+ *
+ * Triggers validation if a summary is invalid or null
+ */
+export const summary = () => null
