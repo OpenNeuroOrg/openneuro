ce0e33478e1be2ddfa64fe1b3b4688a439e25c3a
Nell Hardcastle <nell@squishymedia.com>
2018-05-12 13:57:27 -0700

    Implement context manager for commit info.

--- datalad_service/common/annex.py
+++ datalad_service/common/annex.py
@@ -1,3 +1,10 @@
+from datalad.config import ConfigManager
+
+
+SERVICE_EMAIL = 'git@openneuro.org'
+SERVICE_USER = 'Git Worker'
+
+
 def filter_git_files(files):
     """Remove any git/datalad files from a list of files."""
     return [f for f in files if not (f.startswith('.datalad/') or f == '.gitattributes')]
@@ -11,3 +18,20 @@ def get_repo_files(dataset, branch=None):
         size = dataset.repo.get_size_from_key(key)
         files.append({'filename': filename, 'size': size, 'id': key})
     return files
+
+
+class CommitInfo():
+    """Context manager for setting commit info on datalad operations that use it."""
+
+    def __init__(self, dataset, email=None, name=None):
+        self.config_manager = ConfigManager(dataset)
+        self.email = email if email else SERVICE_EMAIL
+        self.name = name if name else SERVICE_USER
+
+    def __enter__(self):
+        self.config_manager.set('user.email', self.email, 'local')
+        self.config_manager.set('user.name', self.name, 'local')
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.config_manager.set('user.email', SERVICE_EMAIL, 'local')
+        self.config_manager.set('user.name', SERVICE_USER, 'local')
