3ce4fa77cfaa60a0b15f65c3845fe8a569089996
Nell Hardcastle <nell@squishymedia.com>
2018-05-16 14:44:13 -0700

    Validator output is now parsed, updated test.

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -4,7 +4,7 @@ from .dataset_fixtures import *
 from datalad_service.tasks.validator import validate_dataset
 
 def test_validator(new_dataset):
-    results = json.loads(validate_dataset(new_dataset.path))
+    results = validate_dataset(new_dataset.path)
     # new_dataset doesn't pass validation, should return an error
     assert 'issues' in results
     assert 'errors' in results['issues']
