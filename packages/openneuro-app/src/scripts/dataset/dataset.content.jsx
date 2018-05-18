63626b990987b0510daa0265a674e3d659104b8f
Teal Hobson-Lowther <teal@squishymedia.com>
2018-05-18 11:18:11 -0700

    update dataset authors display to support authors without orcid ids

--- packages/openneuro-app/src/scripts/dataset/dataset.content.jsx
+++ packages/openneuro-app/src/scripts/dataset/dataset.content.jsx
@@ -221,7 +221,7 @@ class DatasetContent extends Reflux.Component {
       let authorString = 'authored by '
       for (let i = 0; i < authors.length; i++) {
         let author = authors[i]
-        authorString += author.name
+        authorString += author
         if (authors.length > 1) {
           if (i < authors.length - 2) {
             authorString += ', '
