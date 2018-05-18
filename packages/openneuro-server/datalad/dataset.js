6e0da8dc23af2a5179a6179e03ee3474ea3df757
Teal Hobson-Lowther <teal@squishymedia.com>
2018-05-18 15:00:46 -0700

    add deleteFile to openneuro-server datalad functions

--- packages/openneuro-server/datalad/dataset.js
+++ packages/openneuro-server/datalad/dataset.js
@@ -116,7 +116,7 @@ const encodeFilePath = path => {
 const fileUrl = (datasetId, path, filename) => {
   // If path is provided, this is a subdirectory, otherwise a root level file.
   const filePath = path ? [path, filename].join('/') : filename
-  const fileName = encodeFilePath(filePath)
+  const fileName = filename ? encodeFilePath(filePath) : encodeFilePath(path)
   const url = `http://${uri}/datasets/${datasetId}/files/${fileName}`
   return url
 }
@@ -170,6 +170,16 @@ export const commitFiles = (datasetId, name, email) => {
   setCommitInfo(req, name, email)
   return req
 }
+/** 
+ * Delete an existing file in a dataset
+ */
+export const deleteFile = (datasetId, path, file) => {
+  // Cannot use superagent 'request' due to inability to post streams
+  let url = fileUrl(datasetId, path, file.name)
+  console.log('sending deleteFile request to datalad path:', url)
+  return request.del(url)
+}
+
 /**
  * Update public state
  */
