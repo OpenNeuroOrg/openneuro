1537e6ee4df4c6c5b59d612de43002d6f208d10e
Teal Hobson-Lowther <teal@squishymedia.com>
2018-05-22 10:13:39 -0700

    remove unused var from client mock

--- packages/openneuro-client/src/__mocks__/client.js
+++ packages/openneuro-client/src/__mocks__/client.js
@@ -2,7 +2,6 @@ import ApolloClient from 'apollo-client'
 import { SchemaLink } from 'apollo-link-schema'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { addMockFunctionsToSchema } from 'graphql-tools'
-import {GraphQLDateTime} from 'graphql-iso-date'
 import schema from 'openneuro-server/graphql/schema'
 
 /**
