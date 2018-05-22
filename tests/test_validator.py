e5bb1455859c42736c58321333d88a0b1dde2ae4
Nell Hardcastle <nell@squishymedia.com>
2018-05-22 10:30:18 -0700

    Fix validation test for new function names.

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,10 +1,10 @@
 import json
 
 from .dataset_fixtures import *
-from datalad_service.tasks.validator import validate_dataset
+from datalad_service.tasks.validator import validate_dataset_sync
 
 def test_validator(new_dataset):
-    results = validate_dataset(new_dataset.path)
+    results = validate_dataset_sync(new_dataset.path)
     # new_dataset doesn't pass validation, should return an error
     assert 'issues' in results
     assert 'errors' in results['issues']
