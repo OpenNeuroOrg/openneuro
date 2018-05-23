751eaaba885d6093e9350a9c6310746500466e4a
Nell Hardcastle <nell@squishymedia.com>
2018-05-23 09:55:11 -0700

    Cache snapshot contents in redis too.

--- packages/openneuro-server/datalad/snapshots.js
+++ packages/openneuro-server/datalad/snapshots.js
@@ -2,9 +2,14 @@
  * Get snapshots from datalad-service tags
  */
 import request from 'superagent'
+import { redis } from '../libs/redis.js'
 import config from '../config.js'
 
 const uri = config.datalad.uri
+
+const snapshotKey = (datasetId, tag) => {
+  return `openneuro:snapshot:${datasetId}:${tag}`
+}
 
 export const getSnapshots = datasetId => {
   const url = `${uri}/datasets/${datasetId}/snapshots`
@@ -18,10 +23,16 @@ export const getSnapshots = datasetId => {
 
 export const getSnapshot = (datasetId, tag) => {
   const url = `${uri}/datasets/${datasetId}/snapshots/${tag}`
-  return request
-    .get(url)
-    .set('Accept', 'application/json')
-    .then(({ body }) => {
-      return body
-    })
+  const key = snapshotKey(datasetId, tag)
+  return redis.get(key).then(data => {
+    if (data) return JSON.parse(data)
+    else
+      return request
+        .get(url)
+        .set('Accept', 'application/json')
+        .then(({ body }) => {
+          redis.set(key, JSON.stringify(body))
+          return body
+        })
+  })
 }
