02bc6bdc6522ddcb80295ad6bcfd413ecf91b79d
Teal Hobson-Lowther <tealhobsonlowther@gmail.com>
2018-06-20 12:56:13 -0700

    analysis-related dashboard panels are toggleable

--- packages/openneuro-app/src/scripts/dashboard/dashboard.jsx
+++ packages/openneuro-app/src/scripts/dashboard/dashboard.jsx
@@ -4,7 +4,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Switch, Route, NavLink, Redirect } from 'react-router-dom'
 import Datasets from './dashboard.datasets.jsx'
+import { JobLink, JobRoute } from '../common/partials/jobs.jsx'
 import Jobs from './dashboard.jobs.jsx'
+import config from '../../../config'
+
+const analysisEnabled = config.analysis.enabled
 
 const publicDatasets = () => <Datasets public />
 const publicJobs = () => <Jobs public />
@@ -32,15 +36,17 @@ class Dashboard extends React.Component {
                 </NavLink>
               </li>
               <li>
-                <NavLink to={prefix + '/jobs'} className="btn-tab">
-                  {isPublic ? 'Public' : 'My'} Analyses
-                </NavLink>
+                <JobLink
+                  prefix={prefix}
+                  isPublic={isPublic}
+                  enabled={analysisEnabled}
+                />
               </li>
             </ul>
             <Switch>
               <Redirect path={prefix} to={prefix + '/datasets'} exact />
               <Route component={datasets} path={prefix + '/datasets'} />
-              <Route component={jobs} path={prefix + '/jobs'} />
+              <JobRoute prefix={prefix} jobs={jobs} enabled={analysisEnabled} />
             </Switch>
           </div>
         </div>
