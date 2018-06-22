b2a844543b8d6a850eb2956cc5d25f154bfeaec8
Nell Hardcastle <nell@squishymedia.com>
2018-06-22 10:03:31 -0700

    Disable sessions for Google auth (the API is stateless).

--- packages/openneuro-server/libs/authentication/google.js
+++ packages/openneuro-server/libs/authentication/google.js
@@ -2,13 +2,15 @@ import passport from 'passport'
 
 export const requestAuth = passport.authenticate('google', {
   scope: ['https://www.googleapis.com/auth/plus.login'],
+  session: false,
 })
 
 export const authCallback = passport.authenticate(
   'google',
   { failureRedirect: '/' },
-  () => {
+  (req, res) => {
     // eslint-disable-next-line no-console
     console.log('Auth callback ran.')
+    res.redirect('/')
   },
 )
