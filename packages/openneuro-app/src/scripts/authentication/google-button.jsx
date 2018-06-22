512a1014023597b691d6940ae64f2dc3a451c863
Nell Hardcastle <nell@squishymedia.com>
2018-06-22 10:34:28 -0700

    Fix Google auth button URL issue.

--- packages/openneuro-app/src/scripts/authentication/google-button.jsx
+++ packages/openneuro-app/src/scripts/authentication/google-button.jsx
@@ -5,7 +5,7 @@ import config from '../../../config.js'
  * Button starts Google oauth2 workflow
  */
 const GoogleButton = () => (
-  <a href={config.crn.url + '/auth/google'}>
+  <a href={config.crn.url + 'auth/google'}>
     <button className="btn-admin">
       <i className="icon fa fa-google" />
       Sign in with Google
