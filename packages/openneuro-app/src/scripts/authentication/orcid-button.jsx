0bd6b010cb21f01a377cd4fa9fd879ee374640d0
Teal Hobson-Lowther <tealhobsonlowther@gmail.com>
2018-06-25 12:01:02 -0700

    add orcid button to home page

--- packages/openneuro-app/src/scripts/authentication/orcid-button.jsx
+++ packages/openneuro-app/src/scripts/authentication/orcid-button.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import config from '../../../config'
 
 const OrcidButton = () => (
-  <button className="btn-admin" onClick={userStore.orcidSignIn}>
-    <span className="icon">
-      <img
-        alt="ORCID"
-        width="20"
-        height="20"
-        src="https://orcid.org/sites/default/files/images/orcid_24x24.png"
-      />
-    </span>
-    Sign in with ORCID
-  </button>
+  <a href={config.crn.url + 'auth/orcid'}>
+    <button className="btn-admin">
+      <span className="icon">
+        <img
+          alt="ORCID"
+          width="20"
+          height="20"
+          src="https://orcid.org/sites/default/files/images/orcid_24x24.png"
+        />
+      </span>
+      Sign in with ORCID
+    </button>
+  </a>
 )
 
 export default OrcidButton
