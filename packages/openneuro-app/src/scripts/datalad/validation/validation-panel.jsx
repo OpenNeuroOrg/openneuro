6edd8add45804262d586040c733d43f9cbb134a6
Teal Hobson-Lowther <tealhobsonlowther@gmail.com>
2018-07-03 16:35:46 -0700

    fix validation panel toggle bug

--- packages/openneuro-app/src/scripts/datalad/validation/validation-panel.jsx
+++ packages/openneuro-app/src/scripts/datalad/validation/validation-panel.jsx
@@ -12,9 +12,9 @@ class ValidationPanel extends React.Component {
   }
 
   togglePanel() {
-    if (this.state.activeKey === '1') {
+    if (this.props.children.length && this.state.activeKey === '1') {
       this.setState({ activeKey: '2' })
-    } else if (this.state.activeKey === '2') {
+    } else if (this.props.children.length && this.state.activeKey === '2') {
       this.setState({ activeKey: '1' })
     }
   }
@@ -27,7 +27,7 @@ class ValidationPanel extends React.Component {
         activeKey={this.state.activeKey}
         onSelect={this.togglePanel}>
         <Panel className="status" header={this.props.heading} eventKey="1">
-          {this.props.children}
+          {this.props.children ? this.props.children : null}
         </Panel>
       </PanelGroup>
     )
