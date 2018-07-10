7fd73f1a9b4727db528175f4c2d86740e685519b
Nell Hardcastle <nell@squishymedia.com>
2018-07-10 13:54:17 -0700

    Comment about migration ordering.

--- packages/openneuro-server/migrations/index.js
+++ packages/openneuro-server/migrations/index.js
@@ -2,4 +2,5 @@ import scitranUser from './01-scitranUser.js'
 import scitranPermission from './02-scitranPermission.js'
 import genericAccounts from './03-genericAccounts.js'
 
+// Ordered list should match a sort of the filenames
 export default [scitranUser, scitranPermission, genericAccounts]
