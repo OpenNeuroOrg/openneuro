c4fcf8937bbe73697a978734f7878e50d13bb46e
Teal Hobson-Lowther <tealhobsonlowther@gmail.com>
2018-07-20 16:20:16 -0700

    mock content config and update testing snapshots

---
+++ packages/openneuro-app/__mocks__/contentMock.js
@@ -0,0 +1,17 @@
+module.exports = {
+  faq: [
+    {
+      faq: 'Who tests the best?',
+      faq_answer: 'we test the best!',
+    },
+  ],
+  frontPage: {
+    titlePanel: {
+      privacyLink: 'nutsaboutbrains.org',
+    },
+    pageTitle: 'Testing',
+    copyright: {
+      holder: 'squirrels',
+    },
+  },
+}
