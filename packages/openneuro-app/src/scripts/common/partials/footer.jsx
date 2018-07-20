033db02eaacdf587f5da36222379a7f16066bf83
Teal Hobson-Lowther <tealhobsonlowther@gmail.com>
2018-07-20 14:16:57 -0700

    footer uses config from openneuro/content

--- packages/openneuro-app/src/scripts/common/partials/footer.jsx
+++ packages/openneuro-app/src/scripts/common/partials/footer.jsx
@@ -3,6 +3,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import packageJson from '../../../../package.json'
+import moment from 'moment'
+import { frontPage } from 'openneuro-content'
 
 const Footer = props => {
   let version = props.version ? props.version : packageJson.version
@@ -13,10 +15,16 @@ const Footer = props => {
           <div className="col-xs-12 col-md-4 version">
             <span>OpenNeuro v{version}</span>
           </div>
-          <div className="col-xs-12 col-md-4 footer-menu" />
+          <div className="col-xs-12 col-md-4 privacy-policy">
+            <span>
+              <a href={frontPage.titlePanel.privacyLink}>
+                {frontPage.pageTitle}&#39;s Privacy Policy
+              </a>
+            </span>
+          </div>
           <div className="col-xs-12 col-md-4 copy">
             <span>
-              &copy; 2017 Stanford Center for Reproducible Neuroscience
+              &copy; {moment().year()} {frontPage.copyright.holder}
             </span>
           </div>
         </div>
