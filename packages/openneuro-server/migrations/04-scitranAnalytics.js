bf84bb4ae36c7bc5293f017dd3ad899efef4111c
Teal Hobson-Lowther <tealhobsonlowther@gmail.com>
2018-07-20 10:43:14 -0700

    fix analytics migration script

--- packages/openneuro-server/migrations/04-scitranAnalytics.js
+++ packages/openneuro-server/migrations/04-scitranAnalytics.js
@@ -14,17 +14,25 @@ export default {
   update: async () => {
     const analytics = await scitran.analytics.find({}).toArray()
     // console.log('analytics:', analytics)
-    const newAnalytics = []
     for (const analytic of analytics) {
       let { datasetId, tag } = bidsId.decode(analytic.container_id)
-      newAnalytics.push({
-        datasetId,
-        tag,
-        type: analytic.analytics_type,
-      })
+      let type = analytic.analytics_type + 's' //convert 'view' -> 'views' and 'download' -> 'downloads'
+      try {
+        await Analytics.collection.updateOne(
+          {
+            datasetId,
+            tag,
+          },
+          {
+            $inc: { [type]: 1 },
+          },
+          {
+            upsert: true,
+          },
+        )
+      } catch (e) {
+        throw e
+      }
     }
-    Analytics.collection.insert(newAnalytics).catch(e => {
-      throw e
-    })
   },
 }
