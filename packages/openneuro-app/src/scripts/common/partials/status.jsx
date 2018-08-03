34893bb93f1a1890a3fe24c3f00827558a349d8c
Nell Hardcastle <nell@squishymedia.com>
2018-08-03 11:31:21 -0700

    Revert "remove unused file-select vars"
    This reverts commit 2b5994a33900a6e4ced69d50342f70e419cfb1ca.

--- packages/openneuro-app/src/scripts/common/partials/status.jsx
+++ packages/openneuro-app/src/scripts/common/partials/status.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Tooltip from './tooltip.jsx'
-// import FileSelect from '../forms/file-select.jsx'
+import FileSelect from '../forms/file-select.jsx'
 
 // component setup -----------------------------------------------------------
 
