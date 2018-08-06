b0a7b343fcf56e18040a896886209ca35dd9085a
Nell Hardcastle <nell@squishymedia.com>
2018-08-06 14:19:47 -0700

    Show a disabled button for resume while another upload is in progress.

--- packages/openneuro-app/src/scripts/uploader/upload-resume.jsx
+++ packages/openneuro-app/src/scripts/uploader/upload-resume.jsx
@@ -5,9 +5,24 @@ import UploaderContext from './uploader-context.js'
 
 const UploadResume = ({ datasetId }) => (
   <UploaderContext.Consumer>
-    {uploader => (
-      <FileSelect onChange={uploader.resumeDataset(datasetId)} resume />
-    )}
+    {uploader =>
+      uploader.uploading ? (
+        <button
+          className="fileupload-btn btn-admin"
+          disabled={true}
+          title="Please wait for your current upload to finish before resuming">
+          <span>
+            <i className="fa fa-repeat" />&nbsp;
+          </span>Resume
+        </button>
+      ) : (
+        <FileSelect
+          onChange={uploader.resumeDataset(datasetId)}
+          resume
+          disabled={uploader.uploading}
+        />
+      )
+    }
   </UploaderContext.Consumer>
 )
 
