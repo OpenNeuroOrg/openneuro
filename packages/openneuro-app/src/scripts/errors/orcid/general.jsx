8f2979c5bcf5d6758be6f3fdf01cfc731638b212
Nell Hardcastle <nell@squishymedia.com>
2018-09-10 16:38:57 -0700

    More accurate error header.

--- packages/openneuro-app/src/scripts/errors/orcid/general.jsx
+++ packages/openneuro-app/src/scripts/errors/orcid/general.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => (
   <div className="panel-heading">
-    <h2>There was an issue logging into your ORCID account</h2>
+    <h2>There was an issue authenticating with your ORCID account</h2>
   </div>
 )
