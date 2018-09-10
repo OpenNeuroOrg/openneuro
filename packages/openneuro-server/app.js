195a7678750a3bca0007a4cc40632e5a73472483
Nell Hardcastle <nell@squishymedia.com>
2018-09-10 16:33:08 -0700

    Remove unused generic express error handler.
    This was just swallowing useful errors.

--- packages/openneuro-server/app.js
+++ packages/openneuro-server/app.js
@@ -49,26 +49,6 @@ export default test => {
   // Raven reporting passes to the next step
   test || app.use(Raven.errorHandler())
 
-  app.use(function(err, req, res, next) {
-    res.header('Content-Type', 'application/json')
-    var send = { error: '' }
-    var http_code = typeof err.http_code === 'undefined' ? 500 : err.http_code
-    if (typeof err.message !== 'undefined' && err.message !== '') {
-      send.error = err.message
-    } else {
-      if (err.http_code == 400) {
-        send.error = 'there was something wrong with that request'
-      } else if (err.http_code == 401) {
-        send.error = 'you are not authorized to do that'
-      } else if (err.http_code == 404) {
-        send.error = 'that resource was not found'
-      } else {
-        send.error = 'there was a problem'
-      }
-    }
-    res.status(http_code).send(send)
-  })
-
   // Apollo engine setup
   const engineConfig = {
     privateVariables: ['files'],
