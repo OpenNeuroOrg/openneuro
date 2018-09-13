e02217f6cd66862cc1f43d17aed105e0526e581c
Nell Hardcastle <nell@squishymedia.com>
2018-09-13 14:13:00 -0700

    Add a helper script for profiling get_repo_files.

---
+++ tests/profile_get_files.py
@@ -0,0 +1,25 @@
+import os
+
+from datalad.api import Dataset
+from datalad_service.common.annex import get_repo_files
+import vmprof
+
+from .dataset_fixtures import *
+
+
+def test_profile_get_repo_files(annex_path, new_dataset):
+    ds_id = os.path.basename(new_dataset.path)
+    ds = Dataset(str(annex_path.join(ds_id)))
+    for each in range(5000):
+        filename = 'file-{}'.format(each)
+        path = os.path.join(new_dataset.path, filename)
+        with open(path, 'a'):
+            os.utime(path)
+    # Add all generated files
+    ds.add('.')
+    # Profile get_repo_files by itself
+    with open('{}.prof'.format(__name__), 'w+b') as fd:
+        vmprof.enable(fd.fileno())
+        for n in range(1):
+            get_repo_files(ds)
+        vmprof.disable()
