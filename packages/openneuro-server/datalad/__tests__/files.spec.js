398ec5e4c3441b0653d9a6377408ad206dd3245b
Nell Hardcastle <nell@squishymedia.com>
2018-09-18 14:14:43 -0700

    Add tests for datalad/files.js

---
+++ packages/openneuro-server/datalad/__tests__/files.spec.js
@@ -0,0 +1,44 @@
+import {
+  commitFilesKey,
+  encodeFilePath,
+  decodeFilePath,
+  fileUrl,
+} from '../files.js'
+
+jest.mock('../../config.js')
+
+const filename = 'sub-01/anat/sub-01_T1w.nii.gz'
+
+describe('datalad files', () => {
+  describe('commitFilesKey()', () => {
+    it('encodes a valid cache key', () => {
+      expect(
+        commitFilesKey('ds000001', '13582a0b2dc82b3644431ba54fd38926a5d2238f'),
+      ).toBe(
+        'openneuro:draftFiles:ds000001:13582a0b2dc82b3644431ba54fd38926a5d2238f',
+      )
+    })
+  })
+  describe('encodeFilePath()', () => {
+    it('should encode a nested path', () => {
+      expect(encodeFilePath(filename)).toBe('sub-01:anat:sub-01_T1w.nii.gz')
+    })
+  })
+  describe('decodeFilePath()', () => {
+    it('decodes a file path', () => {
+      expect(decodeFilePath('sub-01:anat:sub-01_T1w.nii.gz')).toBe(filename)
+    })
+  })
+  describe('fileUrl()', () => {
+    it('returns a working URL', () => {
+      expect(fileUrl('ds000001', '', filename)).toBe(
+        'http://datalad:9877/datasets/ds000001/files/sub-01:anat:sub-01_T1w.nii.gz',
+      )
+    })
+    it('handles path nesting', () => {
+      expect(fileUrl('ds000001', 'sub-01/anat', 'sub-01_T1w.nii.gz')).toBe(
+        'http://datalad:9877/datasets/ds000001/files/sub-01:anat:sub-01_T1w.nii.gz',
+      )
+    })
+  })
+})
