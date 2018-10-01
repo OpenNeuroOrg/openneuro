992d50d54a56c495134361b5f16598ccb6118ccf
Nell Hardcastle <nell@squishymedia.com>
2018-10-01 12:12:52 -0700

    Add tests for resolver dataset permission helpers.

---
+++ packages/openneuro-server/graphql/__tests__/permissions.spec.js
@@ -0,0 +1,50 @@
+import {
+  datasetReadQuery,
+  checkReadPermissionLevel,
+  checkDatasetWrite,
+} from '../permissions.js'
+
+describe('resolver permissions helpers', () => {
+  describe('datasetReadQuery', () => {
+    it('returns public for anonymous users', () => {
+      expect(datasetReadQuery('ds000001', null, null)).toHaveProperty(
+        'public',
+        true,
+      )
+    })
+    it('returns non-public for admins', () => {
+      expect(
+        datasetReadQuery('ds000001', '1234', { admin: true }),
+      ).not.toHaveProperty('public', true)
+    })
+    it('returns public for logged in users', () => {
+      expect(
+        datasetReadQuery('ds000001', '1234', { admin: false }),
+      ).toHaveProperty('public', true)
+    })
+  })
+  describe('checkReadPermissionLevel', () => {
+    it('returns false if no permission passed in', () => {
+      expect(checkReadPermissionLevel(null)).toBe(false)
+    })
+    it('returns true for valid read access level', () => {
+      expect(checkReadPermissionLevel({ level: 'admin' })).toBe(true)
+      expect(checkReadPermissionLevel({ level: 'ro' })).toBe(true)
+    })
+    it('returns false if an unexpected level is present', () => {
+      expect(checkReadPermissionLevel({ level: 'not-real' })).toBe(false)
+    })
+  })
+  describe('checkDatasetWrite', () => {
+    it('resolves to false for anonymous users', () => {
+      return expect(
+        checkDatasetWrite('ds000001', null, null),
+      ).rejects.toThrowErrorMatchingSnapshot()
+    })
+    it('resolves to true for admins', () => {
+      return expect(
+        checkDatasetWrite('ds000001', '1234', { admin: true }),
+      ).resolves.toBe(true)
+    })
+  })
+})
