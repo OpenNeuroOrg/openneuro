f0c064424bcd291a3bc88a10818c090e923c7d7c
Nell Hardcastle <nell@squishymedia.com>
2018-10-11 23:40:36 -0700

    CLI: Snapshot download support.
    Limited to public snapshots. Stubs in place for drafts.

---
+++ packages/openneuro-cli/src/download.js
@@ -0,0 +1,64 @@
+import fs from 'fs'
+import path from 'path'
+import mkdirp from 'mkdirp'
+import request from 'superagent'
+import { getUrl } from './config'
+
+export const downloadUrl = (datasetId, tag) =>
+  tag
+    ? `${getUrl()}crn/datasets/${datasetId}/snapshots/${tag}/download`
+    : `${getUrl()}crn/datasets/${datasetId}/download`
+
+export const checkDestination = destination => {
+  if (fs.existsSync(destination)) {
+    // Exists, check if directory
+    if (!fs.lstatSync(destination).isDirectory()) {
+      throw new Error(`Target file "${destination}" must be a directory`)
+    }
+  } else {
+    // Doesn't exist, make directory
+    fs.mkdirSync(destination)
+  }
+}
+
+/**
+ * Test if a file exists and matches correct size
+ * @param {string} destination
+ * @param {string} filename
+ * @param {number} size
+ */
+export const testFile = (destination, filename, size) => {
+  const fullPath = path.join(destination, filename)
+  if (fs.existsSync(fullPath)) {
+    return fs.lstatSync(fullPath).size !== size
+  } else {
+    return true
+  }
+}
+
+export const getDownloadMetadata = (datasetId, tag) =>
+  request.get(downloadUrl(datasetId, tag))
+
+export const downloadFile = async (destination, filename, fileUrl) => {
+  const fullPath = path.join(destination, filename)
+  // Create any needed parent dirs
+  mkdirp.sync(path.dirname(fullPath))
+  const writeStream = fs.createWriteStream(fullPath)
+  await request.get(fileUrl).pipe(writeStream)
+}
+
+export const getDownload = (datasetId, tag, destination) =>
+  getDownloadMetadata(datasetId, tag).then(async ({ body }) => {
+    checkDestination(destination)
+    for (let file of body.files) {
+      if (testFile(destination, file.filename, file.size)) {
+        // Now actually download
+        // eslint-disable-next-line no-console
+        console.log(`Downloading "${file.filename}" - size ${file.size} bytes`)
+        await downloadFile(destination, file.filename, file.urls.pop())
+      } else {
+        // eslint-disable-next-line no-console
+        console.log(`Skipping present file "${file.filename}"`)
+      }
+    }
+  })
