0a0bb649b54986a19fd73ee53fab8caf4ba146e3
Nell Hardcastle <nell@squishymedia.com>
2018-10-13 20:24:17 -0700

    Restore Googlebot - fix transpilation issues in ES6 deps.
    Includes an update to babel 7 as this simplified the fix.

--- packages/openneuro-app/webpack.common.js
+++ packages/openneuro-app/webpack.common.js
@@ -56,11 +56,9 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: [/node_modules/],
         use: [
           {
             loader: 'babel-loader',
-            options: { presets: ['es2015', 'react'] },
           },
         ],
       },
