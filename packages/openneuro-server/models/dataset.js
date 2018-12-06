c8c9a196b3a2463cb7c309f7c6f046609ce77169
Nell Hardcastle <nell@squishymedia.com>
2018-12-06 11:56:13 -0800

    Include populated fields in dataset model as JSON or Object.

--- packages/openneuro-server/models/dataset.js
+++ packages/openneuro-server/models/dataset.js
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose'
 
-const datasetSchema = new mongoose.Schema({
-  id: { type: String, unique: true }, // Accession number
-  created: { type: Date, default: Date.now },
-  modified: { type: Date, default: Date.now },
-  uploader: String,
-  revision: String,
-})
+const datasetSchema = new mongoose.Schema(
+  {
+    id: { type: String, unique: true }, // Accession number
+    created: { type: Date, default: Date.now },
+    modified: { type: Date, default: Date.now },
+    revision: String,
+  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } },
+)
 
 datasetSchema.virtual('uploader', {
   ref: 'User',
