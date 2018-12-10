6108f76c9f0be26f109120805549c38fd0ff0043
Nell Hardcastle <nell@squishymedia.com>
2018-12-10 15:39:33 -0800

    Get a little more performance out of dashboard page rendering.

--- packages/openneuro-app/src/scripts/datalad/dashboard/datasets/uploaded.jsx
+++ packages/openneuro-app/src/scripts/datalad/dashboard/datasets/uploaded.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
 
-const Uploaded = ({ uploader, created }) => {
-  const fullname = uploader ? uploader.name : ''
-  const dateAdded = created ? new Date(created).toISOString().split('T')[0] : ''
-  const timeago = created ? distanceInWordsToNow(created) : ''
-  return (
-    <div className="meta-container">
-      <p className="date">
-        uploaded {uploader ? 'by ' : ''}
-        <span className="name">{fullname}</span> on{' '}
-        <span className="time-ago">
-          {dateAdded} - {timeago} ago
-        </span>
-      </p>
-    </div>
-  )
+class Uploaded extends React.PureComponent {
+  render() {
+    const fullname = this.props.uploader ? this.props.uploader.name : ''
+    const dateAdded = this.props.created
+      ? new Date(this.props.created).toISOString().split('T')[0]
+      : ''
+    const timeago = this.props.created
+      ? distanceInWordsToNow(this.props.created)
+      : ''
+    return (
+      <div className="meta-container">
+        <p className="date">
+          uploaded {this.props.uploader ? 'by ' : ''}
+          <span className="name">{fullname}</span> on{' '}
+          <span className="time-ago">
+            {dateAdded} - {timeago} ago
+          </span>
+        </p>
+      </div>
+    )
+  }
 }
 
 Uploaded.propTypes = {
