b566c60a51bd8446110366bfb8c3c23732e9450d
Nell Hardcastle <nell@squishymedia.com>
2018-12-09 19:58:44 -0800

    Restore removed search results component.

--- packages/openneuro-app/src/scripts/routes.jsx
+++ packages/openneuro-app/src/scripts/routes.jsx
@@ -20,7 +20,7 @@ const Faq = loadable(() =>
   import(/* webpackChunkName: 'Faq' */ './faq/faq.jsx'),
 )
 const SearchResults = loadable(() =>
-  import(/* webpackChunkName: 'SearchResults' */ './dashboard/dashboard.searchresults.jsx'),
+  import(/* webpackChunkName: 'SearchResults' */ './datalad/dashboard/search-results.jsx'),
 )
 const APIKey = loadable(() =>
   import(/* webpackChunkName: 'APIKey' */ './user/api.jsx'),
