72b522740007f4752bf24a01c23b61897647db8a
Nell Hardcastle <nell@squishymedia.com>
2018-12-09 19:23:21 -0800

    Remove unused import from pagination tests.

--- packages/openneuro-server/datalad/__tests__/pagination.spec.js
+++ packages/openneuro-server/datalad/__tests__/pagination.spec.js
@@ -1,6 +1,5 @@
 import { ObjectID } from 'mongodb'
 import mockingoose from 'mockingoose'
-import Dataset from '../../models/dataset.js'
 import * as pagination from '../pagination.js'
 
 const base64 = /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$/
