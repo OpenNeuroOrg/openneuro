7908fea20f712a59396e8cc9bf7d5a2826c09e7f
Nell Hardcastle <nell@squishymedia.com>
2018-12-10 17:06:52 -0800

    Fix anonymous user regression in withProfile component.

--- packages/openneuro-app/src/scripts/authentication/withProfile.js
+++ packages/openneuro-app/src/scripts/authentication/withProfile.js
@@ -4,7 +4,8 @@ import { getProfile, parseJwt } from './profile.js'
 
 const withProfile = WrappedComponent => {
   const profile = getProfile()
-  return props => <WrappedComponent profile={profile} {...props} />
+  return props =>
+    profile ? <WrappedComponent profile={profile} {...props} /> : null
 }
 
 export default withProfile
