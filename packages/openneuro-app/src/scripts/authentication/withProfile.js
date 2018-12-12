cb5a98542979b470449e5df59822a3441807c709
Nell Hardcastle <nell@squishymedia.com>
2018-12-11 17:54:22 -0800

    Remove unused import from withProfile.

--- packages/openneuro-app/src/scripts/authentication/withProfile.js
+++ packages/openneuro-app/src/scripts/authentication/withProfile.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import React from 'react'
-import { getProfile, parseJwt } from './profile.js'
+import { getProfile } from './profile.js'
 
 const withProfile = WrappedComponent => {
   const profile = getProfile()
