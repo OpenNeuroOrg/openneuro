53f3ac71182b5e64cc1b2c3a073726dbf9638b56
Nell Hardcastle <nell@squishymedia.com>
2018-12-18 13:50:25 -0800

    Add unit test for snapshotCreationComparison with strings.

--- packages/openneuro-server/graphql/resolvers/__tests__/dataset.spec.js
+++ packages/openneuro-server/graphql/resolvers/__tests__/dataset.spec.js
@@ -34,5 +34,16 @@ describe('dataset resolvers', () => {
       expect(sorted[1].id).toBe(2)
       expect(sorted[2].id).toBe(3)
     })
+    it('sorts array of objects by the "created" property as strings', () => {
+      const testArray = [
+        { id: 2, created: '2018-11-20T00:05:43.473Z' },
+        { id: 1, created: '2018-11-19T00:05:43.473Z' },
+        { id: 3, created: '2018-11-23T00:05:43.473Z' },
+      ]
+      const sorted = testArray.sort(ds.snapshotCreationComparison)
+      expect(sorted[0].id).toBe(1)
+      expect(sorted[1].id).toBe(2)
+      expect(sorted[2].id).toBe(3)
+    })
   })
 })
