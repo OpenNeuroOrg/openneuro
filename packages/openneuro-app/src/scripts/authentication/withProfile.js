a57756b373589292172ad455ea6894d30f7a3f13
Nell Hardcastle <nell@squishymedia.com>
2018-12-18 16:31:57 -0800

    Refresh profile components when logged out state changes.
    Fixes #807

--- packages/openneuro-app/src/scripts/authentication/withProfile.js
+++ packages/openneuro-app/src/scripts/authentication/withProfile.js
@@ -1,11 +1,25 @@
 /* eslint-disable */
 import React from 'react'
+import cookies from '../utils/cookies.js'
 import { getProfile } from './profile.js'
 
 const withProfile = WrappedComponent => {
-  const profile = getProfile()
-  return props =>
-    profile ? <WrappedComponent profile={profile} {...props} /> : null
+  return class ProfileComponent extends React.Component {
+    constructor(props) {
+      super(props)
+      cookies.addChangeListener(({ name }) => {
+        if (name === 'accessToken') {
+          this.forceUpdate()
+        }
+      })
+    }
+    render() {
+      const profile = getProfile()
+      return profile ? (
+        <WrappedComponent profile={profile} {...this.props} />
+      ) : null
+    }
+  }
 }
 
 export default withProfile
---
+++ packages/openneuro-app/src/scripts/utils/cookies.js
@@ -0,0 +1,5 @@
+import Cookies from 'universal-cookie'
+
+const cookies = new Cookies()
+
+export default cookies
