00ea47ef3924ae87c3b9495943698690b20bb7c4
Nell Hardcastle <nell@squishymedia.com>
2019-03-08 14:00:44 -0800

    Add stub for wider comments block.

---
+++ packages/openneuro-app/src/scripts/datalad/dataset/dataset-comments.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const DatasetComments = () => <div>Comments go here</div>
+
+export default DatasetComments
--- packages/openneuro-app/src/scripts/datalad/dataset/dataset-main.jsx
+++ packages/openneuro-app/src/scripts/datalad/dataset/dataset-main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import DatasetRoutes from './dataset-routes.jsx'
+import DatasetComments from './dataset-comments.jsx'
 
 const DatasetMain = ({ dataset }) => (
-  <div className="row">
-    <DatasetRoutes dataset={dataset} />
-  </div>
+  <>
+    <div className="row">
+      <DatasetRoutes dataset={dataset} />
+    </div>
+    <div className="row">
+      <DatasetComments datasetId={dataset.id} />
+    </div>
+  </>
 )
 
 DatasetMain.propTypes = {
