0705b6bbaae0b3273123779198d1db8995dcb020
Nell Hardcastle <nell@squishymedia.com>
2019-03-14 13:55:46 -0700

    Fix missing dataset tools import.

--- packages/openneuro-app/src/scripts/datalad/fragments/dataset-tools.jsx
+++ packages/openneuro-app/src/scripts/datalad/fragments/dataset-tools.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { withRouter } from 'react-router-dom'
 import snapshotVersion from '../snapshotVersion.js'
 import DownloadButton from './tools/download-button.jsx'
 
