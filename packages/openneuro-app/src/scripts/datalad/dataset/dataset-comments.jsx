6a4a598ddb140d7930e5d3ccab898ea184d5f98c
Nell Hardcastle <nell@squishymedia.com>
2019-03-14 15:30:35 -0700

    Improve comments layout.

--- packages/openneuro-app/src/scripts/datalad/dataset/dataset-comments.jsx
+++ packages/openneuro-app/src/scripts/datalad/dataset/dataset-comments.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
-const DatasetComments = () => <div>Comments go here</div>
+const DatasetComments = () => (
+  <div className="col-xs-12 dataset-inner">
+    <hr />
+    Comments go here
+  </div>
+)
 
 export default DatasetComments
