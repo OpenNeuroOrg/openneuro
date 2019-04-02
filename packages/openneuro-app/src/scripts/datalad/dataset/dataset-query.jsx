27c00da4c12aa25d37b6e8281e329ec73c98d1f4
Nell Hardcastle <nell@squishymedia.com>
2019-04-02 12:51:18 -0700

    Implement DatasetQuery tests.

--- packages/openneuro-app/src/scripts/datalad/dataset/dataset-query.jsx
+++ packages/openneuro-app/src/scripts/datalad/dataset/dataset-query.jsx
@@ -6,7 +6,7 @@ import Spinner from '../../common/partials/spinner.jsx'
 import DatasetPage from './dataset-page.jsx'
 import * as DatasetQueryFragments from './dataset-query-fragments.js'
 
-const getDatasetPage = gql`
+export const getDatasetPage = gql`
   query dataset($datasetId: ID!) {
     dataset(id: $datasetId) {
       id
@@ -35,19 +35,27 @@ const getDatasetPage = gql`
   ${DatasetQueryFragments.DATASET_COMMENTS}
 `
 
+export const DatasetQueryRender = ({ loading, error, data }) => {
+  if (loading) {
+    return <Spinner text="Loading Dataset" active />
+  } else if (error) {
+    throw new Error(error)
+  } else {
+    return <DatasetPage dataset={data.dataset} />
+  }
+}
+
+DatasetQueryRender.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.object,
+  data: PropTypes.object,
+}
+
 const DatasetQuery = ({ match }) => (
   <Query
     query={getDatasetPage}
     variables={{ datasetId: match.params.datasetId }}>
-    {({ loading, error, data }) => {
-      if (loading) {
-        return <Spinner text="Loading Dataset" active />
-      } else if (error) {
-        throw new Error(error)
-      } else {
-        return <DatasetPage dataset={data.dataset} />
-      }
-    }}
+    {DatasetQueryRender}
   </Query>
 )
 
