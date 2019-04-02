eed61b845818b7b664173f20a187de7859216dc7
Nell Hardcastle <nell@squishymedia.com>
2019-04-02 13:13:37 -0700

    Fix linting bug in LeftSidebar tests.

--- packages/openneuro-app/src/scripts/datalad/dataset/__tests__/left-siderbar.spec.jsx
+++ packages/openneuro-app/src/scripts/datalad/dataset/__tests__/left-siderbar.spec.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import LeftSidebar, { SidebarRow } from '../left-sidebar.jsx'
 
 const fixedDate = new Date('2019-04-02T19:56:41.222Z')
