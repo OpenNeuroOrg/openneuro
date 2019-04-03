d55f6a200f303f5dddf285afae8bdf738e078ab2
Nell Hardcastle <nell@squishymedia.com>
2019-04-03 10:49:31 -0700

    Add DatasetUploaded rendering test.

---
+++ packages/openneuro-app/src/scripts/datalad/fragments/__tests__/dataset-uploaded.spec.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import DatasetUploaded from '../dataset-uploaded.jsx'
+
+describe('DatasetUploaded component', () => {
+  it('renders with common props', () => {
+    const wrapper = shallow(
+      <DatasetUploaded uploader={{ name: 'Tester' }} created="2016-11-06" />,
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+})
