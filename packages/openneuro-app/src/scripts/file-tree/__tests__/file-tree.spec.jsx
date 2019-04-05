e2332f9c23e368539e8a1296ab6f833f9ca125fb
Nell Hardcastle <nell@squishymedia.com>
2019-04-05 09:51:34 -0700

    Improve file tree directory toggle test.

--- packages/openneuro-app/src/scripts/file-tree/__tests__/file-tree.spec.jsx
+++ packages/openneuro-app/src/scripts/file-tree/__tests__/file-tree.spec.jsx
@@ -6,9 +6,24 @@ describe('FileTree component', () => {
   it('renders with default props', () => {
     expect(mount(<FileTree />)).toMatchSnapshot()
   })
-  it('expands when clicked', () => {
+  it('expands and closes when clicked', () => {
     const wrapper = mount(<FileTree name="Top Level" />)
+    expect(
+      wrapper
+        .find('button.btn-file-folder > i.type-icon')
+        .hasClass('fa-folder-open'),
+    ).toBe(false)
     wrapper.find('button').simulate('click')
-    expect(wrapper.find('Panel').hasClass('open')).toBe(true)
+    expect(
+      wrapper
+        .find('button.btn-file-folder > i.type-icon')
+        .hasClass('fa-folder-open'),
+    ).toBe(true)
+    wrapper.find('button').simulate('click')
+    expect(
+      wrapper
+        .find('button.btn-file-folder > i.type-icon')
+        .hasClass('fa-folder-open'),
+    ).toBe(false)
   })
 })
