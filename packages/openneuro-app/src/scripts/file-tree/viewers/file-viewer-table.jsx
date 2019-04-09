2213d6c53c2ac3e64071c93d2aa814709b3d737a
Nell Hardcastle <nell@squishymedia.com>
2019-04-09 12:15:36 -0700

    Resize table views for very tall layouts.

--- packages/openneuro-app/src/scripts/file-tree/viewers/file-viewer-table.jsx
+++ packages/openneuro-app/src/scripts/file-tree/viewers/file-viewer-table.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import { AutoSizer, Column, Table } from 'react-virtualized'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+
+const HalfViewport = styled.div`
+  height: 50vh;
+`
 
 const FileViewerTable = ({ tableData }) => (
-  <AutoSizer disableHeight>
-    {({ width }) => (
-      <Table
-        width={width}
-        height={800}
-        headerHeight={20}
-        rowHeight={30}
-        rowCount={tableData.length}
-        rowGetter={({ index }) => tableData[index]}>
-        {Object.keys(tableData[0]).map((colName, index) => (
-          <Column label={colName} dataKey={colName} key={index} width={100} />
-        ))}
-      </Table>
-    )}
-  </AutoSizer>
+  <HalfViewport>
+    <AutoSizer>
+      {({ width, height }) => (
+        <Table
+          width={width}
+          height={height}
+          headerHeight={20}
+          rowHeight={30}
+          rowCount={tableData.length}
+          rowGetter={({ index }) => tableData[index]}>
+          {Object.keys(tableData[0]).map((colName, index) => (
+            <Column label={colName} dataKey={colName} key={index} width={100} />
+          ))}
+        </Table>
+      )}
+    </AutoSizer>
+  </HalfViewport>
 )
 
 FileViewerTable.propTypes = {
