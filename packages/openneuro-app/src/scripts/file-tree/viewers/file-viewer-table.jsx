06e07809680095df7bbe2f4b61df531c3efb0d85
Nell Hardcastle <nell@squishymedia.com>
2019-04-09 12:07:20 -0700

    Resize table viewer to fit most screens better.

--- packages/openneuro-app/src/scripts/file-tree/viewers/file-viewer-table.jsx
+++ packages/openneuro-app/src/scripts/file-tree/viewers/file-viewer-table.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Column, Table } from 'react-virtualized'
+import { AutoSizer, Column, Table } from 'react-virtualized'
 import PropTypes from 'prop-types'
 
 const FileViewerTable = ({ tableData }) => (
-  <Table
-    width={300}
-    height={300}
-    headerHeight={20}
-    rowHeight={30}
-    rowCount={tableData.length}
-    rowGetter={({ index }) => tableData[index]}>
-    {Object.keys(tableData[0]).map((colName, index) => (
-      <Column label={colName} dataKey={colName} key={index} />
-    ))}
-  </Table>
+  <AutoSizer disableHeight>
+    {({ width }) => (
+      <Table
+        width={width}
+        height={800}
+        headerHeight={20}
+        rowHeight={30}
+        rowCount={tableData.length}
+        rowGetter={({ index }) => tableData[index]}>
+        {Object.keys(tableData[0]).map((colName, index) => (
+          <Column label={colName} dataKey={colName} key={index} width={100} />
+        ))}
+      </Table>
+    )}
+  </AutoSizer>
 )
 
 FileViewerTable.propTypes = {
