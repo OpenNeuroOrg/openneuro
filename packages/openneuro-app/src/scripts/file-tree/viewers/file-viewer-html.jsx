aae2ab49d616ea6e438b072834fdbf471ef24d21
Nell Hardcastle <nell@squishymedia.com>
2019-04-11 11:40:28 -0700

    Style HTML file viewer to use more screen area for content.

--- packages/openneuro-app/src/scripts/file-tree/viewers/file-viewer-html.jsx
+++ packages/openneuro-app/src/scripts/file-tree/viewers/file-viewer-html.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+
+const ScaledIframe = styled.iframe`
+  width: 100%;
+  height: 50vh;
+  border: none;
+`
 
 const FileViewerHtml = ({ data }) => {
   const decoder = new TextDecoder()
-  return <iframe srcDoc={decoder.decode(data)} />
+  return <ScaledIframe srcDoc={decoder.decode(data)} />
 }
 
 FileViewerHtml.propTypes = {
