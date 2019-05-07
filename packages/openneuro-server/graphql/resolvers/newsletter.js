5aaca5364ab7b5372c7c435c2f85f29ae6a4c960
David Nishikawa <david@squishymedia.com>
2019-05-07 10:13:53 -0700

    remove email server-side validation

--- packages/openneuro-server/graphql/resolvers/newsletter.js
+++ packages/openneuro-server/graphql/resolvers/newsletter.js
@@ -6,14 +6,12 @@
 
 import Newsletter from '../../models/newsletter'
 
-const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
 // finds or adds email to newsletter collection
 // returns true if email exists or is successfully created
 // returns false if email format is invalid or operation fails
 export const subscribeToNewsletter = async (obj, { email }) => {
-  // check if email format is valid
-  if (!emailPattern.test(email)) return false
+  // check if email is not empty
+  if (!email) return false
   // check if email exists in collection
   const exists = await new Promise(resolve => {
     Newsletter.findOne({ email }, (err, subscriber) => {
