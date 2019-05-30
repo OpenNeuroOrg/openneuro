b69ef6fbdc70a2804604f0c0b1fc9479fff5b1bc
Nell Hardcastle <nell@squishymedia.com>
2019-05-30 14:28:09 -0700

    Add Kubernetes support to get_docker_scale.py

--- get_docker_scale.py
+++ get_docker_scale.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 """Helper script to get the docker-compose offset from within a container."""
+import re
 import socket
 import dns.resolver
 import dns.reversename
 
-ip_addr = socket.gethostbyname(socket.gethostname())
-answer = dns.resolver.query(dns.reversename.from_address(ip_addr), "PTR")
-subdomain = str(answer[0]).split('.')[0]
-docker_compose_offset = subdomain.split('_')[-1]
-# Printed so this can be referenced as a script
-print(docker_compose_offset)
+hostname = socket.gethostname()
+matches = re.match(".*?-([0-9]+)$", hostname)
+if matches:
+  # Kubernetes
+  print(matches[1])
+else:
+  # docker-compose
+  ip_addr = socket.gethostbyname(hostname)
+  answer = dns.resolver.query(dns.reversename.from_address(ip_addr), "PTR")
+  subdomain = str(answer[0]).split('.')[0]
+  docker_compose_offset = subdomain.split('_')[-1]
+  # Printed so this can be referenced as a script
+  print(docker_compose_offset)
