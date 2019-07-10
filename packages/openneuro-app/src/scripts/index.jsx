1864b2d36a35cf28d339e3ad3345515662c1b763
Nell Hardcastle <nell@squishymedia.com>
2019-07-10 13:20:31 -0700

    Prevent global toast from redrawing until page reload.

--- packages/openneuro-app/src/scripts/index.jsx
+++ packages/openneuro-app/src/scripts/index.jsx
@@ -5,13 +5,8 @@ import Navbar from './nav/navbar.jsx'
 import Happybrowser from './common/partials/happybrowser.jsx'
 import Routes from './routes.jsx'
 import Uploader from './uploader/uploader.jsx'
-import { expiringBanner } from './utils/userNotify.js'
 
 const Index = () => {
-  expiringBanner(
-    'OpenNeuro will be unavailable for approximately 1 hour for planned maintenance on Wednesday, July 17th at 19:00 UTC',
-    new Date(1563519600 * 1000), // Friday, July 19th
-  )
   return (
     <Uploader>
       <div className="page">
