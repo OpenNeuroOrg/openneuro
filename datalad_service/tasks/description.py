6d6c4401c94c92d8cb83136afedb3c2bbc659942
Nell Hardcastle <nell@squishymedia.com>
2019-07-25 16:54:54 -0700

    Run validation on update_description
    Fixes a regression where validation is not run on automatic edits.

--- datalad_service/tasks/description.py
+++ datalad_service/tasks/description.py
@@ -1,6 +1,7 @@
 import json
 import os
 from datalad_service.common.celery import dataset_task
+from datalad_service.tasks.files import commit_files
 
 
 def edit_description(description, new_fields):
@@ -10,14 +11,15 @@ def edit_description(description, new_fields):
 
 
 @dataset_task
-def update_description(store, dataset, description_fields):
+def update_description(store, dataset, description_fields, name=None, email=None):
     ds = store.get_dataset(dataset)
     description = ds.repo.repo.git.show(
         'HEAD:dataset_description.json')
     description_json = json.loads(description)
     if description_json.get('License') != 'CC0':
-        description_fields = edit_description(description_fields, { 'License': 'CC0' })
-    if any (description_fields):
+        description_fields = edit_description(
+            description_fields, {'License': 'CC0'})
+    if any(description_fields):
         updated = edit_description(description_json, description_fields)
         path = os.path.join(store.get_dataset_path(
             dataset), 'dataset_description.json')
@@ -30,7 +32,8 @@ def update_description(store, dataset, description_fields):
             description_file.seek(0)
             description_file.truncate(0)
             description_file.write(json.dumps(updated, indent=4))
-        ds.add(path=path, message='update dataset_description')
+        # Commit new content, run validator
+        commit_files.run(store.annex_path, dataset, ['dataset_description.json'])
         return updated
     else:
         return description_json
