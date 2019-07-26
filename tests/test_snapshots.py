338d199584571748b27fd8e5a3f58d6686f28b16
Nell Hardcastle <nell@squishymedia.com>
2019-07-26 09:02:34 -0700

    Add test case for commit -> snapshot bug.

--- tests/test_snapshots.py
+++ tests/test_snapshots.py
@@ -27,6 +27,33 @@ def test_create_snapshot(client, new_dataset, celery_app):
     assert response.status == falcon.HTTP_OK
 
 
+def test_pre_snapshot_edit(client, new_dataset, celery_app):
+    ds_id = os.path.basename(new_dataset.path)
+    snapshot_id = '1.0.0'
+    file_data = json.dumps({
+        "BIDSVersion": "1.0.2",
+        "License": "CC0",
+        "Name": "Test fixture new dataset"
+    }, indent=4)
+    # Update a file
+    response = client.simulate_post(
+        '/datasets/{}/files/dataset_description.json'.format(ds_id), body=file_data)
+    assert response.status == falcon.HTTP_OK
+    # Commit changes
+    response = client.simulate_post(
+        '/datasets/{}/draft'.format(ds_id), params={"validate": "false"})
+    assert response.status == falcon.HTTP_OK
+    commit_ref = response.json['ref']
+    # Make a snapshot
+    response = client.simulate_post(
+        '/datasets/{}/snapshots/{}'.format(ds_id, snapshot_id), json={'skip_publishing': True})
+    assert response.status == falcon.HTTP_OK
+    # Validate that create_snapshot has not moved master commit
+    with open(os.path.join(new_dataset.path, '.git/refs/heads/master')) as fd:
+        current_ref = fd.read()[:-1]
+        assert commit_ref == current_ref
+
+
 def test_duplicate_snapshot(client, new_dataset, celery_app):
     ds_id = os.path.basename(new_dataset.path)
     snapshot_id = '2'
@@ -59,6 +86,7 @@ def test_get_snapshots(client, new_dataset, celery_app):
     assert result_doc['snapshots'][0]['tag'] == 'v1.0.0'
     assert result_doc['snapshots'][1]['tag'] == 'v2.0.0'
 
+
 def test_description_update(client, new_dataset, celery_app):
     key = 'ReferencesAndLinks'
     value = ['https://www.wikipedia.org']
@@ -68,7 +96,7 @@ def test_description_update(client, new_dataset, celery_app):
         },
         'skip_publishing': True
     })
-    
+
     ds_id = os.path.basename(new_dataset.path)
     update_response = client.simulate_post(
         '/datasets/{}/snapshots/{}'.format(ds_id, 'v1.0.0'), body=body)
