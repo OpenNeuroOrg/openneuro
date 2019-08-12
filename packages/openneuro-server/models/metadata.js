38f4afd96d727ca4c51d445328f6a418b8a0382a
David Nishikawa <david@squishymedia.com>
2019-08-11 22:44:41 -0700

    add Metadata model to mongo

---
+++ packages/openneuro-server/models/metadata.js
@@ -0,0 +1,77 @@
+import uuid from 'uuid'
+import mongoose from 'mongoose'
+
+const metadataSchema = new mongoose.Schema({
+  id: { type: String, default: uuid.v4 }, // OpenNeuro id
+  type: String,
+
+  datasetUrl: String, // @id type
+  datasetName: String,
+  firstSnapshotCreatedAt: Date,
+  latestSnapshotCreatedAt: Date,
+  subjectCount: Number,
+  modalities: [String],
+  dxStatus: [String],
+  ages: String,
+  tasksCompleted: Boolean,
+  trialCount: Number,
+  studyDesign: String,
+  studyDomain: String,
+  studyLongitudinal: String, // 'true' | 'false' | 'user input string'
+  dataProcessed: String, // 'true' | 'false' | 'user input string'
+  species: String,
+  associatedPaperDOI: String, // @id type
+  openneuroPaperDOI: String, // @id type
+  seniorAuthor: { // Person type
+    firstname: String,
+    lastname: String
+  },
+  adminUsers: String, // email type (@id type?)
+  notes: String,
+})
+
+metadataSchema.virtual('context').get(function() {
+  return {
+    id: 'http://schema.org/Text', // OpenNeuro id
+
+    datasetUrl: 'http://schema.org/Text', // @id type
+    datasetName: 'http://schema.org/Text',
+    firstSnapshotCreatedAt: 'http://schema.org/DateTime',
+    latestSnapshotCreatedAt: 'http://schema.org/DateTime',
+    subjectCount: 'http://schema.org/Number',
+    modalities: {
+      '@type': 'http://schema.org/Text',
+      '@container': '@list',
+    },
+    dxStatus: {
+      '@type': 'http://schema.org/Text',
+      '@container': '@list',
+    },
+    ages: 'http://schema.org/typicalAgeRange',
+    tasksCompleted: 'http://schema.org/Boolean',
+    trialCount: 'http://schema.org/Number',
+    studyDesign: 'http://schema.org/Text',
+    studyDomain: 'http://schema.org/Text',
+    studyLongitudinal: 'http://schema.org/Text', // 'true' | 'false' | 'user input string'
+    dataProcessed: 'http://schema.org/Text', // 'true' | 'false' | 'user input string'
+    species: 'http://schema.org/Text',
+    associatedPaperDOI: {
+      '@id': 'http://schema.org/url',
+      '@type': '@id',
+    }, // @id type
+    openneuroPaperDOI: {
+      '@id': 'http://schema.org/url',
+      '@type': '@id',
+    }, // @id type
+    seniorAuthor: { // Person type
+      firstName: 'http://schema.org/givenName',
+      lastName: 'http://schema.org/familyName'
+    },
+    adminUsers: 'http://schema.org/email', // email type (@id type?)
+    notes: 'http://schema.org/Text',
+  }
+})
+
+const Metadata = mongoose.model('Metadata', metadataSchema)
+
+export default Metadata
