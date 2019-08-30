cdd0bdf1d66cdc6428965758c6f9be1e3f3dfeae
Nell Hardcastle <nell@squishymedia.com>
2019-08-30 09:43:32 -0700

    Cleanup unused comment fragment.

--- packages/openneuro-app/src/scripts/datalad/dataset/comments-fragments.js
+++ packages/openneuro-app/src/scripts/datalad/dataset/comments-fragments.js
@@ -1,22 +1,5 @@
 import gql from 'graphql-tag'
 
-export const DATASET_COMMENT_FIELDS = gql`
-  fragment DatasetCommentFields on Comment {
-    id
-    text
-    createDate
-    user {
-      email
-    }
-    parent {
-      id
-    }
-    replies {
-      id
-    }
-  }
-`
-
 export const DATASET_COMMENTS = gql`
   fragment DatasetComments on Dataset {
     id
