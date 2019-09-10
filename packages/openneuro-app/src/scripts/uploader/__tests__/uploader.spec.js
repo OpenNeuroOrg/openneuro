bd2cd1f667bdc2bd542c229d3084fb05795a9c13
David Nishikawa <david@squishymedia.com>
2019-09-10 09:42:27 -0700

    update tests

--- packages/openneuro-app/src/scripts/uploader/__tests__/uploader.spec.js
+++ packages/openneuro-app/src/scripts/uploader/__tests__/uploader.spec.js
@@ -28,45 +28,4 @@ describe('uploader', () => {
       done.fail(e)
     }
   })
-  describe('_editName()', () => {
-    it('updates dataset_description.json correctly when a name is set', async done => {
-      const wrapper = shallow(<UploadClient />)
-      const instance = wrapper.instance()
-      const newName = 'Renamed'
-
-      // Simulate selectFiles
-      await instance.selectFiles({
-        files: [
-          TestFile(['file body one'], 'file_one'),
-          TestFile(['file body two'], 'file_two'),
-          TestFile(
-            ['{"Name": "Dataset Name", "BIDSVersion": "1.1.1"}'],
-            'dataset_description.json',
-          ),
-        ],
-      })
-
-      // Edit dataset_description
-      const editedFiles = await instance._editName(newName)
-      const descriptionFile = editedFiles.find(
-        f => f.name === 'dataset_description.json',
-      )
-
-      // Read file and check that the name is updated
-      const reader = new FileReader()
-      reader.onload = () => {
-        const description = JSON.parse(reader.result.toString())
-        try {
-          // Check for the new name
-          expect(description.Name).toBe(newName)
-          // Check that BIDSVersion is still there
-          expect(description.BIDSVersion).toBe('1.1.1')
-          done()
-        } catch (e) {
-          done.fail(e)
-        }
-      }
-      reader.readAsText(descriptionFile)
-    })
-  })
 })
