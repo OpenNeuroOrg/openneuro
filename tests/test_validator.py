25edd51571441fe9026770a6ddba2c0a627926de
Nell Hardcastle <nell@squishymedia.com>
2019-09-24 10:58:30 -0700

    Fix test_validator missing argument.

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -4,7 +4,7 @@ from .dataset_fixtures import *
 from datalad_service.tasks.validator import validate_dataset_sync
 
 def test_validator(new_dataset):
-    results = validate_dataset_sync(new_dataset.path)
+    results = validate_dataset_sync(new_dataset.path, 'HEAD')
     # new_dataset doesn't pass validation, should return an error
     assert 'issues' in results
     assert 'errors' in results['issues']
