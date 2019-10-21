9835dc7b0fe6357a64162245dc68ecfb98b96bbb
MatthewZito <matthewtzito@gmail.com>
2019-10-21 11:51:44 -0700

    custom modal for error boundary

--- packages/openneuro-app/src/scripts/errors/errorBoundary.jsx
+++ packages/openneuro-app/src/scripts/errors/errorBoundary.jsx
@@ -3,6 +3,49 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Modal } from '../utils/modal.jsx'
 import FreshdeskWidget from '../datalad/fragments/freshdesk-widget.jsx'
+import styled from '@emotion/styled'
+
+//styled components
+
+const Overlay = styled.div`
+  background: rgba(0, 0, 0, 0.8);
+  padding-top: 55px;
+  pointer-events: none;
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  -webkit-transition: opacity 400ms ease-in;
+  -moz-transition: opacity 400ms ease-in;
+  transition: opacity 400ms ease-in;
+  display: inline-block;
+  opacity: 100;
+  z-index: 99;
+`
+
+const ModalContainer = styled.div`
+  background-color: white;
+  height: 100%;
+  width: 100%;
+  position: relative;
+  margin: 0 auto;
+  padding: 3em;
+  pointer-events: auto;
+`
+
+const ExitButton = styled.a`
+  color: black;
+  font-size: 34px;
+  padding: 12px 12px;
+  padding-top: 20px;
+  position: absolute;
+  right: 0;
+  text-align: center;
+  pointer-events: auto;
+  top: 0;
+  z-index: 100;
+`
 
 // raises error if catchErrorIf returns true
 const getDerivedStateFromErrorOnCondition = (error, catchErrorIf) => {
@@ -47,6 +90,7 @@ class ErrorBoundary extends React.Component {
       ...prevState,
       supportModal: false,
     }))
+
   openSupportModalFromLink = e => {
     e.preventDefault()
     this.setState(prevState => ({
@@ -56,6 +100,7 @@ class ErrorBoundary extends React.Component {
   }
 
   render() {
+    const { supportModal } = this.state
     const { subject, description } = this.props
     const error = this.props.error || this.state.error
     return this.state.hasError ? (
@@ -69,23 +114,28 @@ class ErrorBoundary extends React.Component {
           </a>
           .
         </p>
-        <Modal show={this.state.supportModal} onHide={this.closeSupportModal}>
-          <Modal.Header closeButton>
-            <Modal.Title>Support</Modal.Title>
-          </Modal.Header>
-          <hr className="modal-inner" />
-          <Modal.Body>
-            To ensure that we can quickly help resolve this issue, please
-            provide as much detail as you can, including what you were trying to
-            accomplish when the error occurred.
-            <FreshdeskWidget
-              {...{ subject, description, error, sentryId: this.state.eventId }}
-            />
-          </Modal.Body>
-          <Modal.Footer>
-            <a onClick={this.closeSupportModal}>Close</a>
-          </Modal.Footer>
-        </Modal>
+        {supportModal && (
+          <Overlay>
+            <ModalContainer>
+              <ExitButton onClick={this.closeSupportModal}>&times;</ExitButton>
+              <h3>Support</h3>
+              <hr />
+              <div>
+                To ensure that we can quickly help resolve this issue, please
+                provide as much detail as you can, including what you were
+                trying to accomplish when the error occurred.
+              </div>
+              <FreshdeskWidget
+                {...{
+                  subject,
+                  description,
+                  error,
+                  sentryId: this.state.eventId,
+                }}
+              />
+            </ModalContainer>
+          </Overlay>
+        )}
       </>
     ) : (
       this.props.children
