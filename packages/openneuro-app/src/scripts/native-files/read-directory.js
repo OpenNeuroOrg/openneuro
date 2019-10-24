caf2594baaed21d227b557931996b4de510f7ddc
Nell Hardcastle <nell@squishymedia.com>
2019-10-24 13:58:06 -0700

    Cleanup unused native files modules.

--- packages/openneuro-app/src/scripts/native-files/read-directory.js
+++
@@ -1,8 +0,0 @@
-/**
- * Ask the user to select a directory and return entries
- */
-export const readDirectory = async () => {
-  const opts = { type: 'openDirectory' }
-  const handle = await window.chooseFileSystemEntries(opts)
-  return handle.getEntries()
-}
