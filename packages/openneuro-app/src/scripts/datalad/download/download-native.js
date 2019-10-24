58fcbbbc8b163f9fe24b8f5d338e5ada75c72ae1
Nell Hardcastle <nell@squishymedia.com>
2019-10-24 13:33:11 -0700

    Add test for native file download method.

--- packages/openneuro-app/src/scripts/datalad/download/download-native.js
+++ packages/openneuro-app/src/scripts/datalad/download/download-native.js
@@ -2,6 +2,27 @@ import { trackDownload } from './track-download.js'
 import { downloadUri } from './download-uri.js'
 
 /**
+ * Given a file, create any missing parent directories, obtain directory handle, and return the file handle within that
+ * @param {*} initialDirectoryHandle Native file API directoryEntry
+ * @param {string} path 'sub-01/anat/subject-image.nii.gz' style paths
+ */
+export const openFileTree = async (initialDirectoryHandle, path) => {
+  let directoryHandle = initialDirectoryHandle
+  // Get list of any parent directories
+  const pathTokens = path.split('/')
+  const dirTokens = pathTokens.slice(0, -1)
+  const filename = pathTokens.slice(-1)
+  if (dirTokens.length > 0) {
+    for (const token of dirTokens) {
+      directoryHandle = await directoryHandle.getDirectory(token, {
+        create: true,
+      })
+    }
+  }
+  return directoryHandle.getFile(filename, { create: true })
+}
+
+/**
  * Downloads a dataset via the native file API, skipping expensive compression if the browser supports it
  * @param {string} datasetId Accession number string for a dataset
  * @param {string} snapshotTag Snapshot tag name
@@ -10,27 +31,17 @@ export const downloadNative = (datasetId, snapshotTag) => async () => {
   const uri = downloadUri(datasetId, snapshotTag)
   const filesToDownload = await (await fetch(uri)).json()
   trackDownload(datasetId, snapshotTag)
-  // Obtain primary URL for each file
+  // Open user selected directory
   const dirHandle = await window.chooseFileSystemEntries({
     type: 'openDirectory',
   })
   for (const file of filesToDownload.files) {
-    let dirLevel = dirHandle
-    // Get list of any parent directories
-    const pathTokens = file.filename.split('/')
-    const dirTokens = pathTokens.slice(0, -1)
-    const filename = pathTokens.slice(-1)
-    if (dirTokens.length > 0) {
-      for (const token of dirTokens) {
-        dirLevel = await dirLevel.getDirectory(token, { create: true })
-      }
-    }
-    const fileHandle = await dirLevel.getFile(filename, { create: true })
+    const fileHandle = await openFileTree(dirHandle, file.filename)
     // Skip files which are already complete
     if (fileHandle.size == file.size) continue
     const writer = await fileHandle.createWriter()
     const ff = await fetch(file.urls.pop())
-    await writer.write(0, await ff.blob())
+    await writer.write(0, ff.arrayBuffer())
     await writer.close()
   }
 }
