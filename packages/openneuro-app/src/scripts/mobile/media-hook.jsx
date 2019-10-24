1cf83f7bb2f60d9647aec3f05f5cbba3b23f3e78
MatthewZito <matthewtzito@gmail.com>
2019-10-24 14:07:32 -0700

    update custom media hook

--- packages/openneuro-app/src/scripts/mobile/media-hook.jsx
+++ packages/openneuro-app/src/scripts/mobile/media-hook.jsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from 'react'
 
-// custom media query hook
+// custom media query hook - can be used anywhere by passing in desired breakpoint
 function useMedia(query) {
-  const [matches, setMatches] = useState(window.matchMedia(query).matches)
+  const [match, setmatch] = useState(window.matchMedia(query).match)
 
   useEffect(() => {
     const media = window.matchMedia(query)
-    if (media.matches !== matches) setMatches(media.matches)
-    const listener = () => setMatches(media.matches)
+    if (media.match !== match) setmatch(media.match)
+    const listener = () => setmatch(media.match)
     media.addListener(listener)
     return () => media.removeListener(listener)
-  }, [matches, query])
+  }, [match, query])
 
-  return matches
+  return match
 }
 
 export default useMedia
