7b6a9b16492608c24b217748098aae1a1bdc2a83
David Nishikawa <david@squishymedia.com>
2019-11-14 14:11:45 -0800

    add tests for file subscription and delete reducer

--- packages/openneuro-app/src/scripts/datalad/mutations/__tests__/delete.spec.jsx
+++ packages/openneuro-app/src/scripts/datalad/mutations/__tests__/delete.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import DeleteDataset from '../delete.jsx'
-import DeleteDir, { deleteFilesReducer } from '../delete-dir.jsx'
+import DeleteDir from '../delete-dir.jsx'
 
 describe('DeleteDataset mutation', () => {
   it('renders with common props', () => {
@@ -23,45 +23,5 @@ describe('DeleteDir mutation', () => {
       />,
     )
     expect(wrapper).toMatchSnapshot()
-  })
-  describe('deleteFilesReducer', () => {
-    it('removes all files in given fileTree from dataset.draft', () => {
-      const fileTree = {
-        files: [{ filename: 'A' }],
-        directories: [
-          {
-            files: [{ filename: 'B.ext' }, { filename: 'C.ext' }],
-            directories: [],
-            path: 'root_dir:removed_dir:dir1',
-          },
-          {
-            files: [{ filename: 'D' }],
-            directories: [
-              {
-                files: [{ filename: 'E.ext' }, { filename: 'F.ext' }],
-                directories: [],
-                path: 'root_dir:removed_dir:dir2:dir3',
-              },
-            ],
-            path: 'root_dir:removed_dir:dir2',
-          },
-        ],
-        path: 'root_dir:removed_dir',
-      }
-      const draft = {
-        files: [
-          { filename: 'root_dir/removed_dir/A' },
-          { filename: 'root_dir/removed_dir/dir1/B.ext' },
-          { filename: 'root_dir/removed_dir/dir1/C.ext' },
-          { filename: 'root_dir/removed_dir/dir2/D' },
-          { filename: 'root_dir/removed_dir/dir2/dir3/E.ext' },
-          { filename: 'root_dir/removed_dir/dir2/dir3/F.ext' },
-          { filename: 'root_dir/last_lonely_file' },
-        ],
-      }
-      const updatedDraft = deleteFilesReducer(fileTree, draft)
-      expect(updatedDraft.files.length).toBe(1)
-      expect(updatedDraft.files[0].filename).toBe('root_dir/last_lonely_file')
-    })
   })
 })
