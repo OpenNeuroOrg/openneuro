f3052ead917004335cc23a2000ec245860b14117
David Nishikawa <david@squishymedia.com>
2020-01-06 13:21:01 -0800

    make autoenable no for private datasets

--- datalad_service/common/s3.py
+++ datalad_service/common/s3.py
@@ -51,12 +51,13 @@ def generate_s3_annex_options(dataset, realm):
         'partsize=1GiB',
         'encryption=none',
         'fileprefix={}/'.format(dataset_id),
-        'autoenable=yes',
+        'autoenable=no',
     ]
     if realm == DatasetRealm.PUBLIC:
         public = getattr(datalad_service.config, 'DATALAD_S3_PUBLIC_ON_EXPORT')
         if public == 'yes':
             annex_options += [
+                'autoenable=yes',
                 'publicurl=http://{}.s3.amazonaws.com/'.format(realm.s3_bucket),
             ]
     else:
