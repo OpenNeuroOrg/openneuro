3e3622a966849984106f4fc7d01b31a99eda557d
Nell Hardcastle <nell@squishymedia.com>
2020-01-10 12:51:22 -0800

    Switch to graphql-compose as pre-requisite for elasticsearch schema.
    This makes it easier to programmically extend the schema with search
    fields defined by the structure of our ElasticSearch indexes.

--- packages/openneuro-server/graphql/resolvers/subscriptions.js
+++ packages/openneuro-server/graphql/resolvers/subscriptions.js
@@ -10,48 +10,48 @@ import { withFilter } from 'graphql-subscriptions'
 const filterDatasetId = (payload, variables) =>
   payload.datasetId === variables.datasetId
 
-export const datasetCreated = {
+export const datasetCreated = () => ({
   subscribe: () => pubsub.asyncIterator('datasetCreated'),
-}
+})
 
-export const datasetDeleted = {
+export const datasetDeleted = () => ({
   subscribe: () => pubsub.asyncIterator('datasetDeleted'),
-}
+})
 
-export const snapshotAdded = {
+export const snapshotAdded = () => ({
   subscribe: withFilter(
     () => pubsub.asyncIterator('snapshotAdded'),
     filterDatasetId,
   ),
-}
+})
 
-export const snapshotDeleted = {
+export const snapshotDeleted = () => ({
   subscribe: withFilter(
     () => pubsub.asyncIterator('snapshotDeleted'),
     filterDatasetId,
   ),
-}
+})
 
-export const draftUpdated = {
+export const draftUpdated = () => ({
   subscribe: withFilter(
     () => pubsub.asyncIterator('draftUpdated'),
     filterDatasetId,
   ),
-}
+})
 
-export const permissionsUpdated = {
+export const permissionsUpdated = () => ({
   subscribe: withFilter(
     () => pubsub.asyncIterator('permissionsUpdated'),
     filterDatasetId,
   ),
-}
+})
 
-export const filesUpdated = {
+export const filesUpdated = () => ({
   subscribe: withFilter(
     () => pubsub.asyncIterator('filesUpdated'),
     filterDatasetId,
   ),
-}
+})
 
 const Subscription = {
   datasetCreated,
