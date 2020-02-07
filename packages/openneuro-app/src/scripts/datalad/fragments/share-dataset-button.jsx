3c875e1df6cd611ee68c97a15429aef3800248a8
Matthew <matthew@squishymedia.com>
2020-02-07 12:41:12 -0800

    update share syntax

--- packages/openneuro-app/src/scripts/datalad/fragments/share-dataset-button.jsx
+++ packages/openneuro-app/src/scripts/datalad/fragments/share-dataset-button.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import WarnButton from '../../common/forms/warn-button.jsx'
 
-const ShareDatasetLink = ({ datasetId }) => {
+const ShareDatasetLink = props => {
+  let { url } = props.url
   return (
-    <button
-      className="btn-modal-action"
-      onClick={async () => {
-        await ShareDatasetPermissions({
-          variables: { datasetId, userEmail, level: metadata },
-        })
-        done()
-      }}>
-      Share
-    </button>
+    <WarnButton
+      tooltip="Share Dataset"
+      // icon={following ? 'fa-tag icon-minus' : 'fa-tag icon-plus'}
+      warn={false}
+      action={async () => {
+        try {
+          ;(await window.navigator) && window.navigator.share(url)
+        } catch (err) {
+          console.error(err)
+        }
+        window.alert('Success!')
+      }}></WarnButton>
   )
 }
 
 ShareDatasetLink.propTypes = {
   datasetId: PropTypes.string,
+  url: PropTypes.string,
 }
 
 export default ShareDatasetLink
