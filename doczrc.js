ab3e0b45b1b5d79e0f7ada0561868cbdeac1da17
Nell Hardcastle <nell@squishymedia.com>
2020-05-13 14:11:41 -0700

    Add gatsby-plugin-s3 for documentation site.

--- doczrc.js
+++ doczrc.js
@@ -2,4 +2,14 @@ export default {
   title: 'OpenNeuro Documentation',
   typescript: true,
   menu: ['Introduction', 'GraphQL API', 'Command Line Interface'],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: 'docs.openneuro.org',
+        protocol: 'https',
+        hostname: 'docs.openneuro.org',
+      },
+    },
+  ],
 }
