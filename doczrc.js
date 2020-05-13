a9a2c80a9ae25a0ca602c0aad8c41562ed8f7e2a
Nell Hardcastle <nell@squishymedia.com>
2020-05-13 14:27:00 -0700

    Add description to documentation site.

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,6 @@
 export default {
   title: 'OpenNeuro Documentation',
+  description: 'User and developer documentation for the OpenNeuro platform',
   typescript: true,
   menu: ['Introduction', 'GraphQL API', 'Command Line Interface'],
   plugins: [
