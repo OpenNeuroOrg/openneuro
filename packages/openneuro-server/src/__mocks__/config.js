fc1693bde0373d3bc6dbb180c9318db3914fcaa1
Nell Hardcastle <nell@squishymedia.com>
2020-06-01 16:17:05 -0700

    Fix new URL scheme in tests for datalad-service.

--- packages/openneuro-server/src/__mocks__/config.js
+++ packages/openneuro-server/src/__mocks__/config.js
@@ -1,7 +1,7 @@
 const config = jest.genMockFromModule('../config.js').default
 
 config.auth.jwt.secret = '123456'
-config.datalad.uri = 'datalad-0'
+config.datalad.uri = 'datalad'
 config.datalad.workers = 4
 config.mongo.url = 'mongodb://'
 
