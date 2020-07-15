66bd0d6a713264adc44ed84decb910def582f25d
Nell Hardcastle <nell@squishymedia.com>
2020-07-14 17:53:56 -0700

    Fix anonymous API call regression from #1640

--- packages/openneuro-server/src/libs/authentication/jwt.js
+++ packages/openneuro-server/src/libs/authentication/jwt.js
@@ -6,7 +6,7 @@ import User from '../../models/user'
 import config from '../../config.js'
 
 // Helper to generate a JWT containing user info
-export const addJWT = config => (user, expiration = 60 * 60 * 24 * 7) => {
+export const addJWT = (config) => (user, expiration = 60 * 60 * 24 * 7) => {
   const token = jwt.sign(
     {
       sub: user.id,
@@ -39,7 +39,7 @@ const requestNewAccessToken = (jwtProvider, refreshToken) =>
  * Extract the JWT from a cookie
  * @param {Object} req
  */
-export const jwtFromRequest = req => {
+export const jwtFromRequest = (req) => {
   if (req.cookies && req.cookies.accessToken) {
     return req.cookies.accessToken
   } else {
@@ -47,17 +47,17 @@ export const jwtFromRequest = req => {
   }
 }
 
-export const decodeJWT = token => {
+export const decodeJWT = (token) => {
   return jwt.decode(token)
 }
 
-const parsedJwtFromRequest = req => {
+const parsedJwtFromRequest = (req) => {
   const jwt = jwtFromRequest(req)
   if (jwt) return decodeJWT(jwt)
   else return null
 }
 
-const refreshToken = async jwt => {
+const refreshToken = async (jwt) => {
   const user = await User.findOne({ id: jwt.sub, provider: jwt.provider })
   if (user && user.refresh) {
     const refreshToken = decrypt(user.refresh)
@@ -73,7 +73,7 @@ const refreshToken = async jwt => {
 // if user does not exist, continue
 export const authenticate = async (req, res, next) => {
   const jwt = parsedJwtFromRequest(req)
-  if (Date.now() > jwt.exp * 1000) {
+  if (jwt && Date.now() > jwt.exp * 1000) {
     const token = await refreshToken(jwt)
     if (token) {
       req.cookies.accessToken = token
@@ -96,6 +96,6 @@ export const authSuccessHandler = (req, res, next) => {
   return next()
 }
 
-export const generateDataladCookie = config => user => {
+export const generateDataladCookie = (config) => (user) => {
   return user ? `accessToken=${addJWT(config)(user).token}` : ''
 }
