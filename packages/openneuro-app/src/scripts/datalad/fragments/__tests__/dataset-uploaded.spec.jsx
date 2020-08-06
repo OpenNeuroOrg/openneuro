c628604d8e0e9d10ecbc4c668085624adec45f34
David Nishikawa <david@squishymedia.com>
2020-08-05 22:54:41 -0700

    fix snapshot

--- packages/openneuro-app/src/scripts/datalad/fragments/__tests__/dataset-uploaded.spec.jsx
+++ packages/openneuro-app/src/scripts/datalad/fragments/__tests__/dataset-uploaded.spec.jsx
@@ -5,7 +5,11 @@ import DatasetUploaded from '../dataset-uploaded.jsx'
 describe('DatasetUploaded component', () => {
   it('renders with common props', () => {
     const wrapper = shallow(
-      <DatasetUploaded uploader={{ name: 'Tester' }} created="2016-11-06" />,
+      <DatasetUploaded
+        uploader={{ name: 'Tester' }}
+        created="2016-11-06"
+        testDifference="awhile ago"
+      />,
     )
     expect(wrapper).toMatchSnapshot()
   })
