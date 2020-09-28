122aa3a94f9dc21f1cfceca9381114f154063952
Nell Hardcastle <nell@squishymedia.com>
2020-09-28 13:15:30 -0700

    Remove origin trial tokens for native file API.

--- packages/openneuro-app/src/scripts/datalad/download/__tests__/download-native.spec.js
+++ packages/openneuro-app/src/scripts/datalad/download/__tests__/download-native.spec.js
@@ -5,12 +5,14 @@ describe('dataset/download - native file API method', () => {
     it('iterates over directory to correct depth', async () => {
       const mockDirectoryHandle = {}
       const mockFile = Symbol('mockFile')
-      mockDirectoryHandle.getDirectory = jest.fn(() => mockDirectoryHandle)
-      mockDirectoryHandle.getFile = jest.fn(() => mockFile)
+      mockDirectoryHandle.getDirectoryHandle = jest.fn(
+        () => mockDirectoryHandle,
+      )
+      mockDirectoryHandle.getFileHandle = jest.fn(() => mockFile)
       expect(
         await openFileTree(mockDirectoryHandle, 'sub-01/anat/something.nii.gz'),
       ).toBe(mockFile)
-      expect(mockDirectoryHandle.getDirectory).toHaveBeenCalledTimes(2)
+      expect(mockDirectoryHandle.getDirectoryHandle).toHaveBeenCalledTimes(2)
     })
   })
 })
