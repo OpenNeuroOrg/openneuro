a0cee9b559013cdf135bf9a14719b5e32367b5d9
Nell Hardcastle <nell@squishymedia.com>
2020-10-21 09:30:30 -0700

    Fix TypeScript build issue with openneuro-server

--- packages/openneuro-app/jestsetup.ts
+++ packages/openneuro-app/jestsetup.ts
@@ -12,10 +12,6 @@ Enzyme.configure({ adapter: new Adapter() })
 // Run all tests in virtual Katmandu (UTC +05:45)
 moment.tz.setDefault('Asia/Katmandu')
 
-global.React = React
-global.shallow = shallow
-global.render = render
-global.mount = mount
 global.fetch = fetch
 
 if (!Object.fromEntries) {
