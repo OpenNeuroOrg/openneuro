561557a59fb5360c2ecfdc095736e53ce8748ea3
David Nishikawa <david@squishymedia.com>
2021-01-27 01:41:08 -0800

    fix yarn tests

--- packages/openneuro-app/src/scripts/datalad/dashboard/__tests__/dashboard.spec.jsx
+++ packages/openneuro-app/src/scripts/datalad/dashboard/__tests__/dashboard.spec.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { StaticRouter as Router } from 'react-router-dom'
+import { mount, shallow } from 'enzyme'
+import { ApolloProvider } from '@apollo/client'
 import Dashboard from '../dashboard.jsx'
 
 describe('Dashboard', () => {
   it('my datasets renders successfully', () => {
-    expect(shallow(<Dashboard />)).toMatchSnapshot()
+    const wrapper = mount(
+      <ApolloProvider client={{}}>
+        <Router initialEntries={['/']}>
+          <Dashboard />
+        </Router>
+      </ApolloProvider>,
+    )
+    expect(wrapper).toMatchSnapshot()
   })
   it('public dashboard renders successfully', () => {
     expect(shallow(<Dashboard public />)).toMatchSnapshot()
