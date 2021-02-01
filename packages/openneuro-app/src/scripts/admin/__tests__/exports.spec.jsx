3633f6d5f8a9216702b45c883b37fd981da4db00
David Nishikawa <david@squishymedia.com>
2021-01-31 23:28:10 -0800

    fix exports snapshot

--- packages/openneuro-app/src/scripts/admin/__tests__/exports.spec.jsx
+++ packages/openneuro-app/src/scripts/admin/__tests__/exports.spec.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import { ApolloProvider } from '@apollo/client'
 import Exports from '../../datalad/routes/admin-exports.jsx'
 
 describe('Dashboard', () => {
   it('my datasets renders successfully', () => {
-    expect(shallow(<Exports />)).toMatchSnapshot()
+    const wrapper = mount(
+      <ApolloProvider client={{}}>
+        <Exports />
+      </ApolloProvider>,
+    )
+    expect(wrapper).toMatchSnapshot()
   })
 })
