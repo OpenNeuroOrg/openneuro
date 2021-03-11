e0da4dbd820a0aed69ce1ae0cfaf51b1b4eb6bce
David Nishikawa <david@squishymedia.com>
2021-03-10 17:26:27 -0800

    Add test for deleteFiles resolver.

--- packages/openneuro-server/src/graphql/resolvers/__tests__/dataset.spec.js
+++ packages/openneuro-server/src/graphql/resolvers/__tests__/dataset.spec.js
@@ -1,6 +1,8 @@
 import mockingoose from 'mockingoose'
+import request from 'superagent'
 import * as ds from '../dataset'
 
+jest.mock('superagent')
 jest.mock('../../../config.js')
 
 describe('dataset resolvers', () => {
@@ -44,6 +46,43 @@ describe('dataset resolvers', () => {
       expect(sorted[0].id).toBe(1)
       expect(sorted[1].id).toBe(2)
       expect(sorted[2].id).toBe(3)
+    })
+  })
+  describe('deleteFiles', () => {
+    beforeEach(() => {
+      mockingoose.resetAll()
+      request.post.mockClear()
+    })
+    it('makes correct delete call to datalad', done => {
+      // pass checkDatasetExists()
+      mockingoose.Dataset.toReturn(true, 'count')
+      // capture and check datalad delete request
+      request.del = url => ({
+        query: ({ recursive }) => ({
+          set: (header1, headerValue1) => ({
+            set: async () => {
+              expect(url).toEqual(
+                'http://datalad-0/datasets/ds999999/files/:sub-99',
+              )
+              expect(recursive).toBe(true)
+              expect(header1).toEqual('Cookie')
+              expect(headerValue1).toMatch(/^accessToken=/)
+            },
+          }),
+        }),
+      })
+
+      ds.deleteFiles(
+        null,
+        { datasetId: 'ds999999', path: '/sub-99' },
+        {
+          user: 'a_user_id',
+          userInfo: {
+            // bypass permission checks
+            admin: true,
+          },
+        },
+      ).then(() => done())
     })
   })
 })
