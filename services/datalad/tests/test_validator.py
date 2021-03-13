52d32cbdfa3192017566a535dd5a4599b15c8e54
Tommy Tran <tommy@squishymedia.com>
2021-03-12 17:12:20 -0800

    updated test with mock logger

--- services/datalad/tests/test_validator.py
+++ services/datalad/tests/test_validator.py
@@ -3,9 +3,12 @@ import json
 from .dataset_fixtures import *
 from datalad_service.tasks.validator import validate_dataset_sync
 
+class MockLogger:
+    def log():
+        pass
 
 def test_validator(new_dataset):
-    results = validate_dataset_sync(new_dataset.path, 'HEAD')
+    results = validate_dataset_sync(new_dataset.path, 'HEAD', MockLogger())
     # new_dataset doesn't pass validation, should return an error
     assert 'issues' in results
     assert 'errors' in results['issues']
