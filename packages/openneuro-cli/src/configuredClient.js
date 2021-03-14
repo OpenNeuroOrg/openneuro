3d411124ef653822e27464533ceb4c24cfffa277
Chris Thompson <chris@233analytics.com>
2021-03-13 23:12:00 -0600

    changed client reference

--- packages/openneuro-cli/src/configuredClient.js
+++ packages/openneuro-cli/src/configuredClient.js
@@ -1,4 +1,4 @@
-import { createClient } from 'openneuro-client'
+import { createClient } from '../../openneuro-client'
 import { getToken, getUrl } from './config.js'
 import { version } from '../package.json'
 
