b91f64d4906e36d094229f9d70611925624ec631
Nell Hardcastle <nell@squishymedia.com>
2021-03-26 19:50:10 -0700

    Disable code-splitting because of SSR conflicts.

--- packages/openneuro-app/src/ssr.ts
+++ packages/openneuro-app/src/ssr.ts
@@ -41,7 +41,6 @@ async function createServer(): Promise<void> {
       // 4. render the app HTML. This assumes entry-server.js's exported `render`
       //    function calls appropriate framework SSR APIs,
       //    e.g. ReactDOMServer.renderToString()
-      console.log(url)
       const appHtml = await render(url, req['universalCookies'])
 
       // 5. Inject the app-rendered HTML into the template.
