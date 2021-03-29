494417af2518d50df625c9d853b4814698e3c10f
Tommy Tran <tommy@squishymedia.com>
2021-03-29 10:54:14 -0700

    updated test

--- services/datalad/tests/test_validator.py
+++ services/datalad/tests/test_validator.py
@@ -2,14 +2,14 @@ import json
 
 from .dataset_fixtures import *
 from datalad_service.tasks.validator import validate_dataset_sync
+from unittest.mock import Mock
 
-class MockLogger:
-    def log():
-        pass
+class MockLogger: 
+    pass
 
 def test_validator(new_dataset):
-    results = validate_dataset_sync(new_dataset.path, 'HEAD', MockLogger())
-    # new_dataset doesn't pass validation, should return an error
-    assert 'issues' in results
-    assert 'errors' in results['issues']
-    assert results['issues']['errors'][0]['key'] == 'QUICK_VALIDATION_FAILED'
+    logger = MockLogger()
+    logger.log = Mock()
+    validate_dataset_sync(new_dataset.path, 'HEAD', logger)
+    # new_dataset doesn't pass validation, should catch the error and log it
+    assert logger.log.called
