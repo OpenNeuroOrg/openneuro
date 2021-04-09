3d9a08a2c97481f019dd023944db88a828a31d73
Nell Hardcastle <nell@squishymedia.com>
2021-04-08 21:52:48 -0700

    Rework configuration to inject at runtime in production builds.

--- packages/openneuro-app/src/ssr.ts
+++ packages/openneuro-app/src/ssr.ts
@@ -20,6 +20,25 @@ self.addEventListener('activate', function(e) {
     })
 })`
 
+globalThis.OpenNeuroConfig = {
+  CRN_SERVER_URL: process.env.CRN_SERVER_URL,
+  GRAPHQL_URI: process.env.GRAPHQL_URI,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GLOBUS_CLIENT_ID: process.env.GLOBUS_CLIENT_ID,
+  ORCID_CLIENT_ID: process.env.ORCID_CLIENT_ID,
+  ORCID_URI: process.env.ORCID_URI,
+  ORCID_REDIRECT_URI: process.env.ORCID_REDIRECT_URI,
+  GOOGLE_TRACKING_IDS: process.env.GOOGLE_TRACKING_IDS,
+  ENVIRONMENT: process.env.ENVIRONMENT,
+  SUPPORT_URL: process.env.SUPPORT_URL,
+  DATALAD_GITHUB_ORG: process.env.DATALAD_GITHUB_ORG,
+  AWS_S3_PUBLIC_BUCKET: process.env.AWS_S3_PUBLIC_BUCKET,
+}
+
+const configScript = `window.OpenNeuroConfig = ${JSON.stringify(
+  globalThis.OpenNeuroConfig,
+)}`
+
 async function createServer(): Promise<void> {
   const app = express()
 
@@ -48,6 +67,12 @@ async function createServer(): Promise<void> {
           .status(200)
           .set({ 'Content-Type': 'application/javascript' })
           .end(selfDestroyingServiceWorker)
+        return
+      } else if (url === '/config.js') {
+        res
+          .status(200)
+          .set({ 'Content-Type': 'application/javascript' })
+          .end(configScript)
         return
       }
 
