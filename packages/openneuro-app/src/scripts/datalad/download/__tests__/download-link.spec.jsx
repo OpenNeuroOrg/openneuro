2ecf990df4142b69d639d5f1970b4a0793161bfe
Tommy Tran <tommy@squishymedia.com>
2021-04-19 04:31:17 -0700

    updated tests

--- packages/openneuro-app/src/scripts/datalad/download/__tests__/download-link.spec.jsx
+++ packages/openneuro-app/src/scripts/datalad/download/__tests__/download-link.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import DownloadLink from '../download-link.jsx'
-
+import { MockedProvider } from '@apollo/client/testing'
 const defProps = {
   datasetId: 'ds000001',
   snapshotTag: '1.0.0',
@@ -9,7 +9,11 @@ const defProps = {
 
 describe('dataset/download/DownloadLink', () => {
   it('renders successfully', () => {
-    const wrapper = shallow(<DownloadLink {...defProps} />)
+    const wrapper = shallow(
+      <MockedProvider>
+        <DownloadLink {...defProps} />
+      </MockedProvider>,
+    )
     expect(wrapper).toMatchSnapshot()
   })
 })
