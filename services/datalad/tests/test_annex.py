9e4b3b1dd87fac3b2d5cfd8c146ed9af864ae3fb
David Nishikawa <david@squishymedia.com>
2021-04-24 12:01:27 -0700

    Remove unecessary print in test.

--- services/datalad/tests/test_annex.py
+++ services/datalad/tests/test_annex.py
@@ -35,7 +35,6 @@ def test_get_ls_tree_line():
     symlinkObjects = []
     read_ls_tree_line("""100644 blob a786c385bd1812410d01177affb6ce834d85facd     459	dataset_description.json""",
                       files, symlinkFilenames, symlinkObjects)
-    print(files)
     assert files == [
         {
             'filename': 'dataset_description.json',
