78bb73ec19122e6981c8d2a3fb66fa1c4461797d
Tommy Tran <tommy@squishymedia.com>
2021-04-30 03:34:11 -0700

    made AggregateCount component for PublicDatasets and Participants

---
+++ packages/openneuro-components/src/aggregate-count/AggregateCount.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export interface AggregateCountProps {
+  count?: number
+  type: 'publicDataset' | 'participants'
+}
+
+export const AggregateCount = ({ count, type }: AggregateCountProps) => {
+  const textContent: string = {
+    publicDataset: 'Public Dataset',
+    participants: 'Participant',
+  }[type]
+
+  return (
+    <div>
+      <span>{count.toLocaleString()}</span> {textContent}{count !== 1 && 's'}
+    </div>
+  )
+}
