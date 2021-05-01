1c8199c55d7a36962ab3eb2f27c4bed19c33f46b
Gregory Noack <gregorynoack@thinknoack.com>
2021-04-30 20:13:38 -0700

    created tooltip

---
+++ packages/openneuro-components/src/Tooltip/Tooltip.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import './tooltip.scss'
+
+export interface TooltipProps {
+  tooltip: string
+  flow: 'up' | 'down' | 'left' | 'right'
+}
+
+export const Tooltip = ({
+  children,
+  tooltip,
+  flow,
+  ...props
+}: TooltipProps) => {
+  const myRef = React.createRef<HTMLSpanElement>()
+  useEffect(() => {
+    const placement = myRef.current.offsetTop < 150 ? 'down' : flow
+    myRef.current.setAttribute('data-flow', placement)
+  }, [])
+
+  return (
+    <span ref={myRef} data-tooltip={tooltip} {...props} data-flow={flow}>
+      {children}
+    </span>
+  )
+}
