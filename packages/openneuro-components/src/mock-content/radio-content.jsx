a5c38482462decef47a31c8a83c51abd613b9ead
Gregory Noack <gregorynoack@thinknoack.com>
2021-05-01 20:13:55 -0700

    upate tp radio mock content

--- packages/openneuro-components/src/mock-content/radio-content.jsx
+++ packages/openneuro-components/src/mock-content/radio-content.jsx
@@ -3,18 +3,15 @@ import React from 'react'
 export const RadioContent = [
   {
     label: 'All',
-    name: 'example',
     value: 'All',
   },
 
   {
     label: 'My Bookmarks',
-    name: 'example',
     value: 'mybookmarks',
   },
   {
     label: 'Shared with me',
-    name: 'example',
     value: 'shared',
   },
 ]
