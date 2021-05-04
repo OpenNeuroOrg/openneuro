6cc71c7a691be0b3ff622e6055e609a10a453aa6
Gregory Noack <gregorynoack@thinknoack.com>
2021-05-04 15:17:51 -0700

    updat isopen to use callback and adding removeIsOpen function

--- packages/openneuro-components/src/dropdown/Dropdown.tsx
+++ packages/openneuro-components/src/dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import './dropdown.scss'
 
 export interface DropdownProps {
@@ -7,12 +7,29 @@ export interface DropdownProps {
 }
 
 export const Dropdown = ({ children, label }: DropdownProps) => {
-  const [isOpen, setOpen] = React.useState(false)
+  const [isOpen, setIsOpen] = React.useState(false)
+  console.log(isOpen)
+
+  function removeIsOpen(ref) {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setIsOpen(prevIsOpen => false)
+        }
+      }
+      document.addEventListener('mousedown', handleClickOutside)
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside)
+      }
+    }, [ref])
+  }
+  const wrapperRef = useRef(null)
+  removeIsOpen(wrapperRef)
   return (
-    <div className="dropdown-wrapper" onClick={() => setOpen(!isOpen)}>
+    <div className="dropdown-wrapper" ref={wrapperRef}>
       <div
         className={`toggle ${isOpen ? 'active' : ''}`}
-        onClick={() => setOpen(!isOpen)}>
+        onClick={() => setIsOpen(prevIsOpen => !prevIsOpen)}>
         <span>{label}</span>
       </div>
       <div className={`menu ${isOpen ? 'expanded' : 'collapsed'}`}>
