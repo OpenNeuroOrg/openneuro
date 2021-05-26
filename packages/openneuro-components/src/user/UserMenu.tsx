2dd31f3334bc65fc11933383eee495569b238f1a
Gregory Noack <gregorynoack@thinknoack.com>
2021-05-26 13:34:26 -0700

    update mock content

--- packages/openneuro-components/src/user/UserMenu.tsx
+++ packages/openneuro-components/src/user/UserMenu.tsx
@@ -11,7 +11,7 @@ export interface UserMenuProps {
     name: string
     admin: boolean
   }
-  signOutAndRedirect: (history: any) => void
+  signOutAndRedirect: () => void
 }
 
 export const UserMenu = ({ profile, signOutAndRedirect }: UserMenuProps) => {
