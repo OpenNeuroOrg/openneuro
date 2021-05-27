fc68e1de52bb02391bd2ce054814defa642799e9
David Nishikawa <david@squishymedia.com>
2021-05-27 14:16:47 -0700

    Add endpoints for enabling/disabling features.

---
+++ packages/openneuro-server/src/handlers/feature-flags.js
@@ -0,0 +1,14 @@
+const feature = {
+  REDESIGN_2021: 'redesign_2021',
+}
+
+export const setFlagRedesign2021 = (req, res) => {
+  res
+    .cookie(feature.REDESIGN_2021, true)
+    .send(`You have enabled feature ${feature.REDESIGN_2021}.`)
+}
+
+export const unsetFlagRedesign2021 = (req, res) => {
+  res.clearCookie(feature.REDESIGN_2021)
+  res.send(`You have disabled feature ${feature.REDESIGN_2021}.`)
+}
