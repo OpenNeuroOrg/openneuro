75b55081caaab1ebe2789fa0e4c75a8c796f1822
David Nishikawa <david@squishymedia.com>
2021-05-27 15:11:05 -0700

    Redesign endpoints redirect to homepage.

--- packages/openneuro-server/src/handlers/feature-flags.js
+++ packages/openneuro-server/src/handlers/feature-flags.js
@@ -3,12 +3,10 @@ const feature = {
 }
 
 export const setFlagRedesign2021 = (req, res) => {
-  res
-    .cookie(feature.REDESIGN_2021, true)
-    .send(`You have enabled feature ${feature.REDESIGN_2021}.`)
+  res.cookie(feature.REDESIGN_2021, true).redirect('/')
 }
 
 export const unsetFlagRedesign2021 = (req, res) => {
   res.clearCookie(feature.REDESIGN_2021)
-  res.send(`You have disabled feature ${feature.REDESIGN_2021}.`)
+  res.redirect('/')
 }
