da718d2e64e7100c565003a16a8d01c19dc187f2
David Nishikawa <david@squishymedia.com>
2021-06-07 21:14:17 -0700

    Fix test imports.

--- packages/openneuro-app/src/scripts/authentication/__tests__/profile.spec.js
+++ packages/openneuro-app/src/scripts/authentication/__tests__/profile.spec.js
@@ -1,4 +1,4 @@
-import { parseJwt } from '../profile.js'
+import { parseJwt } from '../../refactor_2021/authentication/profile.js'
 
 const asciiToken =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
