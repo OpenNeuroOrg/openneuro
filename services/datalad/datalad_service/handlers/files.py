9ebeeb228b28e4a29eac4d3a55527d6f2da7887d
Nell Hardcastle <nell@squishymedia.com>
2021-06-15 13:31:30 -0700

    refactor(datalad): Simplify file access for datalad objects

--- services/datalad/datalad_service/handlers/files.py
+++ services/datalad/datalad_service/handlers/files.py
@@ -1,11 +1,9 @@
 import logging
 import os
-import json
 from subprocess import CalledProcessError
 
 import falcon
 
-import git
 from datalad_service.common.git import git_show
 from datalad_service.common.user import get_user_info
 from datalad_service.common.stream import update_file
@@ -26,20 +24,21 @@ class FilesResource(object):
         ds_path = self.store.get_dataset_path(dataset)
         if filename:
             try:
-                ds = self.store.get_dataset(dataset)
-                if ds.repo.is_under_annex([filename])[0]:
-                    path = git_show(ds_path, snapshot + ':' + filename)
-                    # remove leading relative folder paths
-                    fd = path[path.find('.git/annex'):]
-
-                    # if fd fails, that means the file is not present in the annex and we need to get it from s3
-                    # so we send the client a 404 to indicate the file was not found locally.
-                    fd = open(os.path.join(ds_path, fd), 'rb')
-                    resp.stream = fd
-                    resp.stream_len = os.fstat(fd.fileno()).st_size
-                    resp.status = falcon.HTTP_OK
+                file_content = git_show(ds_path, snapshot + ':' + filename)
+                # If the file begins with an annex path, return that path
+                if file_content[0:4096].find('.git/annex') != -1:
+                    target_path = os.path.join(ds_path, file_content)
+                    # Verify the annex path is within the dataset dir
+                    if ds_path == os.path.commonpath((ds_path, target_path)):
+                        fd = open(target_path, 'rb')
+                        resp.stream = fd
+                        resp.stream_len = os.fstat(fd.fileno()).st_size
+                        resp.status = falcon.HTTP_OK
+                    else:
+                        resp.media = {'error': 'file not found in git tree'}
+                        resp.status = falcon.HTTP_NOT_FOUND
                 else:
-                    resp.body = git_show(ds_path, snapshot + ':' + filename)
+                    resp.body = file_content
                     resp.status = falcon.HTTP_OK
             except CalledProcessError:
                 # File is not present in tree
@@ -125,23 +124,27 @@ class FilesResource(object):
                     paths_not_found.append(filename)
 
             if len(paths_not_found) == 0:
-                media_dict = { 'deleted': dirs_to_delete + files_to_delete }
+                media_dict = {'deleted': dirs_to_delete + files_to_delete}
                 name, email = get_user_info(req)
                 if name and email:
                     media_dict['name'] = name
                     media_dict['email'] = email
-                try: 
+                try:
                     if len(dirs_to_delete) > 0:
-                        remove_recursive(self.store, dataset, dirs_to_delete, name=name, email=email, cookies=req.cookies)
+                        remove_recursive(
+                            self.store, dataset, dirs_to_delete, name=name, email=email, cookies=req.cookies)
                         resp.status = falcon.HTTP_INTERNAL_SERVER_ERROR
                     if len(files_to_delete) > 0:
-                        remove_files(self.store, dataset, files=files_to_delete, name=name, email=email, cookies=req.cookies)
+                        remove_files(self.store, dataset, files=files_to_delete,
+                                     name=name, email=email, cookies=req.cookies)
                     resp.media = media_dict
                     resp.status = falcon.HTTP_OK
                 except:
                     resp.status = falcon.HTTP_INTERNAL_SERVER_ERROR
             else:
-                resp.media = { 'error': f'the following files not found: {", ".join(paths_not_found)}' }
+                resp.media = {
+                    'error': f'the following files not found: {", ".join(paths_not_found)}'}
         else:
-            resp.media = {'error': 'recursive query or request body is missing'}
+            resp.media = {
+                'error': 'recursive query or request body is missing'}
             resp.status = falcon.HTTP_BAD_REQUEST
