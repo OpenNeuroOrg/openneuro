857a18e5c313db7f192fb0250265b7613e6fd74d
Gregory Noack <gregorynoack@thinknoack.com>
2021-06-17 19:37:13 -0700

    minor updates to sign in modal

--- packages/openneuro-components/src/modal/UserModalInner.tsx
+++ packages/openneuro-components/src/modal/UserModalInner.tsx
@@ -12,50 +12,46 @@ import '../header/header.scss'
 export const UserModalInner: React.FC = ({}) => {
   return (
     <>
-      <div className="grid grid-center grid-column">
+      <div className="sign-in-modal-header">
         <Logo horizontal dark={true} width="230px" className="m-t-20" />
         <h4>Sign in</h4>
       </div>
-      <div className="grid grid-center sign-in-modal-content">
-        <div className="col col-3">
-          <div className="grid grid-center">
-            <Button
-              className="login-button"
-              primary
-              label="Google"
-              icon="fab fa-google"
-              iconSize="23px"
-            />
-          </div>
+      <div className="sign-in-modal-content">
+        <div>
+          <Button
+            className="login-button"
+            primary
+            label="Google"
+            icon="fab fa-google"
+            iconSize="23px"
+          />
         </div>
-        <div className="col col-3">
-          <div className="grid grid-center">
-            <Button
-              className="login-button"
-              primary
-              label="ORCID"
-              imgSrc={orcidIcon}
+        <div>
+          <Button
+            className="login-button"
+            primary
+            label="ORCID"
+            imgSrc={orcidIcon}
+          />
+          <AccordionWrap>
+            <AccordionTab
+              name="single"
+              id="orcid-info-accordion"
+              label="What is this?"
+              children={
+                <>
+                  ORCID users are identified and connected to their
+                  contributions and affiliations, across disciplines, borders,
+                  and time.{' '}
+                  <a
+                    href="https://orcid.org/content/about-orcid"
+                    target="_blank">
+                    Learn more
+                  </a>
+                </>
+              }
             />
-            <AccordionWrap>
-              <AccordionTab
-                name="single"
-                tabId="orcid-info-accordion"
-                label="What is this?"
-                children={
-                  <>
-                    ORCID users are identified and connected to their
-                    contributions and affiliations, across disciplines, borders,
-                    and time.{' '}
-                    <a
-                      href="https://orcid.org/content/about-orcid"
-                      target="_blank">
-                      Learn more
-                    </a>
-                  </>
-                }
-              />
-            </AccordionWrap>
-          </div>
+          </AccordionWrap>
         </div>
       </div>
     </>
