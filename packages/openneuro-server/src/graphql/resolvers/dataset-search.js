f5f22f8d813ae1c8cc8a93a1d8a8545ccf1c10ff
David Nishikawa <david@squishymedia.com>
2021-06-19 11:23:49 -0700

    Refactor datasetType query.

--- packages/openneuro-server/src/graphql/resolvers/dataset-search.js
+++ packages/openneuro-server/src/graphql/resolvers/dataset-search.js
@@ -1,5 +1,7 @@
 import elasticClient from '../../elasticsearch/elastic-client'
 import { dataset } from './dataset'
+import Star from '../../models/stars'
+import Subscription from '../../models/subscription'
 
 const elasticIndex = 'datasets'
 
@@ -104,22 +106,63 @@ export const datasetSearch = {
   },
 }
 
+const addClause = (query, type, clause) => {
+  if (query.bool[type]) {
+    query.bool[type] = [...query.bool[type], clause]
+  } else {
+    query.bool[type] = [clause]
+  }
+}
+
+const parseQuery = async (query, datasetType, datasetStatus, userId) => {
+  const queryObj = JSON.parse(query)
+  if (datasetType === 'Following') {
+    const results = await Subscription.find({ userId })
+    const followedDatasets = results.map(({ datasetId }) => datasetId)
+    const termsClause = {
+      terms: {
+        id: followedDatasets,
+      },
+    }
+    addClause(queryObj, 'filter', termsClause)
+  } else if (datasetType === 'My Bookmarks') {
+    const results = await Star.find({ userId })
+    const bookmarkedDatasets = results.map(({ datasetId }) => datasetId)
+    const termsClause = {
+      terms: {
+        id: bookmarkedDatasets,
+      },
+    }
+    addClause(queryObj, 'filter', termsClause)
+  } else if (datasetType === 'My Datasets') {
+  }
+  return queryObj
+}
+
 /**
  * Search result cursor resolver
  * TODO this is a Relay pagination type and could use the interface
  * @param {any} obj
  * @param {object} args
  * @param {string} args.query Stringified Query (DSL) argument for ElasticSearch
+ * @param {string} args.datasetType Stringified Query (DSL) argument for ElasticSearch
+ * @param {string} args.datasetStatus Stringified Query (DSL) argument for ElasticSearch
  * @param {string} args.after Cursor for paging forward
  * @param {number} args.first Limit of entries to find
  */
 export const advancedDatasetSearchConnection = async (
   obj,
-  { query, after, first = 25 },
+  { query, datasetType, datasetStatus, after, first = 25 },
+  { user, userInfo },
 ) => {
+  console.log('==========')
+  console.log('==========')
+  console.log(datasetType)
+  console.log('==========')
+  console.log('==========')
   const requestBody = {
     sort: [{ _score: 'asc', id: 'desc' }],
-    query: JSON.parse(query),
+    query: await parseQuery(query, datasetType, datasetStatus, user),
   }
   if (after) {
     try {
@@ -142,6 +185,8 @@ export const advancedDatasetSearch = {
     resolve: advancedDatasetSearchConnection,
     args: {
       query: { type: 'String!' },
+      datasetType: { type: 'String' },
+      datasetStatus: { type: 'String' },
       after: { type: 'String' },
       first: { type: 'Int' },
     },
