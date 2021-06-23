b58f71162d366eb157b0b88835d2fbd91eeb8c77
Nell Hardcastle <nell@squishymedia.com>
2021-06-23 11:43:44 -0700

    fix(app): Add a default redirect to the MRI modality for unknown paths.

--- packages/openneuro-app/src/scripts/refactor_2021/search/search-routes.tsx
+++ packages/openneuro-app/src/scripts/refactor_2021/search/search-routes.tsx
@@ -1,17 +1,24 @@
 import React, { FC } from 'react'
-import { Route, Switch, useRouteMatch } from 'react-router-dom'
+import { Route, Switch, useRouteMatch, Redirect } from 'react-router-dom'
 import SearchContainer from './search-container'
 import { portalContent } from '@openneuro/components'
 
 const SearchRoutes: FC = () => {
-  const { path } = useRouteMatch()
+  const { path }: { path: string } = useRouteMatch()
   return (
     <Switch>
       <Route exact path={path} component={SearchContainer} />
       <Route
         exact
         path={`${path}/modality/mri`}
-        render={() => <SearchContainer portalContent={portalContent.mri} />}
+        render={(): React.ReactNode => (
+          <SearchContainer portalContent={portalContent.mri} />
+        )}
+      />
+      <Route
+        component={(): React.ReactNode => (
+          <Redirect to={`${path}/modality/mri`} />
+        )}
       />
     </Switch>
   )
