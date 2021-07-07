0fdc7125b21ace0bd1c57f53a91757f24eb608cf
David Nishikawa <david@squishymedia.com>
2021-07-06 22:28:45 -0700

    Set modality to initial value if not set by `portalContent`.

--- packages/openneuro-app/src/scripts/refactor_2021/search/search-container.tsx
+++ packages/openneuro-app/src/scripts/refactor_2021/search/search-container.tsx
@@ -38,7 +38,7 @@ const SearchContainer: FC<SearchContainerProps> = ({ portalContent }) => {
   const profile = getUnexpiredProfile(cookies)
 
   const { searchParams, setSearchParams } = useContext(SearchParamsCtx)
-  const modality = portalContent?.modality || false
+  const modality = portalContent?.modality || null
   useEffect(() => {
     if (searchParams.modality_selected !== modality) {
       setSearchParams(
