f97772770b1a7230895a4ec7bbc6ee36c7665476
Nell Hardcastle <nell@squishymedia.com>
2021-07-09 10:29:42 -0700

    fix(redesign): Fix incorrect UserModalOpenProvider import

--- packages/openneuro-app/src/scripts/redesign.tsx
+++ packages/openneuro-app/src/scripts/redesign.tsx
@@ -3,17 +3,17 @@ import Uploader from './uploader/uploader.jsx'
 import Routes_REFACTOR from './refactor_2021/routes'
 import HeaderContainer from './refactor_2021/containers/header'
 import { SearchParamsProvider } from './refactor_2021/search/search-params-ctx'
-import { UserModalParamsProvider } from './refactor_2021/user-login-modal-ctx'
+import { UserModalOpenProvider } from './refactor_2021/user-login-modal-ctx'
 
 const Redesign = (): React.ReactElement => {
   import('@openneuro/components/page/page.scss')
   return (
     <Uploader>
       <SearchParamsProvider>
-        <UserModalParamsProvider>
+        <UserModalOpenProvider>
           <HeaderContainer />
           <Routes_REFACTOR />
-        </UserModalParamsProvider>
+        </UserModalOpenProvider>
       </SearchParamsProvider>
     </Uploader>
   )
