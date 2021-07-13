314e932f603d65afb5c92e784ba44beea3064fb7
Gregory Noack <gregorynoack@thinknoack.com>
2021-07-13 13:12:34 -0700

    showing flagged file for admin

--- packages/openneuro-server/src/graphql/utils/file.js
+++ packages/openneuro-server/src/graphql/utils/file.js
@@ -29,10 +29,13 @@ export function UpdatedFile(filepath, size) {
   if (size) this.size = size
 }
 
-export const filterRemovedAnnexObjects = datasetId => async files => {
-  const removedAnnexObjectKeys = (
-    await BadAnnexObject.find({ datasetId }).exec()
-  ).map(({ annexKey }) => annexKey)
-  // keep files that havent had their annex objects removed
-  return files.filter(({ key }) => !removedAnnexObjectKeys.includes(key))
-}
+export const filterRemovedAnnexObjects =
+  (datasetId, userInfo) => async files => {
+    const removedAnnexObjectKeys = (
+      await BadAnnexObject.find({ datasetId }).exec()
+    ).map(({ annexKey }) => annexKey)
+    // keep files that havent had their annex objects removed
+    return userInfo.admin
+      ? files
+      : files.filter(({ key }) => !removedAnnexObjectKeys.includes(key))
+  }
