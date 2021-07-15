b008bf570b08481f5a0877f508f7d649f6209954
David Nishikawa <david@squishymedia.com>
2021-07-15 13:04:40 -0700

    Update range state for controlled component.

--- packages/openneuro-components/src/range/TwoHandleRange.tsx
+++ packages/openneuro-components/src/range/TwoHandleRange.tsx
@@ -25,8 +25,6 @@ export const TwoHandleRange: React.FC<TwoHandleRangeProps> = ({
   value,
   onChange,
 }) => {
-  const minValRef = useRef(min)
-  const maxValRef = useRef(max)
   const range = useRef<HTMLDivElement>(null)
 
   // Convert to percentage
@@ -38,7 +36,7 @@ export const TwoHandleRange: React.FC<TwoHandleRangeProps> = ({
   // Set width of the range to decrease from the left side
   useEffect(() => {
     const minPercent = getPercent(value[0])
-    const maxPercent = getPercent(maxValRef.current)
+    const maxPercent = getPercent(value[1])
 
     if (range.current) {
       range.current.style.left = `${minPercent}%`
@@ -48,7 +46,7 @@ export const TwoHandleRange: React.FC<TwoHandleRangeProps> = ({
 
   // Set width of the range to decrease from the right side
   useEffect(() => {
-    const minPercent = getPercent(minValRef.current)
+    const minPercent = getPercent(value[0])
     const maxPercent = getPercent(value[1])
 
     if (range.current) {
@@ -66,7 +64,7 @@ export const TwoHandleRange: React.FC<TwoHandleRangeProps> = ({
       Math.min(Number(changeEvent.target.value), value[1] - 1),
       step,
     )
-    minValRef.current = discreteValue
+    value[0] = discreteValue
     onChange([discreteValue, value[1]])
   }
   const maxChangeHandler = (
@@ -79,7 +77,7 @@ export const TwoHandleRange: React.FC<TwoHandleRangeProps> = ({
       Math.max(Number(changeEvent.target.value), value[0] + 1),
       step,
     )
-    maxValRef.current = discreteValue
+    value[1] = discreteValue
     onChange([value[0], discreteValue])
   }
   return (
