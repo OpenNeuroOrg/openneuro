1e7ef08bc1388bd566fe269c5ed53748b216f732
Nell Hardcastle <nell@squishymedia.com>
2021-07-16 12:39:12 -0700

    fix(app): Add redirects to new design for existing dashboard URLs

---
+++ packages/openneuro-app/src/scripts/refactor_2021/search/__tests__/search-container.spec.tsx
@@ -0,0 +1,56 @@
+import { setDefaultSearch } from '../search-container'
+
+describe('SearchContainer component', () => {
+  describe('setDefaultSearch', () => {
+    it('updates default state when a modality is provided differing from the default', () => {
+      const context = { modality_selected: 'MRI' }
+      const setContext = jest.fn()
+      setDefaultSearch(
+        'MRI',
+        context,
+        setContext,
+        new URLSearchParams('/search'),
+      )
+      expect(setContext).not.toHaveBeenCalled()
+      setDefaultSearch(
+        'PET',
+        context,
+        setContext,
+        new URLSearchParams('/search'),
+      )
+      expect(setContext).toHaveBeenCalled()
+    })
+    it('sets datasetType_select to my datasets with "mydatasets" parameter', () => {
+      let context = {
+        modality_selected: 'MRI',
+        datasetType_selected: 'All Public',
+      }
+      const setContext = jest.fn().mockImplementation(arg => {
+        context = arg(context)
+      })
+      setDefaultSearch(
+        'MRI',
+        context,
+        setContext,
+        new URLSearchParams('mydatasets'),
+      )
+      expect(context.datasetType_selected).toEqual('My Datasets')
+    })
+    it('sets datasetType_select to my datasets with "bookmarks" parameter', () => {
+      let context = {
+        modality_selected: 'PET',
+        datasetType_selected: 'All Public',
+      }
+      const setContext = jest.fn().mockImplementation(arg => {
+        context = arg(context)
+      })
+      setDefaultSearch(
+        'MRI',
+        context,
+        setContext,
+        new URLSearchParams('bookmarks'),
+      )
+      expect(context.datasetType_selected).toEqual('My Bookmarks')
+    })
+  })
+})
