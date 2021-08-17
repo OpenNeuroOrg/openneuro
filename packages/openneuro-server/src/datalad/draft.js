ab82ceb540e7a3fbd9c9add284ff56af8b48e011
Nell Hardcastle <nell@squishymedia.com>
2021-08-17 15:32:36 -0700

    refactor: Make draft files cache immutable
    Avoids possible out of date cache entries for drafts.

--- packages/openneuro-server/src/datalad/draft.js
+++ packages/openneuro-server/src/datalad/draft.js
@@ -1,13 +1,12 @@
 /**
  * Manage serving a Draft object based on DataLad working trees
  */
+import fetch from 'node-fetch'
 import request from 'superagent'
 import Dataset from '../models/dataset'
 import { redis } from '../libs/redis'
 import CacheItem, { CacheType } from '../cache/item'
-import { addFileUrl } from './utils.js'
 import publishDraftUpdate from '../graphql/utils/publish-draft-update.js'
-import { generateFileId } from '../graphql/utils/file.js'
 import { getDatasetWorker } from '../libs/datalad-service'
 
 export const expireDraftFiles = datasetId => {
@@ -15,46 +14,13 @@ export const expireDraftFiles = datasetId => {
   return cache.drop()
 }
 
-/**
- *
- * @param {object} file
- * @param {string} file.filename '/' delimited
- * @param {string|number} file.size
- */
-const withGeneratedId = file => ({
-  ...file,
-  id: generateFileId(file.filename.split('/').join(':'), file.size),
-})
-
-/**
- * Retrieve draft files from cache or the datalad-service backend
- * @param {string} datasetId Accession number string
- * @param {object} options { untracked: true } - ignores the git index
- */
-export const getDraftFiles = (datasetId, options = {}) => {
-  // If untracked is set and true
-  const untracked = 'untracked' in options && options.untracked
-  const query = untracked ? { untracked: true } : {}
-  const filesUrl = `${getDatasetWorker(datasetId)}/datasets/${datasetId}/files`
-  if (untracked) {
-    return request
-      .get(filesUrl)
-      .query(query)
-      .set('Accept', 'application/json')
-      .then(({ body: { files } }) => files.map(withGeneratedId))
-  } else {
-    const cache = new CacheItem(redis, CacheType.commitFiles, [datasetId])
-    return cache.get(() =>
-      request
-        .get(filesUrl)
-        .query(query)
-        .set('Accept', 'application/json')
-        .then(({ body: { files } }) => {
-          const filesWithUrls = files.map(addFileUrl(datasetId))
-          return filesWithUrls.map(withGeneratedId)
-        }),
-    )
-  }
+export const getDraftRevision = async datasetId => {
+  const draftUrl = `http://${getDatasetWorker(
+    datasetId,
+  )}/datasets/${datasetId}/draft`
+  const response = await fetch(draftUrl)
+  const { hexsha } = await response.json()
+  return hexsha
 }
 
 export const updateDatasetRevision = (datasetId, gitRef) => {
