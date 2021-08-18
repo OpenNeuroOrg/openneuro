4b6437a458a8cf0ebad0b2e376ed1c6e80286662
Gregory Noack <gregorynoack@thinknoack.com>
2021-08-18 14:52:45 -0700

    update facet-select so the accordion is optional and lives in the redesign component

--- packages/openneuro-components/src/facets/FacetSelect.tsx
+++ packages/openneuro-components/src/facets/FacetSelect.tsx
@@ -19,14 +19,10 @@ export interface FacetSelectProps {
         children?: null | { label: string; value: string; count: number }[]
       }
   )[]
-  accordionStyle: AccordionTabStyle
-  startOpen: boolean
-  label: string
-  dropdown?: boolean
+  className?: string
+  label?: string
   selected: FacetSelectValueType
   setSelected: (selected: FacetSelectValueType) => void
-  className?: string
-  noAccordion?: boolean
 }
 
 const get = (obj, property) => (typeof obj === 'object' ? obj[property] : obj)
@@ -39,121 +35,56 @@ export const check = (obj, property) =>
 
 export const FacetSelect = ({
   items,
-  selected,
-  setSelected,
-  startOpen,
-  label,
-  accordionStyle,
-  dropdown,
   className,
-  noAccordion,
+  selected,
+  label,
+  setSelected,
 }: FacetSelectProps) => {
   const setSelectorNoPropagation = (e, value) => {
     e.stopPropagation()
     setSelected(value)
   }
   return (
-    <>
-      {noAccordion ? (
-        <div className={className + ' facet-accordion'}>
-          <h2>{label}</h2>
-          <div className="facet-list">
-            <ul className="level-1">
-              {items.map((item, index) => (
-                <li
-                  key={index}
-                  onClick={e => setSelectorNoPropagation(e, get(item, 'value'))}
-                  className={
-                    selected && selected == get(item, 'value')
-                      ? 'selected-facet facet'
-                      : 'facet'
-                  }>
-                  <span className="label">
-                    {get(item, 'label')}
-                    {check(item, 'count') && (
-                      <span>({check(item, 'count')})</span>
-                    )}
-                  </span>
-                  {check(item, 'children') && (
-                    <ul className="level-2">
-                      {get(item, 'children').map((item, index) => (
-                        <li
-                          key={index}
-                          onClick={e => setSelectorNoPropagation(e, item.value)}
-                          className={
-                            selected && selected == item.value
-                              ? 'selected-facet facet'
-                              : 'facet'
-                          }>
-                          <span className="label">
-                            {item.label}
-                            {item.count && <span>({item.count})</span>}
-                          </span>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      ) : (
-        <AccordionWrap className={className + ' facet-accordion'}>
-          <AccordionTab
-            accordionStyle={accordionStyle}
-            label={label}
-            startOpen={startOpen}
-            dropdown={dropdown}>
-            <div className="facet-list">
-              <ul className="level-1">
-                {items.map((item, index) => (
-                  <li
-                    key={index}
-                    onClick={e =>
-                      setSelectorNoPropagation(e, get(item, 'value'))
-                    }
-                    className={
-                      selected && selected == get(item, 'value')
-                        ? 'selected-facet facet'
-                        : 'facet'
-                    }>
-                    <span className="label">
-                      {get(item, 'label')}
-                      {check(item, 'count') && (
-                        <span>({get(item, 'count')})</span>
-                      )}
-                    </span>
-                    {check(item, 'children') && (
-                      <ul className="level-2">
-                        {get(item, 'children').map((item, index) => (
-                          <li
-                            key={index}
-                            onClick={e =>
-                              setSelectorNoPropagation(e, get(item, 'value'))
-                            }
-                            className={
-                              selected && selected == get(item, 'value')
-                                ? 'selected-facet facet'
-                                : 'facet'
-                            }>
-                            <span className="label">
-                              {get(item, 'label')}
-                              {check(item, 'count') && (
-                                <span>({get(item, 'count')})</span>
-                              )}
-                            </span>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </AccordionTab>
-        </AccordionWrap>
-      )}
-    </>
+    <div className={className}>
+      {label && <h2>{label}</h2>}
+      <div className="facet-list">
+        <ul className="level-1">
+          {items.map((item, index) => (
+            <li
+              key={index}
+              onClick={e => setSelectorNoPropagation(e, get(item, 'value'))}
+              className={
+                selected && selected == get(item, 'value')
+                  ? 'selected-facet facet'
+                  : 'facet'
+              }>
+              <span className="label">
+                {get(item, 'label')}
+                {check(item, 'count') && <span>({check(item, 'count')})</span>}
+              </span>
+              {check(item, 'children') && (
+                <ul className="level-2">
+                  {get(item, 'children').map((item, index) => (
+                    <li
+                      key={index}
+                      onClick={e => setSelectorNoPropagation(e, item.value)}
+                      className={
+                        selected && selected == item.value
+                          ? 'selected-facet facet'
+                          : 'facet'
+                      }>
+                      <span className="label">
+                        {item.label}
+                        {item.count && <span>({item.count})</span>}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
