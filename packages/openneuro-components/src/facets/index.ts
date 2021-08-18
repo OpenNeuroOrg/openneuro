b42144fd7a57a00aefbab4fe34acaf09a362ca5a
Gregory Noack <gregorynoack@thinknoack.com>
2021-08-18 15:25:15 -0700

    moving accordion from FacetRange to redesign

--- packages/openneuro-components/src/facets/index.ts
+++ packages/openneuro-components/src/facets/index.ts
@@ -3,13 +3,3 @@ import { FacetRadio } from './FacetRadio'
 import { FacetRange } from './FacetRange'
 import { FacetSearch } from './FacetSearch'
 export { FacetSelect, FacetRadio, FacetRange, FacetSearch }
-
-// export * from './FacetDatePicker'
-
-/* TODO: resolve the following
-Uncaught Error: Dynamic require of "/srv/.yarn/$$virtual/react-calendar-virtual-6f2d36e55f/0/cache/react-calendar-npm-3.4.0-3e72e722bc-36ea2dd18c.zip/node_modules/react-calendar/dist/Calendar.css" is not supported
-    at __require (chunk-G3IZEHTJ.js?v=426b9837:11)
-    at ../../.yarn/$$virtual/@wojtekmaj-react-daterange-picker-virtual-0ee61039a1/0/cache/@wojtekmaj-react-daterange-picker-npm-3.2.0-508eb3e1e1-a7c10a86ac.zip/node_modules/@wojtekmaj/react-daterange-picker/dist/entry.js (entry.js:8)
-    at __require2 (chunk-G3IZEHTJ.js?v=426b9837:17)
-    at dep:@wojtekmaj_react-daterange-picker:1
-*/
