b00cc5dada5053cbf5add24efda475e4c322adbe
Gregory Noack <gregorynoack@thinknoack.com>
2021-08-20 13:31:46 -0700

    adding condtional check to see if readmore is necessary

--- packages/openneuro-components/src/read-more/ReadMore.tsx
+++ packages/openneuro-components/src/read-more/ReadMore.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 
 export interface ReadMoreProps {
   children: React.ReactNode
@@ -13,17 +13,36 @@ export const ReadMore = ({
   expandLabel,
   collapseabel,
 }: ReadMoreProps) => {
-  return (
-    <article className="has-read-more">
-      <input type="checkbox" className="show-more" id={id} />
+  const targetRef = useRef()
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 })
+  useEffect(() => {
+    if (targetRef.current) {
+      setDimensions({
+        width: targetRef.current.offsetWidth,
+        height: targetRef.current.offsetHeight,
+      })
+    }
+  }, [])
 
-      <section>
-        {children}
-        <label htmlFor={id}>
-          <span>{expandLabel}</span>
-          <span>{collapseabel}</span>
-        </label>
-      </section>
-    </article>
+  return (
+    <div ref={targetRef}>
+      {dimensions.height > 400 ? (
+        <article className="has-read-more">
+          <input type="checkbox" className="show-more" id={id} />
+
+          <section>
+            {children}
+            <label htmlFor={id}>
+              <span>{expandLabel}</span>
+              <span>{collapseabel}</span>
+            </label>
+          </section>
+        </article>
+      ) : (
+        <article>
+          <section>{children}</section>
+        </article>
+      )}
+    </div>
   )
 }
