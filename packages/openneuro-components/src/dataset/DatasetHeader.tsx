5e383a68705cce044cac66e538106f0033d8c4bf
Gregory noack <gregorynoack@thinknoack.com>
2021-08-31 12:10:08 -0700

    Update packages/openneuro-components/src/dataset/DatasetHeader.tsx
    Co-authored-by: David Nishikawa <28666458+david-nishi@users.noreply.github.com>

--- packages/openneuro-components/src/dataset/DatasetHeader.tsx
+++ packages/openneuro-components/src/dataset/DatasetHeader.tsx
@@ -29,7 +29,7 @@ export const DatasetHeader: React.FC<DatasetHeaderProps> = ({
                   </div>
                 </div>
               </a>
-              {renderEditor ? renderEditor() : pageHeading}
+              {renderEditor?.() : pageHeading}
             </h1>
           </div>
         </div>
