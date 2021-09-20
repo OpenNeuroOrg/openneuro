da6c1188925a144445dc6363ff4c9da366f0c04c
David Nishikawa <nishikawadavid@gmail.com>
2021-09-20 12:40:19 -0700

    Replace Spinner with Loading in FileView.

--- packages/openneuro-app/src/scripts/refactor_2021/dataset/files/file-view.jsx
+++ packages/openneuro-app/src/scripts/refactor_2021/dataset/files/file-view.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import Spinner from '../../../common/partials/spinner.jsx'
+import { Loading } from '@openneuro/components/loading'
 import { apiPath } from './file.jsx'
 import FileViewerType from './file-viewer-type.jsx'
 
@@ -22,7 +22,7 @@ const FileView = ({ datasetId, snapshotTag, path }) => {
   })
 
   if (loading) {
-    return <Spinner active text="Loading File" />
+    return <Loading />
   } else {
     return <FileViewerType path={path} url={url} data={data} />
   }
