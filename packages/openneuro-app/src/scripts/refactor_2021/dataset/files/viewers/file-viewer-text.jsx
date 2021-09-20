b3f8c440b0ab0eef01fbf1ed06546b3452494dd5
David Nishikawa <nishikawadavid@gmail.com>
2021-09-19 23:38:15 -0700

    Improve text file viewer styles.

--- packages/openneuro-app/src/scripts/refactor_2021/dataset/files/viewers/file-viewer-text.jsx
+++ packages/openneuro-app/src/scripts/refactor_2021/dataset/files/viewers/file-viewer-text.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+
+const Pre = styled.pre`
+  margin: 0 15px;
+  white-space: pre-wrap;
+`
 
 const FileViewerText = ({ data }) => {
   const decoder = new TextDecoder()
-  return <pre>{decoder.decode(data)}</pre>
+  return <Pre>{decoder.decode(data)}</Pre>
 }
 
 FileViewerText.propTypes = {
