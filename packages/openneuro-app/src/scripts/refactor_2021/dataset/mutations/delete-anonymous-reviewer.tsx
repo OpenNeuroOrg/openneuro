2d89866dad351d654d86ec8a9f83f7a3da87aa1e
David Nishikawa <nishikawadavid@gmail.com>
2021-09-22 14:59:50 -0700

    deleteReviewer mutation updates reviewers in cache.

--- packages/openneuro-app/src/scripts/refactor_2021/dataset/mutations/delete-anonymous-reviewer.tsx
+++ packages/openneuro-app/src/scripts/refactor_2021/dataset/mutations/delete-anonymous-reviewer.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { gql, useMutation } from '@apollo/client'
 import { WarnButton } from '@openneuro/components/warn-button'
+import { DATASET_REVIEWERS } from '../fragments/dataset-reviewers'
 
 const DELETE_REVIEWER = gql`
   mutation deleteReviewer($datasetId: ID!, $id: ID!) {
@@ -20,7 +21,24 @@ export const DeleteReviewerLink: FC<DeleteReviewerLinkProps> = ({
   datasetId,
   id,
 }) => {
-  const [DeleteReviewerLink] = useMutation(DELETE_REVIEWER)
+  const [DeleteReviewerLink] = useMutation(DELETE_REVIEWER, {
+    update(cache, { data: { deleteReviewer } }) {
+      const { reviewers } = cache.readFragment({
+        id: `Dataset:${datasetId}`,
+        fragment: DATASET_REVIEWERS,
+      })
+      const updatedReviewers = reviewers.filter(reviewer => reviewer.id !== deleteReviewer.id)
+      cache.writeFragment({
+        id: `Dataset:${datasetId}`,
+        fragment: DATASET_REVIEWERS,
+        data: {
+          __typename: 'Dataset',
+          id: datasetId,
+          reviewers: updatedReviewers,
+        }
+      })
+    }
+  })
   const [displayOptions, setDisplayOptions] = React.useState(false)
   return (
     <WarnButton
