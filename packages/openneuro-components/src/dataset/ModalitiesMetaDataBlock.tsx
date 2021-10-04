81cf982404f480c4edb5e9727cf6885c27d5dded
Gregory Noack <gregorynoack@thinknoack.com>
2021-10-04 13:11:28 -0700

    adding dict for modality list

--- packages/openneuro-components/src/dataset/ModalitiesMetaDataBlock.tsx
+++ packages/openneuro-components/src/dataset/ModalitiesMetaDataBlock.tsx
@@ -11,11 +11,21 @@ export const ModalitiesMetaDataBlock = ({
   items,
   className,
 }: ModalitiesMetaDataBlockProps) => {
+  const uppercased = {
+    mri: 'MRI',
+    ieeg: 'iEEG',
+    pet: 'PET',
+    eeg: 'EEG',
+    meg: 'MEG',
+  }
   return (
     <div className={'dataset-meta-block ' + className}>
       <h2 className="dmb-heading">Available Modalities</h2>
       {items.map((item, index) => (
-        <Link key={index} to={'/search/modality/' + item.toLowerCase()}>
+        <Link
+          key={index}
+          to={'/search/modality/' + uppercased[item.toLowerCase()]}
+        >
           {item}
         </Link>
       ))}
