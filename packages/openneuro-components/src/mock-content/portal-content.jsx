32e927193931584770f609bbb703bdc81434540e
Gregory Noack <gregorynoack@thinknoack.com>
2021-10-05 13:32:53 -0700

    removing commas in portal headers

--- packages/openneuro-components/src/mock-content/portal-content.jsx
+++ packages/openneuro-components/src/mock-content/portal-content.jsx
@@ -37,7 +37,7 @@ export const portalContent = {
     portalName: 'OpenNeuro EEG',
     portalPrimary: (
       <>
-        OpenNeuro added support for EEG datasets in 2019, when{' '}
+        OpenNeuro added support for EEG datasets in 2019 when{' '}
         <a href="https://www.nature.com/articles/s41597-019-0104-8">
           EEG was incorporated
         </a>{' '}
@@ -59,7 +59,7 @@ export const portalContent = {
     portalName: 'OpenNeuro iEEG',
     portalPrimary: (
       <>
-        OpenNeuro added support for iEEG datasets in 2019, when{' '}
+        OpenNeuro added support for iEEG datasets in 2019 when{' '}
         <a href="https://www.nature.com/articles/s41597-019-0105-7">
           iEEG was incorporated
         </a>{' '}
@@ -81,7 +81,7 @@ export const portalContent = {
     portalName: 'OpenNeuro MEG',
     portalPrimary: (
       <>
-        OpenNeuro added support for MEG datasets in 2018, when{' '}
+        OpenNeuro added support for MEG datasets in 2018 when{' '}
         <a href="https://www.nature.com/articles/sdata2018110">
           MEG was incorporated
         </a>{' '}
@@ -101,7 +101,8 @@ export const portalContent = {
     modality: 'PET', // corresponds to values in `modality_available` in (packages/openneuro-app/src/scripts/refactor_2021/search/initial-search-params.tsx)
     className: 'search-page-pet',
     portalName: 'OpenNeuro PET',
-    portalPrimary: 'The PET portal of OpenNeuro is supported by a collaboration between Stanford University, NIH, MGH and the Neurobiology Research Unit (NRU) at Copenhagen University Hospital through the OpenNeuroPET project. The project is funded through the BRAIN Initiative and the Novo Nordisk Foundation. Besides developing data sharing, the OpenNeuroPET project also aims at developing user friendly tools for the BIDS based data curation of PET data as well as tools for automated QC and template building.',
+    portalPrimary:
+      'The PET portal of OpenNeuro is supported by a collaboration between Stanford University, NIH, MGH and the Neurobiology Research Unit (NRU) at Copenhagen University Hospital through the OpenNeuroPET project. The project is funded through the BRAIN Initiative and the Novo Nordisk Foundation. Besides developing data sharing, the OpenNeuroPET project also aims at developing user friendly tools for the BIDS based data curation of PET data as well as tools for automated QC and template building.',
     publicDatasetStat: 100,
     participantsStat: 1100,
     hexBackgroundImage: pet,
