d58dc7a6b34341d0ffa8f3474f9885a59b52180a
Nell Hardcastle <nell@squishymedia.com>
2021-10-26 20:33:13 -0700

    feat: Proof of concept NiiVue replacing Papaya

--- packages/openneuro-app/src/scripts/refactor_2021/dataset/files/viewers/file-viewer-nifti.jsx
+++ packages/openneuro-app/src/scripts/refactor_2021/dataset/files/viewers/file-viewer-nifti.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
-import Papaya from '../../../../common/partials/papaya.jsx'
+import { Niivue } from '@niivue/niivue'
 import styled from '@emotion/styled'
 
 const StyleWrapper = styled.div`
@@ -9,11 +9,28 @@ const StyleWrapper = styled.div`
   }
 `
 
-const FileViewerNifti = ({ imageUrl }) => (
-  <StyleWrapper>
-    <Papaya image={imageUrl} />
-  </StyleWrapper>
-)
+const FileViewerNifti = ({ imageUrl }) => {
+  useEffect(() => {
+    const volumeList = [
+      {
+        url: imageUrl,
+        volume: { hdr: null, img: null },
+        colorMap: 'gray',
+        opacity: 100,
+        visible: true,
+      },
+    ]
+    const nv = new Niivue()
+    nv.attachTo('niivue')
+    nv.loadVolumes(volumeList)
+  }, [imageUrl])
+
+  return (
+    <StyleWrapper>
+      <canvas id="niivue" height={480} width={640} />
+    </StyleWrapper>
+  )
+}
 
 FileViewerNifti.propTypes = {
   imageUrl: PropTypes.string,
