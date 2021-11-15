028a55812fa89cc1995199c3fdfe78e32f88fdbc
Nell Hardcastle <nell@squishymedia.com>
2021-11-15 13:53:08 -0800

    fix(search): Prioritize accession number matches ahead of most other matches

--- packages/openneuro-app/src/scripts/refactor_2021/search/use-search-results.tsx
+++ packages/openneuro-app/src/scripts/refactor_2021/search/use-search-results.tsx
@@ -154,6 +154,7 @@ export const useSearchResults = () => {
     boolQuery.addClause(
       'must',
       simpleQueryString(sqsJoinWithAND(keywords), [
+        'id^6',
         'latestSnapshot.readme',
         'latestSnapshot.description.Name^6',
         'latestSnapshot.description.Authors^3',
