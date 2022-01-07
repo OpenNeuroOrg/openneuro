217b62214ae44d614f535e8fe6bdc9289152ff3f
Nell Hardcastle <nell@dev-nell.com>
2022-01-07 12:45:08 -0800

    feat(server): Add a URL validation function for models

---
+++ packages/openneuro-server/src/utils/__tests__/validateUrl.spec.ts
@@ -0,0 +1,13 @@
+import { validateUrl } from '../validateUrl'
+
+describe('validateUrl', () => {
+  it('returns true for a regular HTTPS url', () => {
+    expect(validateUrl('https://openneuro.org')).toBe(true)
+  })
+  it('returns false for a regular HTTP url', () => {
+    expect(validateUrl('http://openneuro.org')).toBe(false)
+  })
+  it('returns false for something that is not really a URL', () => {
+    expect(validateUrl('openneuro.org/robots.txt')).toBe(false)
+  })
+})
