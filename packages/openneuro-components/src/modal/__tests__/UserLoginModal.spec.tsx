bf37a38c1656c727051c6c7c6b477b00096682a6
Nell Hardcastle <nell@dev-nell.com>
2022-01-31 17:53:23 -0800

    chore: Remove unused imports in UserLoginModal tests

--- packages/openneuro-components/src/modal/__tests__/UserLoginModal.spec.tsx
+++ packages/openneuro-components/src/modal/__tests__/UserLoginModal.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { UserLoginModal } from '../UserLoginModal'
 import { MemoryRouter } from 'react-router-dom'
 
