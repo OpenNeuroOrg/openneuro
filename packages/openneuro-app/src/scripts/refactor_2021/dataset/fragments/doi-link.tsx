eed6981232dc1291bdaf6518084901737a53c6ad
Nell Hardcastle <nell@dev-nell.com>
2022-03-10 11:18:04 -0800

    fix(app): Link fallback DOILink to snapshot creation tool page
    Fixes #2566

--- packages/openneuro-app/src/scripts/refactor_2021/dataset/fragments/doi-link.tsx
+++ packages/openneuro-app/src/scripts/refactor_2021/dataset/fragments/doi-link.tsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 // See https://www.crossref.org/blog/dois-and-matching-regular-expressions/
 export const DOIPattern = /^10.\d{4,9}\/[-._;()/:A-Z0-9]+$/i
 
-export const DOILinkFallback = () => (
-  <Link to="snapshot">
+export const DOILinkFallback = ({ datasetId }) => (
+  <Link to={`/datasets/${datasetId}/snapshot`}>
     Create a new snapshot to obtain a DOI for the snapshot.
   </Link>
 )
 
-export const DOILink = ({ DOI }) => {
+export const DOILink = ({ DOI, datasetId }) => {
   if (
     DOI &&
     (DOI.match(DOIPattern) ||
@@ -26,8 +26,8 @@ export const DOILink = ({ DOI }) => {
     if (DOI.startsWith('https://doi.org/')) {
       return <a href={DOI}>{`doi:${DOI.slice(16)}`}</a>
     }
-    return <DOILinkFallback />
+    return <DOILinkFallback datasetId={datasetId} />
   } else {
-    return <DOILinkFallback />
+    return <DOILinkFallback datasetId={datasetId} />
   }
 }
