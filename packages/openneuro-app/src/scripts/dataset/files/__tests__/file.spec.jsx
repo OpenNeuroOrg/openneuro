daf597a356a484fe66c0fa74e20e166f701757cc
Nell Hardcastle <nell@dev-nell.com>
2022-03-16 14:10:30 -0700

    feat(app): Show file sizes as part of download tooltip

--- packages/openneuro-app/src/scripts/dataset/files/__tests__/file.spec.jsx
+++ packages/openneuro-app/src/scripts/dataset/files/__tests__/file.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
-import File from '../file.jsx'
+import File from '../file'
 
 describe('File component', () => {
   it('renders with common props', () => {
