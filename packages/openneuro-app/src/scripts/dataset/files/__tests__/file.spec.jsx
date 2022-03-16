1e44cc696466c32905f6f9ae9a8bae06a108faff
Nell Hardcastle <nell@dev-nell.com>
2022-03-16 14:11:18 -0700

    test(app): Update File component snapshot for new file size info

--- packages/openneuro-app/src/scripts/dataset/files/__tests__/file.spec.jsx
+++ packages/openneuro-app/src/scripts/dataset/files/__tests__/file.spec.jsx
@@ -6,20 +6,26 @@ import File from '../file'
 describe('File component', () => {
   it('renders with common props', () => {
     const { asFragment } = render(
-      <File datasetId="ds001" path="" filename="README" />,
+      <File datasetId="ds001" path="" filename="README" size={500} />,
       { wrapper: MemoryRouter },
     )
     expect(asFragment()).toMatchSnapshot()
   })
   it('renders for dataset snapshots', () => {
     const { asFragment } = render(
-      <File datasetId="ds001" snapshotTag="1.0.0" path="" filename="README" />,
+      <File
+        datasetId="ds001"
+        snapshotTag="1.0.0"
+        path=""
+        filename="README"
+        size={500}
+      />,
       { wrapper: MemoryRouter },
     )
     expect(asFragment()).toMatchSnapshot()
   })
   it('generates correct download links for top level files', () => {
-    render(<File datasetId="ds001" path="" filename="README" />, {
+    render(<File datasetId="ds001" path="" filename="README" size={500} />, {
       wrapper: MemoryRouter,
     })
     expect(screen.getByRole('link', { name: 'download file' })).toHaveAttribute(
@@ -33,6 +39,7 @@ describe('File component', () => {
         datasetId="ds001"
         path="sub-01:anat"
         filename="sub-01_T1w.nii.gz"
+        size={2000000}
       />,
       { wrapper: MemoryRouter },
     )
