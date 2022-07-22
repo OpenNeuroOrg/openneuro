3cfea7fb03c06984ef292243905f67179666e2d7
Nell Hardcastle <nell@dev-nell.com>
2022-07-22 10:42:33 -0700

    tests(app): Add MockAppWrapper component to quickly integration test full component trees

---
+++ packages/openneuro-app/src/scripts/fixtures/mock-app-wrapper.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { MemoryRouter } from 'react-router-dom'
+import { UserModalOpenCtx } from '../utils/user-login-modal-ctx'
+import { MockedProvider } from '@apollo/client/testing'
+
+// Provide the basics needed to render complex components with mocked router, apollo, etc
+export const MockAppWrapper = ({ children }) => {
+  const [userModalOpen, setUserModalOpen] = useState(false)
+  return (
+    <MemoryRouter>
+      <MockedProvider>
+        <UserModalOpenCtx.Provider value={{ userModalOpen, setUserModalOpen }}>
+          {children}
+        </UserModalOpenCtx.Provider>
+      </MockedProvider>
+    </MemoryRouter>
+  )
+}
