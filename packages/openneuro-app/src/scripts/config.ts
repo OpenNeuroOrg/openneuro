d0cc672960d6d853371583827b077d234e537cf4
Nell Hardcastle <nell@dev-nell.com>
2022-08-31 09:48:26 -0700

    fix: Switch back to configuration loaded as script ahead of time

--- packages/openneuro-app/src/scripts/config.ts
+++ packages/openneuro-app/src/scripts/config.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 /**
  * Interface describing the configuration object
  */
@@ -33,36 +31,36 @@ export interface OpenNeuroConfig {
 }
 
 export const config: OpenNeuroConfig = {
-  url: import.meta.env.VITE_CRN_SERVER_URL,
-  api: `${import.meta.env.VITE_CRN_SERVER_URL as string}/crn/`,
+  url: globalThis.OpenNeuroConfig.CRN_SERVER_URL,
+  api: `${globalThis.OpenNeuroConfig.CRN_SERVER_URL as string}/crn/`,
   graphql: {
-    uri: import.meta.env.VITE_GRAPHQL_URI,
+    uri: globalThis.OpenNeuroConfig.GRAPHQL_URI,
   },
   auth: {
     google: {
-      clientID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+      clientID: globalThis.OpenNeuroConfig.GOOGLE_CLIENT_ID,
     },
     globus: {
-      clientID: import.meta.env.VITE_GLOBUS_CLIENT_ID,
+      clientID: globalThis.OpenNeuroConfig.GLOBUS_CLIENT_ID,
     },
     orcid: {
-      clientID: import.meta.env.VITE_ORCID_CLIENT_ID,
-      URI: import.meta.env.VITE_ORCID_URI,
-      redirectURI: import.meta.env.VITE_ORCID_REDIRECT_URI,
+      clientID: globalThis.OpenNeuroConfig.ORCID_CLIENT_ID,
+      URI: globalThis.OpenNeuroConfig.ORCID_URI,
+      redirectURI: globalThis.OpenNeuroConfig.ORCID_REDIRECT_URI,
     },
   },
   analytics: {
-    trackingIds:
-      import.meta.env.VITE_GOOGLE_TRACKING_IDS &&
-      import.meta.env.VITE_GOOGLE_TRACKING_IDS.split(',').map(id => {
+    trackingIds: globalThis.OpenNeuroConfig.GOOGLE_TRACKING_IDS.split(',').map(
+      id => {
         return id.trim() as string
-      }),
+      },
+    ),
   },
-  sentry: { environment: import.meta.env.VITE_ENVIRONMENT },
-  support: { url: import.meta.env.VITE_SUPPORT_URL },
-  github: import.meta.env.VITE_DATALAD_GITHUB_ORG,
-  publicBucket: import.meta.env.VITE_AWS_S3_PUBLIC_BUCKET,
-  ELASTIC_APM_SERVER_URL: import.meta.env.VITE_ELASTIC_APM_SERVER_URL,
+  sentry: { environment: globalThis.OpenNeuroConfig.ENVIRONMENT },
+  support: { url: globalThis.OpenNeuroConfig.SUPPORT_URL },
+  github: globalThis.OpenNeuroConfig.DATALAD_GITHUB_ORG,
+  publicBucket: globalThis.OpenNeuroConfig.AWS_S3_PUBLIC_BUCKET,
+  ELASTIC_APM_SERVER_URL: globalThis.OpenNeuroConfig.ELASTIC_APM_SERVER_URL,
 }
 
 export const getConfig = (): OpenNeuroConfig => config
