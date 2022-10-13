3ecad22b1aacc6465a9505a770a988404002de24
Nell Hardcastle <nell@dev-nell.com>
2022-10-13 10:47:57 -0700

    tests(app): Add additional test for array field selections in SearchParamsProvider

--- packages/openneuro-app/src/scripts/search/__tests__/search-params-ctx.spec.tsx
+++ packages/openneuro-app/src/scripts/search/__tests__/search-params-ctx.spec.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import { SearchParamsProvider, SearchParamsCtx } from '../search-params-ctx'
+import AuthorInput from '../inputs/author-input'
 import { MemoryRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
 
@@ -19,7 +20,6 @@ export const searchRender = (ui, { providerProps, ...renderOptions }) => {
 describe('SearchParamsProvider', () => {
   it('restores URL searchParams state', () => {
     const route = '/search?query={"authors"%3A["Test+Author"]}'
-    const value = {}
     const wrapper = ({ children }) => (
       <MemoryRouter initialEntries={[route]}>
         <SearchParamsProvider>{children}</SearchParamsProvider>
@@ -33,6 +33,32 @@ describe('SearchParamsProvider', () => {
     )
     expect(screen.getByText(/^Received:/).textContent).toBe(
       'Received: Test Author',
+    )
+  })
+  it('edits array fields correctly', () => {
+    const route = '/search?query={"authors"%3A["Test+Author"]}'
+    const wrapper = ({ children }) => (
+      <MemoryRouter initialEntries={[route]}>
+        <SearchParamsProvider>{children}</SearchParamsProvider>
+      </MemoryRouter>
+    )
+    render(
+      <SearchParamsCtx.Consumer>
+        {value => (
+          <>
+            <AuthorInput />
+            <span>Received: {value.searchParams.authors.join(', ')}</span>
+          </>
+        )}
+      </SearchParamsCtx.Consumer>,
+      { wrapper },
+    )
+    const textInput = screen.getByRole('textbox')
+    const button = screen.getByRole('button')
+    fireEvent.change(textInput, { target: { value: 'New Author' } })
+    fireEvent.click(button)
+    expect(screen.getByText(/^Received:/).textContent).toBe(
+      'Received: Test Author, New Author',
     )
   })
 })
