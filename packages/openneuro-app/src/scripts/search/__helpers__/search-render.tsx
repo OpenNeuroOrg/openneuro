be066043c43f1f8ca9705d69775e953dff43c65e
Nell Hardcastle <nell@dev-nell.com>
2022-10-14 08:34:19 -0700

    tests(app): Add failing test for cube search navigation bug

---
+++ packages/openneuro-app/src/scripts/search/__helpers__/search-render.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SearchParamsCtx } from '../search-params-ctx'
+import { render } from '@testing-library/react'
+
+/**
+ * Render with SearchParamsCtx state
+ */
+export const searchRender = (ui, { providerProps, ...renderOptions }) => {
+  return render(
+    <SearchParamsCtx.Provider {...providerProps}>
+      {ui}
+    </SearchParamsCtx.Provider>,
+    renderOptions,
+  )
+}
