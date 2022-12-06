f2c127ca61f9c9ce4b2b0d5e2a43aecdd733eb64
Nell Hardcastle <nell@dev-nell.com>
2022-12-06 11:58:22 -0800

    feat(server): Request /read-limited ORCID scope

--- packages/openneuro-server/src/libs/authentication/passport.js
+++ packages/openneuro-server/src/libs/authentication/passport.js
@@ -173,6 +173,7 @@ export const setupPassportAuth = () => {
           config.auth.orcid.apiURI.includes('sandbox'),
         clientID: config.auth.orcid.clientID,
         clientSecret: config.auth.orcid.clientSecret,
+        scope: '/read-limited',
         callbackURL: `${config.url + config.apiPrefix}auth/orcid/callback`,
       },
       verifyORCIDUser,
