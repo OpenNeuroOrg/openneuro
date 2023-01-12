aa9fc848068f2aac9286a199dd9b952af2908d8c
Nell Hardcastle <nell@dev-nell.com>
2023-01-12 10:06:02 -0800

    fix(cli): Fix warning about max listeners while uploading files

--- packages/openneuro-cli/src/upload.js
+++ packages/openneuro-cli/src/upload.js
@@ -1,3 +1,4 @@
+import { setMaxListeners } from 'events'
 import cliProgress from 'cli-progress'
 import path from 'path'
 import inquirer from 'inquirer'
@@ -152,6 +153,8 @@ export const uploadFiles = async ({
   const controller = new AbortController()
   // Limit open file handles for streams to avoid consuming extra file handles
   const MAX_STREAM_HANDLES = 512
+  // Raise listeners to stream handles + 16 slots for downstream listeners
+  setMaxListeners(MAX_STREAM_HANDLES + 16, controller.signal)
   for (let n = 0; n < files.length; n += MAX_STREAM_HANDLES) {
     const filesChunk = files.slice(n, n + MAX_STREAM_HANDLES)
     const requests = filesChunk.map(file => {
