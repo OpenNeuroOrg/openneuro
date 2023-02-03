5052721de5f942f7e055e495dfc9f0254ae510ae
Nell Hardcastle <nell@dev-nell.com>
2023-02-02 17:08:54 -0800

    fix(cli): Fix incorrect import in configuredClient for fetch

--- packages/openneuro-cli/src/configuredClient.js
+++ packages/openneuro-cli/src/configuredClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@openneuro/client'
 import { getToken, getUrl } from './config.js'
 import { version } from './lerna.json'
-import { fetch } from 'node-fetch'
+import fetch from 'node-fetch'
 
 export const configuredClient = () =>
   createClient(`${getUrl()}crn/graphql`, {
