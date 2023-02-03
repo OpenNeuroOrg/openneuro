a65549331e064ce1b2d4369fc22476919a011b71
Nell Hardcastle <nell@dev-nell.com>
2023-02-02 16:49:20 -0800

    fix(cli): Revert unconfigurable fetch for @openneuro/client
    See #2715 - reverted due to issues caused by native fetch

--- packages/openneuro-cli/src/configuredClient.js
+++ packages/openneuro-cli/src/configuredClient.js
@@ -1,9 +1,11 @@
 import { createClient } from '@openneuro/client'
 import { getToken, getUrl } from './config.js'
 import { version } from './lerna.json'
+import { fetch } from 'node-fetch'
 
 export const configuredClient = () =>
   createClient(`${getUrl()}crn/graphql`, {
     getAuthorization: getToken,
     clientVersion: version,
+    fetch: fetch,
   })
