f7959671113aa46f615c1da72e1eb1533bf672f9
Nell Hardcastle <nell@dev-nell.com>
2023-03-07 13:09:39 -0800

    feat(comments): Use oauth preferred name for comments and include ORCID links if available

--- packages/openneuro-app/src/scripts/dataset/comments/__tests__/comment.spec.jsx
+++ packages/openneuro-app/src/scripts/dataset/comments/__tests__/comment.spec.jsx
@@ -17,7 +17,31 @@ describe('Comment component', () => {
         data={{
           id: '9001',
           text: emptyState,
-          user: { id: '1234', email: 'example@example.com' },
+          user: {
+            id: '1234',
+            email: 'example@example.com',
+            name: 'Example Exampler',
+          },
+          createDate: new Date('2019-04-02T19:56:41.222Z').toISOString(),
+        }}
+      />,
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+  it('renders an ORCID user comment', () => {
+    formatDistanceToNow.mockReturnValueOnce('almost 2 years')
+
+    const wrapper = render(
+      <Comment
+        data={{
+          id: '9001',
+          text: emptyState,
+          user: {
+            id: '1234',
+            email: 'example@example.com',
+            name: 'Example Exampler',
+            orcid: '1234-5678-9101',
+          },
           createDate: new Date('2019-04-02T19:56:41.222Z').toISOString(),
         }}
       />,
