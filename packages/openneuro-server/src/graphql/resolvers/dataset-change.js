c76f37811f6bf8bffdff29ce9045c40268e3b7f1
Nell Hardcastle <nell@dev-nell.com>
2023-04-03 11:07:30 -0700

    fix(server): Clean up unused dataset changes model

--- packages/openneuro-server/src/graphql/resolvers/dataset-change.js
+++
@@ -1,5 +0,0 @@
-import DatasetChange from '../../models/datasetChange'
-
-export const datasetChanges = (_, { limit = 100 }) => {
-  return DatasetChange.find().sort({ $natural: -1 }).limit(limit)
-}
