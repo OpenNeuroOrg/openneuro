d3a8f7324290f49884b1f416a782fe2401e74021
Nell Hardcastle <nell@dev-nell.com>
2023-04-06 10:54:43 -0700

    fix(server): Convert DataLad file utils to TypeScript

--- packages/openneuro-server/src/datalad/files.ts
+++ packages/openneuro-server/src/datalad/files.ts
@@ -7,7 +7,7 @@ import { getDatasetWorker } from '../libs/datalad-service'
  * Convert to URL compatible path
  * @param {String} path
  */
-export const encodeFilePath = path => {
+export const encodeFilePath = (path: string): string => {
   return path.replace(new RegExp('/', 'g'), ':')
 }
 
@@ -15,7 +15,7 @@ export const encodeFilePath = path => {
  * Convert to from URL compatible path fo filepath
  * @param {String} path
  */
-export const decodeFilePath = path => {
+export const decodeFilePath = (path: string): string => {
   return path.replace(new RegExp(':', 'g'), '/')
 }
 
@@ -24,19 +24,24 @@ export const decodeFilePath = path => {
  * @param {String} path
  * @param {String} filename
  */
-export const getFileName = (path, filename) => {
+export const getFileName = (path: string, filename: string): string => {
   const filePath = path ? [path, filename].join('/') : filename
   return filename ? encodeFilePath(filePath) : encodeFilePath(path)
 }
 
 /**
  * Generate file URL for DataLad service
- * @param {String} datasetId
- * @param {String} path - Relative path for the file
- * @param {String} filename
- * @param {String} [revision] - Git hash of commit or tree owning this file
+ * @param {string} datasetId
+ * @param {string} path - Relative path for the file
+ * @param {string} filename
+ * @param {string} [revision] - Git hash of commit or tree owning this file
  */
-export const fileUrl = (datasetId, path, filename, revision) => {
+export const fileUrl = (
+  datasetId: string,
+  path: string,
+  filename: string,
+  revision: string,
+): string => {
   const fileName = getFileName(path, filename)
   if (revision) {
     return `http://${getDatasetWorker(
@@ -53,13 +58,21 @@ export const fileUrl = (datasetId, path, filename, revision) => {
  * Generate path URL (such a directory or virtual path) for DataLad service
  * @param {String} datasetId
  */
-export const filesUrl = datasetId =>
+export const filesUrl = (datasetId: string): string =>
   `http://${getDatasetWorker(datasetId)}/datasets/${datasetId}/files`
+
+/** Minimal variant of DatasetFile type from GraphQL API */
+type DatasetFile = {
+  id: string
+  filename: string
+  directory: boolean
+  size: number
+}
 
 /**
  * Sum all file sizes for total dataset size
  */
-export const computeTotalSize = files =>
+export const computeTotalSize = (files: [DatasetFile]): number =>
   files.reduce((size, f) => size + f.size, 0)
 
 /**
@@ -89,44 +102,3 @@ export const getFiles = (datasetId, treeish) => {
       }),
   )
 }
-
-/**
- * Given a list of files (from getFiles), return a subset matching the prefix
- * @param {string} prefix The prefix to filter on
- * @returns {(files: Object[]) => Object[]}
- */
-export const filterFiles =
-  (prefix = '') =>
-  files => {
-    // Disable on null
-    if (prefix === null) {
-      return files
-    }
-    // Track potential directories and include those as "files"
-    const directoryFacades = {}
-    // Return only root level files if prefix is set
-    const matchingFiles = files.filter(f => {
-      if (prefix === '') {
-        if (f.filename.includes('/')) {
-          const dirName = f.filename.split('/').slice(0, 1)[0]
-          if (directoryFacades[dirName] !== undefined) {
-            directoryFacades[dirName].size += 1
-          } else {
-            directoryFacades[dirName] = {
-              id: `directory:${dirName}`,
-              urls: [],
-              filename: dirName,
-              size: 1,
-              directory: true,
-            }
-          }
-          return false
-        } else {
-          return true
-        }
-      } else {
-        return f.filename.startsWith(prefix)
-      }
-    })
-    return [...matchingFiles, ...Object.values(directoryFacades)]
-  }
