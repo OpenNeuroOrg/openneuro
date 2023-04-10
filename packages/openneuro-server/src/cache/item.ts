177f38b3ad6d4995a94be2bc9b13c46e4d48d45c
Nell Hardcastle <nell@dev-nell.com>
2023-04-10 16:56:28 -0700

    feat(server): Add support for disabling caching on non-erroring miss function

--- packages/openneuro-server/src/cache/item.ts
+++ packages/openneuro-server/src/cache/item.ts
@@ -52,26 +52,34 @@ class CacheItem {
     const deserialized: T = JSON.parse(decompressed.toString())
     return deserialized
   }
-  public async get<T>(miss: () => Promise<T>): Promise<T> {
+  public async get<T>(
+    miss: (setDoNotCache?: (doNotCache) => void) => Promise<T>,
+  ): Promise<T> {
     try {
       const data = await this.redis.getBuffer(this.key)
       if (data) {
         return this.deserialize(data)
       } else {
+        let skipCaching = false
+        const doNotCache = (doNotCache: boolean): void => {
+          skipCaching = doNotCache
+        }
         // Call the cache miss function if we didn't get anything
-        const data = await miss()
+        const data = await miss(doNotCache)
         const serialized = await this.serialize(data)
-        // Allow for the simple case of aging out keys
-        if (this.expiration > 0) {
-          void this.redis.setex(this.key, this.expiration, serialized)
-        } else {
-          void this.redis.set(this.key, serialized)
+        // Allow the cache miss function to return a value that isn't cached
+        if (!skipCaching) {
+          // Allow for the simple case of aging out keys
+          if (this.expiration > 0) {
+            void this.redis.setex(this.key, this.expiration, serialized)
+          } else {
+            void this.redis.set(this.key, serialized)
+          }
         }
         return data
       }
     } catch {
       // Keep going as though we had a cache miss if there is a problem but don't cache it
-      // TODO: Sentry reporting doesn't work here but should be fixed
       return miss()
     }
   }
