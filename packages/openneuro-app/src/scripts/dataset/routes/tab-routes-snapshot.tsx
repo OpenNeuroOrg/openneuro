4650aa45ae0c77f8d5f0bcda181cb9c71fe97e20
Nell Hardcastle <nell@dev-nell.com>
2023-04-13 12:27:54 -0700

    refactor(app): Download files via url props instead of generated API path

--- packages/openneuro-app/src/scripts/dataset/routes/tab-routes-snapshot.tsx
+++ packages/openneuro-app/src/scripts/dataset/routes/tab-routes-snapshot.tsx
@@ -39,7 +39,11 @@ export const TabRoutesSnapshot = ({ dataset, snapshot }) => {
       <Route
         path="file-display/:filePath"
         element={
-          <FileDisplayRoute datasetId={dataset.id} snapshotTag={snapshot.tag} />
+          <FileDisplayRoute
+            dataset={snapshot}
+            datasetId={dataset.id}
+            snapshotTag={snapshot.tag}
+          />
         }
       />
       <Route path="metadata" element={<AddMetadata dataset={dataset} />} />
