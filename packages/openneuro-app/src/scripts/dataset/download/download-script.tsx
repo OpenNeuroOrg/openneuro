f4f7bcd3976299e972a5fcb0e691e6e4725d07d1
Nell Hardcastle <nell@dev-nell.com>
2023-04-25 15:28:39 -0700

    feat(app): Add a bash script generator for snapshot downloads

---
+++ packages/openneuro-app/src/scripts/dataset/download/download-script.tsx
@@ -0,0 +1,94 @@
+/**
+ * Generate a download script for this dataset
+ */
+import React from 'react'
+import { useLazyQuery, gql } from '@apollo/client'
+
+function inlineDownload(filename, data): void {
+  const element = document.createElement('a')
+  element.setAttribute(
+    'href',
+    'data:text/plain;charset=utf-8,' + encodeURIComponent(data),
+  )
+  element.setAttribute('download', filename)
+
+  element.style.display = 'none'
+  document.body.appendChild(element)
+
+  element.click()
+
+  document.body.removeChild(element)
+}
+
+function generateDownloadScript(data): string {
+  let script = '#!/bin/sh\n'
+  for (const f of data.snapshot.downloadFiles) {
+    script += `curl --create-dirs ${f.urls[0]} -o ${f.filename}\n`
+  }
+  return script
+}
+
+interface DownloadS3DerivativesProps {
+  datasetId: string
+  snapshotTag: string
+}
+
+const getSnapshotDownload = gql`
+  query snapshot($datasetId: ID!, $tag: String!) {
+    snapshot(datasetId: $datasetId, tag: $tag) {
+      id
+      downloadFiles {
+        id
+        directory
+        filename
+        urls
+        size
+      }
+    }
+  }
+`
+
+export const DownloadScript = ({
+  datasetId,
+  snapshotTag,
+}: DownloadS3DerivativesProps): JSX.Element => {
+  const [getDownload, { loading, data }] = useLazyQuery(getSnapshotDownload, {
+    variables: {
+      datasetId: datasetId,
+      tag: snapshotTag,
+    },
+    errorPolicy: 'all',
+  })
+  if (data) {
+    const script = generateDownloadScript(data)
+    inlineDownload(`${datasetId}-${snapshotTag}.sh`, script)
+  }
+  // This feature is only implemented for snapshots
+  if (snapshotTag) {
+    return (
+      <div>
+        <h4>Download with a shell script</h4>
+        <p>
+          A script is available to download with only curl. This may be useful
+          if your download environment makes it difficult to install DataLad or
+          Node.js.
+        </p>
+        <p>
+          {loading ? (
+            'Loading...'
+          ) : (
+            <a
+              href="#"
+              onClick={(): void => {
+                void getDownload()
+              }}>
+              Download shell script
+            </a>
+          )}
+        </p>
+      </div>
+    )
+  }
+}
+
+export default DownloadScript
