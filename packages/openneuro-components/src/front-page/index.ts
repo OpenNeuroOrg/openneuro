2337b6e624fec21ae2317fca16abdf07d6f5a1ba
Nell Hardcastle <nell@dev-nell.com>
2023-07-13 13:47:45 -0700

    chore(app): Cleanup unused mock content

--- packages/openneuro-components/src/front-page/index.ts
+++ packages/openneuro-components/src/front-page/index.ts
@@ -3,14 +3,5 @@ import { Contributors } from './Contributors'
 import { GetUpdates } from './GetUpdates'
 import { Infographic } from './Infographic'
 import { ActivityHeader } from './ActivityHeader'
-import { RecentData, TopViewed } from '../content/activity-slider-content'
 
-export {
-  AffiliateBlock,
-  Contributors,
-  GetUpdates,
-  Infographic,
-  ActivityHeader,
-  RecentData,
-  TopViewed,
-}
+export { AffiliateBlock, Contributors, GetUpdates, Infographic, ActivityHeader }
