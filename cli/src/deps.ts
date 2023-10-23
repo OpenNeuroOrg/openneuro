5694bd5a31b2ba3f366a005f3c15a9085e990c2f
Nell Hardcastle <nell@dev-nell.com>
2023-10-22 21:49:53 -0700

    fix(cli): Add Deno CLI support for git-credential use

--- cli/src/deps.ts
+++ cli/src/deps.ts
@@ -11,3 +11,4 @@ export type { ValidatorOptions } from 'https://deno.land/x/bids_validator@v1.13.
 export * as log from 'https://deno.land/std@0.203.0/log/mod.ts'
 export { LogLevelNames } from 'https://deno.land/std@0.203.0/log/levels.ts'
 export type { LevelName } from 'https://deno.land/std@0.203.0/log/mod.ts'
+export { TextLineStream } from 'https://deno.land/std@0.203.0/streams/mod.ts'
