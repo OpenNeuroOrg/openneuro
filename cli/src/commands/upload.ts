e5a179279aa38f4609834f2c287c31df013e4e0f
Nell Hardcastle <nell@dev-nell.com>
2023-11-15 16:27:18 -0800

    feat(cli): Add uppy support

--- cli/src/commands/upload.ts
+++ cli/src/commands/upload.ts
@@ -1,8 +1,14 @@
+import { XMLHttpRequest } from "https://deno.land/x/xhr@0.3.1/mod.ts"
+globalThis.XMLHttpRequest = XMLHttpRequest
 import { validateCommand } from "./validate.ts"
 import { ClientConfig, getConfig } from "./login.ts"
 import { logger } from "../logger.ts"
 import { resolve, walk } from "../deps.ts"
 import type { CommandOptions } from "../deps.ts"
+import {
+  Tus,
+  Uppy,
+} from "https://releases.transloadit.com/uppy/v3.18.1/uppy.min.mjs"
 
 export function readConfig(): ClientConfig {
   const config = getConfig()
@@ -27,16 +33,32 @@ export async function uploadAction(
     `upload ${dataset_directory} resolved to ${dataset_directory_abs}`,
   )
   // TODO - call the validator here
-
+  const uppy = new Uppy({ id: "@openneuro/cli", autoProceed: true }).use(Tus, {
+    endpoint: "http://localhost:9876/api/tusd",
+    chunkSize: 64000000,
+    uploadLengthDeferred: true,
+    headers: {
+      Authorization: `Bearer ${clientConfig.token}`,
+    },
+  })
   for await (const walkEntry of walk(dataset_directory)) {
-    logger.debug(JSON.stringify(walkEntry))
+    const file = await Deno.open(walkEntry.path)
+    const stat = await file.stat()
+    const relativePath = resolve(walkEntry.path)
+    const uppyFile = {
+      name: walkEntry.name,
+      data: file,
+      meta: {
+        relativePath,
+      },
+      size: stat.size,
+      tus: {
+        uploadSize: stat.size,
+      },
+    }
+    logger.debug(JSON.stringify({ name: uppyFile.name, meta: uppyFile.meta }))
+    uppy.addFile(uppyFile)
   }
-}
-
-/**
- * Given a path and context for the upload, add this file to an upload
- */
-export async function uploadFile(path) {
 }
 
 /**
