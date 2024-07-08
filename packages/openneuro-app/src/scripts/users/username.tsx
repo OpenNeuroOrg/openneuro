d1ec513c6e3d026c264ee258c532bbfa6b35a72b
Nell Hardcastle <nell@dev-nell.com>
2024-07-07 20:58:52 -0700

    feat(app): Detect ORCID sandbox API when linking to profiles

--- packages/openneuro-app/src/scripts/users/username.tsx
+++ packages/openneuro-app/src/scripts/users/username.tsx
@@ -1,15 +1,21 @@
 import React from "react"
 import ORCIDiDLogo from "../../assets/ORCIDiD_iconvector.svg"
+import { config } from "../config"
 
 /**
  * Display component for usernames showing ORCID linking if connected
  */
 export const Username = ({ user }): JSX.Element => {
   if (user.orcid) {
+    let orcidURL = "https://orcid.org/"
+    console.log(config.auth.orcid)
+    if (config.auth.orcid.ORCID_API_ENDPOINT.includes("sandbox")) {
+      orcidURL = "https://sandbox.orcid.org/"
+    }
     return (
       <>
         {user.name}{" "}
-        <a href={`https://orcid.org/${user.orcid}`}>
+        <a href={`${orcidURL}${user.orcid}`}>
           <img src={ORCIDiDLogo} width="16" height="16" alt="ORCID logo" />
         </a>
       </>
