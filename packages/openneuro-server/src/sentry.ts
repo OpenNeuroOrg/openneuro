2bf231d16406af49420840611fc7abf3fed06d40
Nell Hardcastle <nell@dev-nell.com>
2024-08-09 21:25:25 -0700

    fix(server): Use correct format for Sentry release

--- packages/openneuro-server/src/sentry.ts
+++ packages/openneuro-server/src/sentry.ts
@@ -13,5 +13,5 @@ Sentry.init({
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set sampling rate for profiling - this is relative to tracesSampleRate
   profilesSampleRate: 1.0,
-  release: `@openneuro/server@${version}`,
+  release: `openneuro-server@${version}`,
 })
