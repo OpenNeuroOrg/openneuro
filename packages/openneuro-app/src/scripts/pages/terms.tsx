eb3bfc096d431440475e5a9c7e18af27625923b4
Nell Hardcastle <nell@dev-nell.com>
2024-08-14 14:13:52 -0700

    fix(app): Add download agreement to terms page.

--- packages/openneuro-app/src/scripts/pages/terms.tsx
+++ packages/openneuro-app/src/scripts/pages/terms.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement } from "react"
 import { Terms } from "../common/content/terms"
 import Helmet from "react-helmet"
 import { frontPage } from "./front-page/front-page-content"
+import { DownloadAgreement } from "../common/content/download-agreement"
 import styled from "@emotion/styled"
 
 const TermsPageStyle = styled.div`
@@ -24,12 +25,14 @@ export function TermsPage(): ReactElement {
         />
       </Helmet>
       <div className="container">
-        <h2>OpenNeuro Upload Terms and Conditions</h2>
-        <p>
+        <h2>OpenNeuro Terms and Conditions</h2>
+        <h3>
           By uploading to the {frontPage.pageTitle}{" "}
           data archive I agree to the following conditions:
-        </p>
+        </h3>
         <Terms />
+        <h3>Downloading Data:</h3>
+        <p>{DownloadAgreement}</p>
       </div>
     </TermsPageStyle>
   )
