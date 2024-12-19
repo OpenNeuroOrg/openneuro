94bb1c58768c6ab59cb74e9da308e925d8cd4f06
Gregory Noack <gregorynoack@thinknoack.com>
2024-12-19 14:30:31 -0800

    copy non admin test for orcid

--- packages/openneuro-server/src/graphql/resolvers/__tests__/user.spec.ts
+++ packages/openneuro-server/src/graphql/resolvers/__tests__/user.spec.ts
@@ -13,6 +13,15 @@ describe("user resolvers", () => {
           { userInfo: {} },
         ),
       ).rejects.toEqual(new Error("You must be a site admin to retrieve users"))
-    })
-  })
-})
+    });
+    it("throws an error for non-admins", () => {
+      expect(
+        users(
+          null,
+          { id: "0000-0000-0000-000X" },
+          { userInfo: {} },
+        ),
+      ).rejects.toEqual(new Error("You must be a site admin to retrieve users"))
+    });
+  });
+});
