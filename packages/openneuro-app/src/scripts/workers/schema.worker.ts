5e2dbd1f1c9a873355e45b5fc8a666c8cde15fc4
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-02-04 10:43:34 -0500

    feat: Demote PARTICIPANT_ID_MISMATCH to a warning for now

--- packages/openneuro-app/src/scripts/workers/schema.worker.ts
+++ packages/openneuro-app/src/scripts/workers/schema.worker.ts
@@ -9,6 +9,12 @@ const config: Config = {
     { code: "SUBJECT_FOLDERS" }, // bids-standard/bids-specification#1928 downgrades to warning
     { code: "EMPTY_DATASET_NAME" },
   ],
+  warning: [
+    // Pending update to BIDS that enforces participants.tsv to be a superset
+    // of subject directories and phenotype participant_id columns
+    // https://github.com/bids-standard/bids-specification/issues/914
+    { code: "PARTICIPANT_ID_MISMATCH" },
+  ],
 }
 
 const options: ValidatorOptions = {
