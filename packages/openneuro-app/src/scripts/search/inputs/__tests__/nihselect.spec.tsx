922493eca14abbbc67c6d196827cf9099511a73e
Gregory Noack <gregorynoack@thinknoack.com>
2025-02-10 13:38:17 -0800

    removing unness comments

--- packages/openneuro-app/src/scripts/search/inputs/__tests__/nihselect.spec.tsx
+++ packages/openneuro-app/src/scripts/search/inputs/__tests__/nihselect.spec.tsx
@@ -4,7 +4,6 @@ import { MemoryRouter, Route, Routes } from "react-router-dom"
 import { SearchParamsCtx } from "../../search-params-ctx"
 import NIHSelect from "../nih-select"
 
-// Create a custom wrapper to mock context and routing
 const customRender = (
   ui,
   { searchParams = {}, initialRoute = "/search" } = {},
@@ -27,11 +26,9 @@ describe("NIHSelect Component", () => {
   it("updates search params when NIH is selected and navigates to the NIH page", async () => {
     customRender(<NIHSelect label="NIH Funding" />)
 
-    // Simulate selecting the NIH option
     const nihOption = screen.getByText("NIH Funding")
     fireEvent.click(nihOption)
 
-    // Wait for the component to navigate and verify the resulting page
     await waitFor(() => {
       expect(screen.getByText("NIH Brain Initiative")).toBeInTheDocument()
     })
