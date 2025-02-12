20d2a8908fe23fc96ae72ba2827dd4e3d31eec87
Nell Hardcastle <nell@dev-nell.com>
2025-02-12 11:39:44 -0800

    fix: Consolidate validator configuration to one JSON file and symlinks

--- packages/openneuro-app/src/scripts/workers/schema.worker.ts
+++ packages/openneuro-app/src/scripts/workers/schema.worker.ts
@@ -2,20 +2,9 @@
 import { fileListToTree, validate } from "@bids/validator/main"
 import type { ValidationResult } from "@bids/validator/main"
 import type { Config, ValidatorOptions } from "@bids/validator/options"
+import validatorConfig from "./validator-config.json"
 
-const config: Config = {
-  error: [
-    { code: "NO_AUTHORS" },
-    { code: "SUBJECT_FOLDERS" }, // bids-standard/bids-specification#1928 downgrades to warning
-    { code: "EMPTY_DATASET_NAME" },
-  ],
-  warning: [
-    // Pending update to BIDS that enforces participants.tsv to be a superset
-    // of subject directories and phenotype participant_id columns
-    // https://github.com/bids-standard/bids-specification/issues/914
-    { code: "PARTICIPANT_ID_MISMATCH" },
-  ],
-}
+const config: Config = validatorConfig
 
 const options: ValidatorOptions = {
   datasetPath: "browser",
