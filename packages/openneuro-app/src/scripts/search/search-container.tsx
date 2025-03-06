0eeea62202abe9850cd1cc27f37653ae82163a34
Nell Hardcastle <nell@dev-nell.com>
2025-03-06 15:05:16 -0800

    Remove unused useState import

--- packages/openneuro-app/src/scripts/search/search-container.tsx
+++ packages/openneuro-app/src/scripts/search/search-container.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext, useEffect } from "react"
 import type { FC } from "react"
 import { useLocation } from "react-router-dom"
 import { SearchPage } from "../components/search-page/SearchPage"
