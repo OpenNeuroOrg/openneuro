28aca0f0dfecebe06b81a44ac588e44141c66554
Nell Hardcastle <nell@dev-nell.com>
2025-03-26 10:41:44 -0700

    fix(app): Correct sign out behavior to work regardless of path

--- packages/openneuro-app/src/scripts/authentication/signOut.ts
+++ packages/openneuro-app/src/scripts/authentication/signOut.ts
@@ -2,7 +2,7 @@ import cookies from "../utils/cookies.js"
 
 const signOut = () => {
   // Delete the token will reset client login state
-  cookies.remove("accessToken")
+  cookies.remove("accessToken", { path: "/" })
 }
 
 export default signOut
