b69812e9b67c401382b5a18291e2019016d6932b
Gregory Noack <gregorynoack@thinknoack.com>
2025-05-22 13:25:57 -0700

    reset to default spec

--- packages/openneuro-server/src/graphql/resolvers/__tests__/user.spec.ts
+++ packages/openneuro-server/src/graphql/resolvers/__tests__/user.spec.ts
@@ -1,54 +1,27 @@
 import { vi } from "vitest"
 import { users } from "../user.js"
-import { UserInfo } from "../user.js"
 
 vi.mock("ioredis")
 
 describe("user resolvers", () => {
   describe("users()", () => {
-    it("throws an error for non-admins when no filter is provided", () => {
-      const mockUserInfo: UserInfo = { userId: "testId", admin: false }
-
+    it("throws an error for non-admins", () => {
       expect(
         users(
           null,
-          { isAdmin: null, isBlocked: false },
-          { userInfo: mockUserInfo },
+          { id: "3311cfe8-9764-434d-b80e-1b1ee72c686d" },
+          { userInfo: {} },
         ),
       ).rejects.toEqual(new Error("You must be a site admin to retrieve users"))
     })
-
-    it("throws an error for non-admins when a filter is provided", () => {
-      const mockUserInfo: UserInfo = { userId: "testId", admin: false }
-
+    it("throws an error for non-admins", () => {
       expect(
         users(
           null,
-          { isAdmin: true, isBlocked: false },
-          { userInfo: mockUserInfo },
+          { id: "0000-0000-0000-000X" },
+          { userInfo: {} },
         ),
       ).rejects.toEqual(new Error("You must be a site admin to retrieve users"))
-    })
-
-    it("allows admins to retrieve users", () => {
-      const mockAdminInfo: UserInfo = { userId: "adminId", admin: true }
-
-      const mockFind = vi.fn().mockReturnValue({
-        exec: vi.fn().mockResolvedValue([{ id: "1" }, { id: "2" }]),
-      })
-      vi.spyOn(require("../../models/user.js"), "default").mockReturnValue({
-        find: mockFind,
-      })
-
-      expect(
-        users(
-          null,
-          { isAdmin: null, isBlocked: false },
-          { userInfo: mockAdminInfo },
-        ),
-      ).resolves.toEqual([{ id: "1" }, { id: "2" }])
-
-      vi.restoreAllMocks()
     })
   })
 })
