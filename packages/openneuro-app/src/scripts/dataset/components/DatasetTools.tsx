7780c79458c756edf59f6b69fc3623fef35a97be
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-06-20 09:23:48 -0400

    feat: Nudge users to agree to ToS in download tooltips

--- packages/openneuro-app/src/scripts/dataset/components/DatasetTools.tsx
+++ packages/openneuro-app/src/scripts/dataset/components/DatasetTools.tsx
@@ -99,7 +99,7 @@ export const DatasetTools = ({
         />
       )}
       <DatasetToolButton
-        tooltip="How to Download"
+        tooltip={agree ? "How to Download" : "Agree to Terms of Service to Download"}
         path={snapshotId
           ? `/datasets/${datasetId}/versions/${snapshotId}/download`
           : `/datasets/${datasetId}/download`}
@@ -109,7 +109,7 @@ export const DatasetTools = ({
       />
       {hasDerivatives && (
         <DatasetToolButton
-          tooltip="Available Derivatives"
+          tooltip={agree ? "Available Derivatives" : "Agree to Terms of Service to access Derivatives"}
           path={snapshotId
             ? `/datasets/${datasetId}/versions/${snapshotId}/derivatives`
             : `/datasets/${datasetId}/derivatives`}
