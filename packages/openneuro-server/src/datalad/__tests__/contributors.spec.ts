73ac81b1842a6a8e73e5b45ced4372322a2fff23
Gregory Noack <gregorynoack@thinknoack.com>
2025-06-30 17:00:48 -0700

    no unused vars

--- packages/openneuro-server/src/datalad/__tests__/contributors.spec.ts
+++ packages/openneuro-server/src/datalad/__tests__/contributors.spec.ts
@@ -1,4 +1,4 @@
-import { afterEach, beforeEach, describe, expect, it, vi } from "vitest"
+import { beforeEach, describe, expect, it, vi } from "vitest"
 import yaml from "js-yaml"
 import * as Sentry from "@sentry/node"
 import CacheItem from "../../cache/item"
