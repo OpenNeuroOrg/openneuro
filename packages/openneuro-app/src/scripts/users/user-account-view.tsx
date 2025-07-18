0559c75cf1425f8fbfc14707bb065c299420bae9
Gregory Noack <gregorynoack@thinknoack.com>
2025-07-18 15:07:05 -0700

    update mutation values

--- packages/openneuro-app/src/scripts/users/user-account-view.tsx
+++ packages/openneuro-app/src/scripts/users/user-account-view.tsx
@@ -32,7 +32,7 @@ export const UserAccountView: React.FC<UserAccountViewProps> = ({
         refetchQueries: [
           {
             query: GET_USER,
-            variables: { userId: orcidUser?.orcid },
+            variables: { id: orcidUser?.orcid },
           },
         ],
       })
@@ -53,7 +53,7 @@ export const UserAccountView: React.FC<UserAccountViewProps> = ({
         refetchQueries: [
           {
             query: GET_USER,
-            variables: { userId: orcidUser?.orcid },
+            variables: { id: orcidUser?.orcid },
           },
         ],
       })
@@ -74,7 +74,7 @@ export const UserAccountView: React.FC<UserAccountViewProps> = ({
         refetchQueries: [
           {
             query: GET_USER,
-            variables: { userId: orcidUser?.orcid },
+            variables: { id: orcidUser?.orcid },
           },
         ],
       })
