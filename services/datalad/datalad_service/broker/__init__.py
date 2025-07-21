9a0093829672144039a93a6c4014b0a3befa8160
Nell Hardcastle <nell@dev-nell.com>
2025-07-21 09:28:06 -0700

    fix(worker): Mock validate_dataset task kiq method

--- services/datalad/datalad_service/broker/__init__.py
+++ services/datalad/datalad_service/broker/__init__.py
@@ -1,15 +1,20 @@
+import sys
+
+from taskiq import InMemoryBroker
+from taskiq_redis import RedisAsyncResultBackend, RedisStreamBroker
+
 from datalad_service import config
 from datalad_service.broker.get_docker_scale import get_docker_scale
 
-from taskiq_redis import RedisAsyncResultBackend, RedisStreamBroker
-
-redis_url = f'redis://{config.REDIS_HOST}:{config.REDIS_PORT}/{get_docker_scale()}'
-
-result_backend = RedisAsyncResultBackend(
-    redis_url=redis_url,
-    result_ex_time=5000,
-)
-
-broker = RedisStreamBroker(
-    url=redis_url,
-).with_result_backend(result_backend)
+# Use InMemoryBroker to run during pytest
+if 'pytest' in sys.modules:
+    broker = InMemoryBroker()
+else:
+    redis_url = f'redis://{config.REDIS_HOST}:{config.REDIS_PORT}/{get_docker_scale()}'
+    result_backend = RedisAsyncResultBackend(
+        redis_url=redis_url,
+        result_ex_time=5000,
+    )
+    broker = RedisStreamBroker(
+        url=redis_url,
+    ).with_result_backend(result_backend)
