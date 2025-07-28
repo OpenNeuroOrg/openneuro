681464f01251a16afc1eb318c0730325da94bee9
Nell Hardcastle <nell@dev-nell.com>
2025-07-28 13:34:32 -0700

    tests(worker): Add smoke test for maintenance tasks

--- services/datalad/datalad_service/tasks/maintenance.py
+++ services/datalad/datalad_service/tasks/maintenance.py
@@ -42,10 +42,11 @@ fsck_dataset_generator = dataset_factory()
 
 
 @broker.task(schedule=[{'cron': '*/15 * * * *'}])
-def gc_dataset():
+def gc_dataset(dataset_path=None):
     """Run git gc on a random dataset periodically."""
     try:
-        dataset_path = next(gc_dataset_generator)
+        if not dataset_path:
+            dataset_path = next(gc_dataset_generator)
     except StopIteration:
         logging.info('No datasets available for git gc.')
         return
@@ -63,10 +64,11 @@ def gc_dataset():
 
 
 @broker.task(schedule=[{'cron': '7 * * * *'}])
-def git_fsck_dataset():
+def git_fsck_dataset(dataset_path=None):
     """Routinely verify git repository integrity and produce an error if any issues are reported."""
     try:
-        dataset_path = next(fsck_dataset_generator)
+        if not dataset_path:
+            dataset_path = next(fsck_dataset_generator)
     except StopIteration:
         logging.info('No datasets available for git fsck.')
         return
