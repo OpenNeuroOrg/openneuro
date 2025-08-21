15a4984c74031e1f2296cce0b07224c2e72d0647
Gregory Noack <gregorynoack@thinknoack.com>
2025-08-21 15:00:14 -0700

    adding styling for file-check in dataset version/snapshot tab

--- packages/openneuro-app/src/scripts/dataset/fragments/file-check-list.tsx
+++ packages/openneuro-app/src/scripts/dataset/fragments/file-check-list.tsx
@@ -1,33 +1,37 @@
 import React from "react"
+import styles from "./scss/file-check-list.module.scss"
 
 export const FileCheckList = ({ fileCheck }) => {
   if (!fileCheck) return null
   return (
-    <table className="table file-check-table">
-      <thead>
-        <tr>
-          <th>File</th>
-          <th>Key</th>
-          <th>Error Messages</th>
-        </tr>
-      </thead>
-      <tbody>
-        {fileCheck.annexFsck.map((item, index) => (
-          <tr key={index}>
-            <td>{item.file}</td>
-            <td>{item.key}</td>
-            <td>
-              {item.errorMessages && item.errorMessages.length > 0
-                ? (
-                  <ul>
-                    {item.errorMessages.map((msg, i) => <li key={i}>{msg}</li>)}
-                  </ul>
-                )
-                : null}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className={styles["fileCheck-container"]}>
+      {fileCheck.annexFsck.map((item, index) => (
+        <div key={index} className={styles["fileCheck-card"]}>
+          <div className={styles["fileCheck-header"]}>
+            <span className={styles["fileCheck-fileIcon"]}>
+              <i className="fas fa-file"></i>
+            </span>
+            <span className={styles["fileCheck-fileName"]}>{item.file}</span>
+          </div>
+          <div className={styles["fileCheck-body"]}>
+            <div className={styles["fileCheck-key"]}>
+              <span className={styles["fileCheck-label"]}>Key:</span>
+              <span className={styles["fileCheck-value"]}>{item.key}</span>
+            </div>
+            {item.errorMessages && item.errorMessages.length > 0 && (
+              <div className={styles["fileCheck-errors"]}>
+                <span className={styles["fileCheck-errorIcon"]}>
+                  <i className="fas fa-exclamation"></i>
+                </span>
+                <span className={styles["fileCheck-label"]}>Errors:</span>
+                <ul className={styles["fileCheck-errorList"]}>
+                  {item.errorMessages.map((msg, i) => <li key={i}>{msg}</li>)}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
