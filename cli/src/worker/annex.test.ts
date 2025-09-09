7f3ace0a765826d8b79630f4cc660c2253160d66
Nell Hardcastle <nell@dev-nell.com>
2025-09-09 14:24:37 -0700

    fix(cli): Support annex keys with all valid filename characters

--- cli/src/worker/annex.test.ts
+++ cli/src/worker/annex.test.ts
@@ -1,6 +1,11 @@
 import { assertEquals } from "@std/assert/equals"
 import { join } from "@std/path/join"
-import { annexRelativePath, hashDirLower, hashDirMixed } from "./annex.ts"
+import {
+  annexKeyRegex,
+  annexRelativePath,
+  hashDirLower,
+  hashDirMixed,
+} from "./annex.ts"
 
 Deno.test("annexRelativePath() returns appropriate paths", () => {
   assertEquals(
@@ -24,5 +29,30 @@ Deno.test("hashDirMixed() returns the correct key prefix", async () => {
       "SHA256E-s311112--c3527d7944a9619afb57863a34e6af7ec3fe4f108e56c860d9e700699ff806fb.nii.gz",
     ),
     ["Xk", "Mx"],
+  )
+})
+
+Deno.test("annexKeyRegex matches valid keys", () => {
+  // Typical key
+  assertEquals(
+    "SHA256E-s311112--c3527d7944a9619afb57863a34e6af7ec3fe4f108e56c860d9e700699ff806fb.nii.gz"
+      .match(annexKeyRegex)?.slice(1, 5),
+    [
+      "SHA256E",
+      "311112",
+      "c3527d7944a9619afb57863a34e6af7ec3fe4f108e56c860d9e700699ff806fb",
+      ".nii.gz",
+    ],
+  )
+  // Filenames with other characters
+  assertEquals(
+    "SHA256E-s6148--e53302cf7b8beb7c5b908c070618bb11e2590719465d0869522716ecc2cfecd8.DS_Store"
+      .match(annexKeyRegex)?.slice(1, 5),
+    [
+      "SHA256E",
+      "6148",
+      "e53302cf7b8beb7c5b908c070618bb11e2590719465d0869522716ecc2cfecd8",
+      ".DS_Store",
+    ],
   )
 })
