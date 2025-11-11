61180cf7533e1c1d677b01c08ef69223b3152626
Nell Hardcastle <nell@dev-nell.com>
2025-11-11 11:24:21 -0800

    fix(worker): Update DropResource to run fsck and drop for each snapshot

--- services/datalad/datalad_service/handlers/drop.py
+++ services/datalad/datalad_service/handlers/drop.py
@@ -1,6 +1,6 @@
 import falcon
 
-from datalad_service.tasks.publish import annex_drop
+from datalad_service.tasks.publish import export_backup_and_drop
 
 
 class DropResource:
@@ -11,6 +11,6 @@ class DropResource:
 
     async def on_post(self, req, resp, dataset):
         dataset_path = self.store.get_dataset_path(dataset)
-        await annex_drop.kiq(dataset_path)
+        await export_backup_and_drop.kiq(dataset_path)
         resp.media = {}
         resp.status = falcon.HTTP_OK
