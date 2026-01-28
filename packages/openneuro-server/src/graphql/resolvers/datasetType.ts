d6e501e27bf0b124f05bdaa89a28b8b7f71692b5
Nell Hardcastle <nell@dev-nell.com>
2026-01-28 12:07:58 -0800

    fix(server): Load Dataset model correctly in datasetType resolver

--- packages/openneuro-server/src/graphql/resolvers/datasetType.ts
+++ packages/openneuro-server/src/graphql/resolvers/datasetType.ts
@@ -5,8 +5,10 @@ import { description } from "./description.js"
  * Return "schema" or "legacy" depending on the validator preferred for a dataset
  */
 export async function datasetType(dsOrSnapshot): Promise<"schema" | "legacy"> {
-  const ds = new Dataset({ id: dsOrSnapshot.datasetId })
-  if (ds.schemaValidator) {
+  const ds = await Dataset.findOne({
+    id: dsOrSnapshot.datasetId || dsOrSnapshot.id,
+  }).lean()
+  if (ds && ds.schemaValidator) {
     return "schema"
   } else {
     const dsDescription = await description(dsOrSnapshot)
