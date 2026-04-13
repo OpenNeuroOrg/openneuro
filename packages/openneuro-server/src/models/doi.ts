764c45c0aa9e0b6514cd3179f53baf464bb379a4
Nell Hardcastle <nell@dev-nell.com>
2026-04-13 09:18:04 -0700

    fix(server): Extend DOI model with state field

--- packages/openneuro-server/src/models/doi.ts
+++ packages/openneuro-server/src/models/doi.ts
@@ -1,17 +1,24 @@
 import mongoose from "mongoose"
 import type { Document } from "mongoose"
+import type { DoiState } from "../types/datacite"
 const { Schema, model } = mongoose
 
 export interface DoiDocument extends Document {
   datasetId: string
   snapshotId: string
   doi: string
+  state: DoiState
 }
 
 const doiSchema = new Schema({
   datasetId: String,
   snapshotId: String,
   doi: String,
+  state: {
+    type: String,
+    enum: ["draft", "registered", "findable"],
+    default: "draft",
+  },
 })
 
 const Doi = model<DoiDocument>("Doi", doiSchema)
