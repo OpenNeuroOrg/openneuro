d5ff0129a670ac433bbc79f14804735ad950e9a2
Christopher J. Markiewicz <markiewicz@stanford.edu>
2026-04-14 21:32:07 -0400

    feat(server): Define Pothos input types

---
+++ packages/openneuro-server/src/graphql/schema/inputs.ts
@@ -0,0 +1,191 @@
+import { builder } from "../builder"
+import { Severity, SortOrdering } from "./enums"
+
+export const UserSortInput = builder.inputType("UserSortInput", {
+  fields: (t) => ({
+    field: t.string({ required: true }),
+    order: t.field({ type: SortOrdering, defaultValue: "ascending" }),
+  }),
+})
+
+export const DatasetSort = builder.inputType("DatasetSort", {
+  fields: (t) => ({
+    created: t.field({ type: SortOrdering }),
+    name: t.field({ type: SortOrdering }),
+    uploader: t.field({ type: SortOrdering }),
+    stars: t.field({ type: SortOrdering }),
+    downloads: t.field({ type: SortOrdering }),
+    views: t.field({ type: SortOrdering }),
+    subscriptions: t.field({ type: SortOrdering }),
+    publishDate: t.field({ type: SortOrdering }),
+  }),
+})
+
+export const DatasetFilter = builder.inputType("DatasetFilter", {
+  fields: (t) => ({
+    public: t.boolean(),
+    shared: t.boolean(),
+    invalid: t.boolean(),
+    starred: t.boolean(),
+    all: t.boolean(),
+  }),
+})
+
+export const DeleteFile = builder.inputType("DeleteFile", {
+  fields: (t) => ({
+    path: t.string({ required: true }),
+    filename: t.string(),
+  }),
+})
+
+export const UploadFile = builder.inputType("UploadFile", {
+  fields: (t) => ({
+    filename: t.string({ required: true }),
+    size: t.field({ type: "BigInt", required: true }),
+  }),
+})
+
+export const SummaryPetInput = builder.inputType("SummaryPetInput", {
+  fields: (t) => ({
+    BodyPart: t.stringList(),
+    ScannerManufacturer: t.stringList(),
+    ScannerManufacturersModelName: t.stringList(),
+    TracerName: t.stringList(),
+    TracerRadionuclide: t.stringList(),
+  }),
+})
+
+export const ValidatorMetadataInput = builder.inputType(
+  "ValidatorMetadataInput",
+  {
+    fields: (t) => ({
+      validator: t.string(),
+      version: t.string(),
+    }),
+  },
+)
+
+export const SubjectMetadataInput = builder.inputType(
+  "SubjectMetadataInput",
+  {
+    fields: (t) => ({
+      participantId: t.string({ required: true }),
+      age: t.float(),
+      sex: t.string(),
+      group: t.string(),
+    }),
+  },
+)
+
+export const SummaryInput = builder.inputType("SummaryInput", {
+  fields: (t) => ({
+    id: t.id({ required: true }),
+    datasetId: t.id({ required: true }),
+    modalities: t.stringList(),
+    secondaryModalities: t.stringList(),
+    dataTypes: t.stringList(),
+    sessions: t.stringList(),
+    subjects: t.stringList(),
+    subjectMetadata: t.field({ type: [SubjectMetadataInput] }),
+    tasks: t.stringList(),
+    size: t.field({ type: "BigInt", required: true }),
+    totalFiles: t.int({ required: true }),
+    dataProcessed: t.boolean(),
+    pet: t.field({ type: SummaryPetInput }),
+    validatorMetadata: t.field({ type: ValidatorMetadataInput }),
+    schemaVersion: t.string(),
+  }),
+})
+
+export const ValidatorIssueInput = builder.inputType("ValidatorIssueInput", {
+  fields: (t) => ({
+    code: t.string({ required: true }),
+    subCode: t.string(),
+    location: t.string(),
+    severity: t.field({ type: Severity }),
+    rule: t.string(),
+    issueMessage: t.string(),
+    affects: t.string(),
+    line: t.int(),
+  }),
+})
+
+export const ValidatorCodeMessageInput = builder.inputType(
+  "ValidatorCodeMessageInput",
+  {
+    fields: (t) => ({
+      code: t.string({ required: true }),
+      message: t.string({ required: true }),
+    }),
+  },
+)
+
+export const ValidatorInput = builder.inputType("ValidatorInput", {
+  fields: (t) => ({
+    id: t.id({ required: true }),
+    datasetId: t.id({ required: true }),
+    issues: t.field({ type: [ValidatorIssueInput], required: true }),
+    codeMessages: t.field({
+      type: [ValidatorCodeMessageInput],
+      required: true,
+    }),
+    validatorMetadata: t.field({
+      type: ValidatorMetadataInput,
+      required: true,
+    }),
+  }),
+})
+
+export const MetadataInput = builder.inputType("MetadataInput", {
+  fields: (t) => ({
+    datasetId: t.id({ required: true }),
+    datasetUrl: t.string(),
+    datasetName: t.string(),
+    firstSnapshotCreatedAt: t.field({ type: "DateTime" }),
+    latestSnapshotCreatedAt: t.field({ type: "DateTime" }),
+    dxStatus: t.string(),
+    tasksCompleted: t.stringList(),
+    trialCount: t.int(),
+    studyDesign: t.string(),
+    studyDomain: t.string(),
+    studyLongitudinal: t.string(),
+    dataProcessed: t.boolean(),
+    species: t.string(),
+    associatedPaperDOI: t.string(),
+    openneuroPaperDOI: t.string(),
+    seniorAuthor: t.string(),
+    adminUsers: t.stringList(),
+    ages: t.floatList(),
+    modalities: t.stringList(),
+    grantFunderName: t.string(),
+    grantIdentifier: t.string(),
+    affirmedDefaced: t.boolean(),
+    affirmedConsent: t.boolean(),
+  }),
+})
+
+export const AnnexFsckInput = builder.inputType("AnnexFsckInput", {
+  fields: (t) => ({
+    command: t.string(),
+    errorMessages: t.stringList(),
+    file: t.string(),
+    key: t.string(),
+    note: t.string(),
+    success: t.boolean(),
+    dead: t.stringList(),
+    missing: t.stringList(),
+    untrusted: t.stringList(),
+    input: t.stringList(),
+  }),
+})
+
+export const ContributorInput = builder.inputType("ContributorInput", {
+  fields: (t) => ({
+    name: t.string(),
+    givenName: t.string(),
+    familyName: t.string(),
+    orcid: t.string(),
+    contributorType: t.string(),
+    order: t.int(),
+  }),
+})
