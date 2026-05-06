13f03f03f956fd18e0edd2c35efca5f938ae1edd
Christopher J. Markiewicz <markiewicz@stanford.edu>
2026-05-06 18:03:38 -0400

    chore: Update import locations for @bids/validator v3

--- packages/openneuro-app/src/scripts/workers/schema.ts
+++ packages/openneuro-app/src/scripts/workers/schema.ts
@@ -1,5 +1,5 @@
 import { runValidator } from "./schema.worker"
-import type { ValidationResult } from "@bids/validator/main"
+import type { ValidationResult } from "@bids/validator/validate"
 
 export function validation(files): Promise<ValidationResult> {
   return runValidator(files)
