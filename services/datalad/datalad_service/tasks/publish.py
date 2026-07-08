5fae2b225452dd940b0bd059c432a9c8e74cdcfc
Nell Hardcastle <nell@dev-nell.com>
2026-07-08 13:26:20 -0700

    fix(worker): Update remote configuration for newly public remotes before export
    Avoid export and retagged new objects, only existing objects are retagged

--- services/datalad/datalad_service/tasks/publish.py
+++ services/datalad/datalad_service/tasks/publish.py
@@ -71,9 +71,11 @@ async def create_remotes_and_export(dataset_path, public=False):
     Called by publish handler to make a dataset public initially.
     """
     create_remotes(dataset_path)
-    await export_dataset(dataset_path)
     if public:
         await set_remote_public(dataset_path)
+    await export_dataset(dataset_path)
+    if public:
+        await set_s3_access_tag(os.path.basename(dataset_path), 'public')
 
 
 def create_remotes(dataset_path):
@@ -311,7 +313,6 @@ async def set_remote_public(dataset_path):
         ['git-annex', 'enableremote', get_s3_remote(), 'x-amz-tagging=access=public'],
         dataset_path,
     )
-    await set_s3_access_tag(os.path.basename(dataset_path), 'public')
 
 
 def update_object_tag(client, s3_bucket, key, version_id, value):
