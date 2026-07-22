18a18e338ebc78c18f1ce95aaed09884844da656
jbwexler <jbwexler@stanford.edu>
2026-07-22 12:38:04 -0700

    Use DATALAD_DATASET_PATH in create_mosaic()

--- services/datalad/datalad_service/tasks/mosaic.py
+++ services/datalad/datalad_service/tasks/mosaic.py
@@ -2,14 +2,20 @@ import asyncio
 from pathlib import Path
 import logging
 import bidsmosaic
-
 import requests
 
 from datalad_service.config import GRAPHQL_ENDPOINT
 from datalad_service.broker import broker
 from datalad_service.common.redis import redis_client
+from datalad_service.config import DATALAD_DATASET_PATH
 
 logger = logging.getLogger('datalad_service.' + __name__)
+
+
+def get_mosaic_path(dataset_id, ref):
+    """Return path of a mosaic pdf file."""
+    mosaic_dir_path = Path(DATALAD_DATASET_PATH) / 'mosaics' / dataset_id
+    return mosaic_dir_path / f'{dataset_id}-{ref[0:6]}_mosaic.pdf'
 
 
 def mosaic_mutation(dataset_id, ref):
@@ -31,20 +37,19 @@ def mosaic_mutation(dataset_id, ref):
 @broker.task
 async def create_mosaic(dataset_id, dataset_path, ref, cookies=None, user=''):
     async with redis_client() as client:
-        lock = client.lock(f'mosaic-lock:{dataset_id}:{ref}', timeout=60 * 60 *4)
+        lock = client.lock(f'mosaic-lock:{dataset_id}:{ref}', timeout=60 * 60 * 4)
         if await lock.acquire(blocking=False):
             try:
-                out_dir_path = Path(f"/datalad/mosaics/{dataset_id}")
-                out_dir_path.mkdir(parents=True, exist_ok=True)
-                out_file_path = out_dir_path / f"{dataset_id}-{ref[0:6]}_mosaic.pdf"
-                
+                out_file_path = get_mosaic_path(dataset_id, ref)
+                out_file_path.parent.mkdir(parents=True, exist_ok=True)
+
                 await asyncio.to_thread(
                     bidsmosaic.create_mosaic_pdf,
                     dataset_path,
                     str(out_file_path),
                     downsample=2,
                 )
-                
+
                 if out_file_path.exists():
                     r = requests.post(
                         url=GRAPHQL_ENDPOINT,
